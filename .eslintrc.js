4511eebae188fcfa629b4b957f10bd85650043ec
YUNGU HAN <keypport@gmail.com>
2023-07-16 08:54:33 +0900

    style: update eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,76 @@
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'browser': true,
+        'es2021': true
     },
-    "parser": "@typescript-eslint/parser",
-    "extends": [
-        "airbnb",
-        "airbnb/hooks",
-        "airbnb-typescript",
-        "prettier",
-        "next/core-web-vitals",
-        "plugin:storybook/recommended"
+    'parser': '@typescript-eslint/parser',
+    'extends': [
+        'airbnb',
+        'airbnb/hooks',
+        'airbnb-typescript',
+        'prettier',
+        'next/core-web-vitals',
+        'plugin:storybook/recommended'
     ],
-    "overrides": [
+    'overrides': [
         {
-            "env": {
-                "node": true
+            'env': {
+                'node': true
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
+            'files': [
+                '.eslintrc.{js,cjs}'
             ],
-            "parserOptions": {
-                "sourceType": "script"
+            'parserOptions': {
+                'sourceType': 'script'
             }
         }
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": ["./tsconfig.json"]
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+        'project': ['./tsconfig.json']
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    'plugins': [
+        'react',
+        '@typescript-eslint'
     ],
-    "rules": {
+    'rules': {
+        'react/react-in-jsx-scope': OFF,
+        'react/require-default-props': OFF,
+        'react/jsx-pascal-case': ERROR,
+        'import/prefer-default-export': OFF,
+        'no-use-before-define': OFF,
+        'no-nested-ternary': OFF,
+        'no-param-reassign': ["error", { "props": false }],
+        'no-unused-vars': OFF,
+        '@typescript-eslint/no-unused-vars': [ERROR, {args: 'none'}],
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                'selector': 'default',
+                'format': ['camelCase']
+
+            },
+            {
+                'selector': 'typeLike',
+                'format': ['PascalCase']
+            },
+            {
+                'selector': 'variable',
+                'format': ['camelCase', 'UPPER_CASE']
+            },
+            {
+                'selector': 'function',
+                'format': ['camelCase', 'PascalCase']
+            },
+            {
+                'selector': 'objectLiteralProperty',
+                'format': null
+            },
+        ]
     }
 }
