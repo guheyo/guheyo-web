ed02ac814ef80b47de074d8ae91af694b6bebbc7
JL <jlee425kr@gmail.com>
2023-07-19 21:15:58 +0900

    add: install `zustand` and devtools

--- app/providers.tsx
+++ app/providers.tsx
@@ -8,6 +8,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  // zustand devtools
+  if (process.env.NODE_ENV === 'development') {
+    // mountStoreDevtool('Store', useStore);
+  }
+
   return (
     <ReduxProviders>
       <QueryClientProvider client={queryClient}>
