33948c59b833420ee2ceab1d07795c16afb34b4d
JL <jlee425kr@gmail.com>
2023-07-23 00:24:18 +0900

    react-query: 포스트피드

--- app/lib/api/posts.ts
+++ app/lib/api/posts.ts
@@ -1,4 +1,6 @@
 import { Post } from 'prisma';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
 import { client } from '../client';
 
 export type Posts = {
@@ -7,18 +9,19 @@ export type Posts = {
   hasNextPage: boolean;
 };
 
-export async function getPosts(
-  categoryId: string,
-  type: string,
-  cursor: string,
-) {
-  const res = await client.get<Posts>(
-    `/categories/${categoryId}/posts?type=${type}&cursor=${cursor}`,
-  );
-  return res.data;
-}
+export const useInfinitePosts = (categoryId: string, type: string) =>
+  useInfiniteQuery(['posts', categoryId, type], {
+    queryFn: async ({ pageParam = '' }) => {
+      const res = await client.get<Posts>(
+        `/categories/${categoryId}/posts?type=${type}&cursor=${pageParam}`,
+      );
+      return res.data;
+    },
+    getNextPageParam: (lastPage) => lastPage.cursor,
+    enabled: !!categoryId,
+  });
 
-export async function getPost(id: string) {
-  const res = await client.get<Post>(`/posts/${id}`);
-  return res.data;
-}
+export const usePost = (id: string) =>
+  useQuery(['posts', id], () => client.get(`/posts/${id}`), {
+    select: (data: AxiosResponse<Post>) => data.data,
+  });
