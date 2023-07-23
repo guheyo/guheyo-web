a2ae45b0a082701aac997af8daf6589756eb4db2
hohokie <nhpunch@gmail.com>
2023-07-23 19:42:29 +0900

    refactor: disable MUI preflight - tailwind랑 충돌 방지

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,15 +4,39 @@ import React from 'react';
 import { ReduxProviders } from '@/redux/provider';
 import { SessionProvider } from 'next-auth/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+/**
+ * MUI preflight Disable
+ */
+const theme = createTheme({
+  palette: {
+    // Your color palette here
+  },
+  typography: {
+    // Your typography settings here
+  },
+  // Other Material-UI customizations
+  // ...
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        // Disable preflight styles by setting it to an empty object
+      },
+    },
+  },
+});
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ReduxProviders>
-      <QueryClientProvider client={queryClient}>
-        <SessionProvider>{children}</SessionProvider>
-      </QueryClientProvider>
-    </ReduxProviders>
+    <ThemeProvider theme={theme}>
+      <ReduxProviders>
+        <QueryClientProvider client={queryClient}>
+          <SessionProvider>{children}</SessionProvider>
+        </QueryClientProvider>
+      </ReduxProviders>
+    </ThemeProvider>
   );
 }
