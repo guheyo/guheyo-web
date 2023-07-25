ab12a5acd185f621b79a6a52a63677dbbb4e696a
hohokie <nhpunch@gmail.com>
2023-07-25 10:53:52 +0900

    feat: Popper 컴포넌트 일반화

---
+++ app/components/base/popper.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import useClickOutside from '@/app/hooks/use-click-outside';
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable react/jsx-props-no-spreading */
+import { Fade, Popper as MUIPopper } from '@mui/material';
+import { PopperProps as MUIPopperProps } from '@mui/material/Popper';
+import { useRef } from 'react';
+
+interface PopperProps extends MUIPopperProps {
+  handler: () => void;
+  delay?: number;
+  children: React.ReactNode;
+}
+export default function Popper({
+  open,
+  anchorEl,
+  handler,
+  placement = 'bottom-start',
+  delay = 350,
+  children,
+}: PopperProps) {
+  const popperRef = useRef(null);
+
+  useClickOutside(popperRef, handler);
+
+  return (
+    <MUIPopper
+      ref={popperRef}
+      open={open}
+      anchorEl={anchorEl}
+      placement={placement}
+      transition
+    >
+      {({ TransitionProps }) => (
+        <Fade {...TransitionProps} timeout={delay}>
+          <div>{children}</div>
+        </Fade>
+      )}
+    </MUIPopper>
+  );
+}
