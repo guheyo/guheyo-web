6b91b729a3af7c74a578ab37bf5fb73d248ad217
hohokie <nhpunch@gmail.com>
2023-07-25 10:55:45 +0900

    refactor: 기존 popover -> mui popper 변경 적용 - typography 포함

--- app/components/users/user-profile.tsx
+++ app/components/users/user-profile.tsx
@@ -1,19 +1,17 @@
 'use client';
 
-import React from 'react';
-import {
-  Popover,
-  PopoverHandler,
-  PopoverContent,
-  Typography,
-} from '@material-tailwind/react';
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable react/jsx-props-no-spreading */
+import { Typography } from '@mui/material';
 import { User } from 'prisma';
 import _ from 'lodash';
 import { useDeviceDetect } from '@/app/hooks/use-device-detect';
+import { useState } from 'react';
 import UserAvatar from './user-avatar';
 import Roles from './roles';
 import SocialJoinDates from './social-join-dates';
 import DmDialog from '../base/dm-dialog';
+import Popper from '../base/popper';
 import Username from './user-name';
 
 export default function UserProfile({
@@ -27,8 +25,18 @@ export default function UserProfile({
   displayUsername: boolean;
   displayDM: boolean;
 }) {
-  const [openPopover, setOpenPopover] = React.useState(false);
   const device = useDeviceDetect();
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handlePopoverOpen = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
   const getDiscordSocialID = () => {
     const account = _.find(
       user.socialAccounts,
@@ -38,49 +46,54 @@ export default function UserProfile({
   };
 
   return (
-    <Popover open={openPopover} handler={setOpenPopover}>
-      <PopoverHandler>
-        <button type="button">
-          {device === 'mobile' && (
-            <div className="flex gap-2 items-center font-medium text-xs">
-              {displayAvatar && <UserAvatar user={user} size="xs" />}
-              {displayUsername && <Username user={user} />}
+    <>
+      <button type="button" onClick={handlePopoverOpen}>
+        {device === 'mobile' && (
+          <div className="flex gap-2 items-center font-medium text-xs">
+            {displayAvatar && <UserAvatar user={user} size="xs" />}
+            {displayUsername && <Username user={user} />}
+          </div>
+        )}
+        {device === 'browser' && (
+          <div className="flex gap-2 items-center font-medium text-base">
+            {displayAvatar && <UserAvatar user={user} size="sm" />}
+            {displayUsername && <Username user={user} />}
+          </div>
+        )}
+      </button>
+      <Popper
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        handler={handlePopoverClose}
+      >
+        <div className="max-w-xs z-50 bg-white p-4 border rounded-lg drop-shadow-lg">
+          <div className="mb-2 flex items-center justify-between gap-4">
+            <UserAvatar user={user} size="md" />
+            {displayDM && (
+              <DmDialog
+                url={`https://discord.com/users/${getDiscordSocialID()}`}
+              />
+            )}
+          </div>
+          <Typography
+            variant="caption"
+            color="blue-gray"
+            className="mb-2 flex items-center text-base"
+          >
+            <span className="font-semibold">{user.username}</span>
+          </Typography>
+          <Typography variant="caption" color="gray" className="font-normal">
+            <div>
+              <div className="font-semibold">가입시기</div>
+              <SocialJoinDates socialAccounts={user.socialAccounts} />
             </div>
-          )}
-          {device === 'browser' && (
-            <div className="flex gap-2 items-center font-medium text-base">
-              {displayAvatar && <UserAvatar user={user} size="sm" />}
-              {displayUsername && <Username user={user} />}
+            <div className="mt-1">
+              <div className="font-semibold">역할</div>
+              <Roles roles={user.roles} />
             </div>
-          )}
-        </button>
-      </PopoverHandler>
-      <PopoverContent className="max-w-xs z-50">
-        <div className="mb-2 flex items-center justify-between gap-4">
-          <UserAvatar user={user} size="md" />
-          {displayDM && (
-            <DmDialog
-              url={`https://discord.com/users/${getDiscordSocialID()}`}
-            />
-          )}
+          </Typography>
         </div>
-        <Typography
-          color="blue-gray"
-          className="mb-2 flex items-center text-base"
-        >
-          <span className="font-semibold">{user.username}</span>
-        </Typography>
-        <Typography variant="small" color="gray" className="font-normal">
-          <div>
-            <div className="font-semibold">가입시기</div>
-            <SocialJoinDates socialAccounts={user.socialAccounts} />
-          </div>
-          <div className="mt-1">
-            <div className="font-semibold">역할</div>
-            <Roles roles={user.roles} />
-          </div>
-        </Typography>
-      </PopoverContent>
-    </Popover>
+      </Popper>
+    </>
   );
 }
