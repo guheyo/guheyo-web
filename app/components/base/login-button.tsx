88a552455653e7fcfdc2382f9891a52c06878059
edenist-x-x <yaron_e5@naver.com>
2023-07-26 18:43:04 +0900

    refactor: login-button 전환 작업

--- app/components/base/login-button.tsx
+++ app/components/base/login-button.tsx
@@ -1,41 +1,63 @@
 'use client';
 
 import { useSession, signIn, signOut } from 'next-auth/react';
-import React from 'react';
-import {
-  Menu,
-  MenuHandler,
-  MenuItem,
-  MenuList,
-} from '@material-tailwind/react';
+import React, { useState } from 'react';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import { Menu, MenuItem, MenuList, MenuProps } from '@mui/material';
 import Avatar from './avatar';
 
 export default function LoginButton() {
   const { data: session } = useSession();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const menuOpen = Boolean(anchorEl);
+
+  const handleOpenMenu = (event: React.MouseEvent<HTMLElement>): void => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = (): void => {
+    setAnchorEl(null);
+  };
+
+  const handleSignOut = (): void => {
+    handleCloseMenu();
+    signOut();
+  };
 
   if (session && session.user) {
     return (
-      <Menu>
-        <MenuHandler>
-          <div className="inline-flex items-center">
-            <Avatar
-              name={session.user.username}
-              avatarURL={session.user.avatarURL}
-            />
-          </div>
-        </MenuHandler>
-        <MenuList>
-          <MenuItem>
-            <button type="submit" onClick={() => signOut()}>
-              <div className="flex flex-row gap-1 items-center">
-                <PowerIcon width={18} height={18} />
-                <div>로그아웃</div>
-              </div>
-            </button>
+      <div>
+        <div className="inline-flex items-center" onClick={handleOpenMenu}>
+          <Avatar
+            name={session.user.username}
+            avatarURL={session.user.avatarURL}
+          />
+        </div>
+        <Menu
+          open={menuOpen}
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          onClose={handleCloseMenu}
+          className="mt-1 pr-3 pl-3 w-[250px] ml-3"
+        >
+          <MenuItem className="text-sm rounded-md w-[180px] focus:bg-transparent pb-2 pt-2">
+            <div className=" focus:bg-gray-100 hover:bg-gray-100 w-full h-[30px] rounded-md">
+              <button
+                type="submit"
+                onClick={handleSignOut}
+                className="focus:bg-gray-50 w-full p-1"
+              >
+                <div className="flex flex-row gap-1 items-center text-sm text-gray-500">
+                  <PowerIcon width={18} height={18} />
+                  <span>로그아웃</span>
+                </div>
+              </button>
+            </div>
           </MenuItem>
-        </MenuList>
-      </Menu>
+        </Menu>
+      </div>
     );
   }
   return (
