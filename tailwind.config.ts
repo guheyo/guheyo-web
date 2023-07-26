f80a88805f4c8f7dc5bf887afc5ad54a55a807d8
Jay_Lee <57380565+edenist-x-x@users.noreply.github.com>
2023-07-26 20:14:08 +0900

    refactor: login-button 전환 작업 (#18)
    * refactor: login-button 전환 작업
    
    * refactor: login-button 전환 작업 > boxShadow 전역 css 적용
    
    * refactor: Menu > 스크롤 고정 해제
    
    * feat: skeleton 적용
    
    * chore: merge
    
    ---------
    
    Co-authored-by: hohokie <nhpunch@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        dropdown:
+          'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+      },
     },
     container: {
       center: true,
