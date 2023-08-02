1add386c1d72e8f2355c1585671c3ad8c11691a4
Jaccree <jlee425kr@gmail.com>
2023-08-02 23:51:13 +0900

    change: styles

--- app/components/base/navbar.tsx
+++ app/components/base/navbar.tsx
@@ -5,9 +5,11 @@ import LoginButton from './login-button';
 
 export default function Navbar() {
   return (
-    <header className="sticky top-0 z-10 w-full px-2 py-1 bg-white shadow-sm flex items-center justify-between">
-      <Link href="/">WTB.KR</Link>
-      <LoginButton />
+    <header className="sticky top-0 w-full border-b-2 box-border z-[41]">
+      <div className="max-w-7xl mx-auto flex items-center justify-between px-2 py-1 h-14 bg-white">
+        <Link href="/">WTB.KR</Link>
+        <LoginButton />
+      </div>
     </header>
   );
 }
