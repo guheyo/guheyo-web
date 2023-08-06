25990f84891835068a1cd1f0bf77919f2307d99d
Jaccree <jlee425kr@gmail.com>
2023-08-06 19:28:46 +0900

    fix: TSConfig에 `next.config.js` 추가

--- next.config.js
+++ next.config.js
@@ -4,20 +4,24 @@ const nextConfig = {
     dirs: ['app', 'redux', 'types'],
   },
   images: {
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: 'cdn.discordapp.com',
-    }, {
-      protocol: 'https',
-      hostname: 'wtb-kr.s3.ap-northeast-2.amazonaws.com'
-    }, {
-      protocol: 'https',
-      hostname: 'wtb-kr-dev.s3.ap-northeast-2.amazonaws.com'
-    }],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wtb-kr.s3.ap-northeast-2.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wtb-kr-dev.s3.ap-northeast-2.amazonaws.com',
+      },
+    ],
     minimumCacheTTL: 24 * 60 * 60,
   },
   typescript: {
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
   },
   async redirects() {
     return [
@@ -28,6 +32,14 @@ const nextConfig = {
       },
     ];
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: '/@:username',
+        destination: '/user/:username',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
