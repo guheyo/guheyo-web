40d15975f1abdf98cbfb1c2a33ce47805be5450b
keypport <113260136+keypport@users.noreply.github.com>
2023-08-21 21:42:34 +0900

    feat: remove user functions in sessions (#45)

--- app/lib/api/sessions.ts
+++ app/lib/api/sessions.ts
@@ -1,44 +1,6 @@
 import { User, Session } from 'prisma';
 import { client } from '../client';
 
-type CreateUserBody = {
-  username: string;
-};
-
-export async function createUser(user: CreateUserBody) {
-  const res = await client.post<User>(`/users`, user);
-  return res.data;
-}
-
-export async function getUser(id: string) {
-  const res = await client.get<User>(`/users/${id}`);
-  return res.data;
-}
-
-export async function getUserBySocailAccount(
-  socialId: string,
-  provider: string,
-) {
-  const res = await client.get<User>(
-    `/users?socialId=${socialId}&provider=${provider}`,
-  );
-  return res.data;
-}
-
-type UpdateUserBody = {
-  username: string;
-};
-
-export async function updateUser(id: string, body: UpdateUserBody) {
-  const res = await client.patch<User>(`/users/${id}`, body);
-  return res.data;
-}
-
-export async function deleteUser(id: string) {
-  const res = await client.delete<User>(`/users/${id}`);
-  return res.data;
-}
-
 type CreateSessionBody = {
   sessionToken: string;
   userId: string;
