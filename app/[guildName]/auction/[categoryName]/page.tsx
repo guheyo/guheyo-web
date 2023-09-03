ffc677312beb8332a57696bf79359a9c0095766f
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-09-03 16:59:48 +0900

    feat: add dummy data for auctiono feed

--- app/[guildName]/auction/[categoryName]/page.tsx
+++ app/[guildName]/auction/[categoryName]/page.tsx
@@ -1,20 +1,28 @@
 'useClient';
 
-// import { useMemo } from 'react';
-// import Feed from '@/app/components/posts/feed';
+import { useMemo } from 'react';
+import Feed from '@/app/components/posts/feed';
 // import { useGuildCategories } from '@/app/lib/api/guilds';
-import { CategoryPageProps } from '../market/[categoryName]/page';
+import { CategoryPageProps } from '../../market/[categoryName]/page';
+
+// dummy data
+const categories = [
+  { id: 'auction', name: '경매' },
+  { id: 'auction-schedule', name: '경매일정' },
+];
 
 function AuctionPage({
   params: { guildName, categoryName },
 }: CategoryPageProps) {
   // const { data: categories, isLoading } = useGuildCategories(guildName);
-  // const currentCategory = useMemo(
-  //   () => categories?.find((c) => encodeURIComponent(c.name) === categoryName),
-  //   [categories, categoryName],
-  // );
+
+  const currentCategory = useMemo(
+    () => categories?.find((c) => encodeURIComponent(c.name) === categoryName),
+    [categoryName],
+  );
   // if (isLoading || !currentCategory) return <div>loading</div>;
-  // return <Feed categoryId={currentCategory.id} />;
+
+  return <Feed categoryId={currentCategory!.id} />;
 }
 
 export default AuctionPage;
