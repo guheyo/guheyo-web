478827fa03c30877de47cdef5212fbf06a85271a
keypport <113260136+keypport@users.noreply.github.com>
2023-10-22 17:06:11 +0900

    feat: keen-slider 적용, slider 상세 보기 아이콘의 배경 색상 지정 (#50)
    * refactor: carousel `react-material-carousel` -> `keen-slider/react` 변경 (#46)
    
    * refactor: carousel `react-material-carousel` -> `keen-slider/react` 변경
    
    * refactor: 컴포넌트 props 타입 분리 (건의용)
    
    * refactor : `image-carousel` -> `image-slider` 변경
    
    * feat : slider 내부 이미지 아이콘 색상 및 배경 색상 지정 (#49)
    
    ---------
    
    Co-authored-by: edenist-x-x <57380565+edenist-x-x@users.noreply.github.com>

--- app/components/base/image-slider.tsx
+++ app/components/base/image-slider.tsx
@@ -1,75 +1,99 @@
+'use client';
+
 import React, { useState } from 'react';
-import 'keen-slider/keen-slider.min.css';
-import {
-  KeenSliderInstance,
-  KeenSliderHooks,
-  useKeenSlider,
-} from 'keen-slider/react';
 import _ from 'lodash';
 import { Image } from 'prisma';
 import NextImage from 'next/image';
+import { IconButton } from '@mui/material';
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  MagnifyingGlassIcon,
+} from '@heroicons/react/24/outline';
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+import { KeenSliderHooks, KeenSliderInstance } from 'keen-slider';
+import Link from 'next/link';
+import { isMobile } from 'react-device-detect';
 
-function Arrow({
-  disabled,
-  onClick,
-  left,
-}: {
+interface CarouselArrowProps {
   disabled: boolean;
-  onClick: React.MouseEventHandler;
   left?: boolean;
-}) {
-  const opacity = disabled ? `opacity-20` : `opacity-100`;
-  if (left) {
-    return (
-      <svg
-        onClick={onClick}
-        className={`arrow absolute left-0 top-1/2 w-4 h-4 md:w-6 md:h-6 arrow--left fill-white ${opacity}`}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-      </svg>
-    );
-  }
-  return (
-    <svg
-      onClick={onClick}
-      className={`arrow absolute right-0 top-1/2 w-4 h-4 md:w-6 md:h-6 arrow--right fill-white ${opacity}`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-    >
-      <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-    </svg>
-  );
+  onClick: (e: any) => void;
 }
 
-function Dots({
-  instanceRef,
-  currentSlide,
-}: {
+interface NavigationProps {
+  activeIndex: number;
+  length: number;
   instanceRef: React.MutableRefObject<KeenSliderInstance<
     {},
     {},
     KeenSliderHooks
   > | null>;
-  currentSlide: number;
-}) {
-  const slides = instanceRef.current?.track.details.slides;
-  if (!slides) return null;
+}
+
+interface ZoomIconProps {
+  targetImage: Image;
+}
+
+function ZoomIcon({ targetImage }: ZoomIconProps) {
   return (
-    <div>
-      {slides.map((slide, i) => {
-        const opacity = currentSlide === i ? `opacity-100` : `opacity-20`;
-        return (
-          <button
-            type="submit"
-            aria-label="dot"
-            key={slide.abs}
-            className={`dot m-1 w-3 h-3 md:w-4 md:h-4 rounded-full bg-black ${opacity}`}
-            onClick={() => instanceRef.current?.moveToIdx(i)}
-          />
-        );
-      })}
+    <Link key={targetImage.id} href={targetImage.url} target="_blank">
+      <IconButton
+        className={`absolute ${
+          isMobile ? 'bottom-[5px] right-[5px]' : 'bottom-[10px] right-[10px]'
+        } bg-gray-500 bg-opacity-10`}
+      >
+        <MagnifyingGlassIcon className="w-[20px] text-gray-200" />
+      </IconButton>
+    </Link>
+  );
+}
+
+function Arrow({ disabled, onClick, left }: CarouselArrowProps) {
+  return (
+    <IconButton
+      className={`w-[50px] h-[50px] absolute top-[45%] cursor-pointer ${
+        disabled && 'hidden'
+      } ${left ? 'left-[5px]' : 'left-[auto] right-[5px]'}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {left ? (
+        <ChevronLeftIcon
+          height={30}
+          width={30}
+          className="text-gray-200 stroke-[1.5]"
+        />
+      ) : (
+        <ChevronRightIcon
+          height={30}
+          width={30}
+          className="text-gray-200 stroke-[1.5]"
+        />
+      )}
+    </IconButton>
+  );
+}
+
+function Navigation({ activeIndex, length, instanceRef }: NavigationProps) {
+  if (length < 2) return null;
+  return (
+    // <div className="flex justify-center gap-2 z-50 mt-4 mb-4">
+    <div className="absolute bottom-2 md:bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+      {_.range(length).map((index) => (
+        <div
+          role="presentation"
+          aria-label="indicator"
+          key={index}
+          className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+            activeIndex === index ? 'bg-gray-200 w-8' : 'bg-gray-300 w-4'
+          }`}
+          onClick={() => {
+            instanceRef.current?.moveToIdx(index);
+          }}
+        />
+      ))}
     </div>
   );
 }
@@ -77,92 +101,75 @@ function Dots({
 export default function ImageSlider({
   images,
   sizes,
-  fill,
   width,
   height,
 }: {
-  images: Image[] | undefined;
+  images: Image[];
   sizes: string;
-  fill: boolean;
-  width?: number;
-  height?: number;
+  width: number;
+  height: number;
 }) {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [loaded, setLoaded] = useState(false);
-
-  const [sliderRef, instanceRef] = useKeenSlider({
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     initial: 0,
     slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel);
+      setActiveIndex(slider.track.details.rel);
     },
     created() {
       setLoaded(true);
     },
   });
 
-  if (_.isUndefined(images) || _.isEmpty(images)) return null;
-
   return (
     <div className="relative">
-      <div ref={sliderRef} className="keen-slider">
+      <div ref={sliderRef} className="keen-slider h-[320px] md:h-[520px]">
         {_.map(images, (image, i) => (
           <div
             key={i}
-            className={`${sizes} flex justify-center items-center`}
+            className="keen-slider__slide"
             style={{
               position: 'relative',
-              objectFit: 'cover',
             }}
           >
-            {fill && (
-              <NextImage
-                className={`keen-slider__slide number-slide${i}`}
-                src={image.url}
-                alt="Preview image"
-                fill
-                loading="lazy"
-              />
-            )}
-            {!fill && (
-              <NextImage
-                className={`keen-slider__slide number-slide${i}`}
-                src={image.url}
-                alt="Preview image"
-                width={width}
-                height={height}
-                loading="lazy"
-              />
-            )}
+            <NextImage
+              src={image.url}
+              alt={image.name}
+              fill
+              style={{ objectFit: 'cover' }}
+              sizes="50vw"
+            />
+            <ZoomIcon targetImage={images[activeIndex]} />
           </div>
         ))}
       </div>
-      {loaded && instanceRef.current && images.length > 1 && (
-        <>
+      {loaded && instanceRef.current && images && images.length > 1 && (
+        <div>
           <Arrow
             left
             onClick={(e: any) =>
               e.stopPropagation() || instanceRef.current?.prev()
             }
-            disabled={currentSlide === 0}
+            disabled={activeIndex === 0}
           />
           <Arrow
             onClick={(e: any) =>
               e.stopPropagation() || instanceRef.current?.next()
             }
             disabled={
-              currentSlide ===
+              activeIndex ===
               instanceRef.current.track.details.slides.length - 1
             }
           />
-        </>
-      )}
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
-        <div>
-          {loaded && instanceRef.current && images.length > 1 && (
-            <Dots instanceRef={instanceRef} currentSlide={currentSlide} />
-          )}
         </div>
-      </div>
+      )}
+      {loaded && instanceRef.current && images && (
+        <Navigation
+          activeIndex={activeIndex}
+          length={images.length}
+          instanceRef={instanceRef}
+        />
+      )}
     </div>
   );
 }
