4ab957f9da4b012e29e4c660baff875d4683d6ea
keypport <keypport@gmail.com>
2023-12-22 06:24:15 +0900

    feat: change home text

--- components/base/navbar.tsx
+++ components/base/navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <header className="sticky top-0 w-full border-b-2 box-border z-[41]">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-2 py-1 h-14 bg-white">
         <Link href="/">
-          <p className="text-xl font-bold">guheyo</p>
+          <p className="text-xl font-bold">구해요</p>
         </Link>
         <LoginButton />
       </div>
