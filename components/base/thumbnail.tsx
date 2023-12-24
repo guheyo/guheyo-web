13c01238e6a61d465679a48b6bb2531bafb06d5e
keypport <keypport@gmail.com>
2023-12-25 02:40:21 +0900

    feat: update offer, thumbnail

--- components/base/thumbnail.tsx
+++ components/base/thumbnail.tsx
@@ -3,27 +3,10 @@
 import { Image } from 'prisma';
 import NextImage from 'next/image';
 
-export default function Thumbnail({
-  image,
-  sizes,
-}: {
-  image: Image;
-  sizes: string;
-}) {
+export default function Thumbnail({ image }: { image: Image }) {
   return (
-    <div
-      className={`${sizes} flex overflow-hidden justify-center items-center`}
-      style={{
-        position: 'relative',
-      }}
-    >
-      <NextImage
-        src={image.url}
-        alt={image.name}
-        fill
-        style={{ objectFit: 'cover' }}
-        sizes="50vw"
-      />
+    <div className="flex justify-center items-center relative overflow-hidden w-fit h-32 md:h-52 lg:h-44 rounded-md">
+      <NextImage src={image.url} alt={image.name} width={1024} height={1024} />
     </div>
   );
 }
