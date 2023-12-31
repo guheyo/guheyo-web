87ac9cef488652c2b9b4a10325ce7922f49c2151
keypport <keypport@gmail.com>
2024-01-01 03:25:27 +0900

    feat: devide cache from apollo provider

--- lib/apollo/apollo-provider.tsx
+++ lib/apollo/apollo-provider.tsx
@@ -1,34 +1,7 @@
 'use client';
 
-import { ApolloLink, HttpLink } from '@apollo/client';
-import {
-  ApolloNextAppProvider,
-  NextSSRApolloClient,
-  SSRMultipartLink,
-} from '@apollo/experimental-nextjs-app-support/ssr';
-import React from 'react';
-import { cache } from './cache';
-
-function makeClient() {
-  const httpLink = new HttpLink({
-    uri: `${process.env.NEXT_PUBLIC_GRAPHQL_BASE_URL}:${process.env.NEXT_PUBLIC_GRAPHQL_PORT}/graphql`,
-  });
-
-  return new NextSSRApolloClient({
-    cache,
-    link:
-      typeof window === 'undefined'
-        ? ApolloLink.from([
-            new SSRMultipartLink({
-              stripDefer: true,
-            }),
-            httpLink,
-          ])
-        : httpLink,
-  });
-}
-
-export const client = makeClient();
+import { ApolloNextAppProvider } from '@apollo/experimental-nextjs-app-support/ssr';
+import { makeClient } from './client';
 
 export function ApolloWrapper({ children }: React.PropsWithChildren) {
   return (
