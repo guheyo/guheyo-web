b835ab442098f7fad3c81214e2016e0e9e933364
keypport <113260136+keypport@users.noreply.github.com>
2024-01-07 06:02:45 +0900

    feat: add graphql infinite scroll to offers (#62)
    * feat: update category navbar position
    
    * feat: update scrollbar up position
    
    * feat: add graphql infinite scroll

--- components/base/navbar.tsx
+++ components/base/navbar.tsx
@@ -3,18 +3,21 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import LoginButton from './login-button';
+import Scrollbar from './scrollbar';
 
 export default function Navbar() {
   return (
-    <header className="sticky top-0 w-full z-[41] bg-dark-600">
-      <div className="max-w-5xl mx-auto flex items-center justify-between px-2 py-1 h-14">
-        <Link href="/">
-          <div className="flex flex-row items-center">
-            <Image alt="guheyo logo" src="/star.ico" width={30} height={30} />
-          </div>
-        </Link>
-        <LoginButton />
-      </div>
-    </header>
+    <Scrollbar upPosition="top-0">
+      <header className="w-full z-[41] bg-dark-600">
+        <div className="max-w-5xl mx-auto flex items-center justify-between px-2 py-1 h-14">
+          <Link href="/">
+            <div className="flex flex-row items-center">
+              <Image alt="guheyo logo" src="/star.ico" width={30} height={30} />
+            </div>
+          </Link>
+          <LoginButton />
+        </div>
+      </header>
+    </Scrollbar>
   );
 }
