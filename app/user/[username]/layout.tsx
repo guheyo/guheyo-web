640087859650bde7da1acbbe107c6d12215ab2e6
keypport <keypport@gmail.com>
2024-01-16 16:19:17 +0900

    feat: add app/user

---
+++ app/user/[username]/layout.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="max-w-md pt-0 md:pt-12 mx-2 md:mx-0">
+      {children}
+    </div>
+  );
+}
