1a1c813bf71af0b8333cdbc89af1fbf5f90358b3
keypport <keypport@gmail.com>
2024-01-22 06:09:38 +0900

    feat: update link in guild preview

--- components/guilds/guild-preview.tsx
+++ components/guilds/guild-preview.tsx
@@ -15,12 +15,12 @@ export default function GuildPreview({ guild }: Props) {
   return (
     <div className="bg-dark-500 rounded-lg">
       <div className="w-fit px-0 md:px-0 pt-4 mx-2 md:mx-0">
-        <Link href={`g/${guild.slug}/market`}>
+        <Link href={`g/${guild.slug}`}>
           <GuildInfo slug={guild.slug!} />
         </Link>
       </div>
       <div className="flex flex-row justify-between gap-2 text-lg text-star-500 font-medium mx-2 md:mx-3 pt-5 pb-1">
-        <Link href={`g/${guild.slug}/market`}>
+        <Link href={`g/${guild.slug}/market/offers`}>
           <span className="flex flex-row font-medium items-center gap-1">
             {guild.name} 팝니다
           </span>
@@ -32,7 +32,7 @@ export default function GuildPreview({ guild }: Props) {
         ))}
       </div>
       <div className="flex justify-end text-base md:text-lg text-light-200 font-medium mx-2 md:mx-3 pt-2">
-        <Link href={`g/${guild.slug}/market`}>
+        <Link href={`g/${guild.slug}/market/offers`}>
           <span className="flex flex-row font-medium items-center gap-1">
             <PlayCircleOutlineOutlinedIcon fontSize="medium" />
             판매 채널 입장
@@ -40,7 +40,7 @@ export default function GuildPreview({ guild }: Props) {
         </Link>
       </div>
       <div className="text-lg text-star-500 font-medium mx-2 md:mx-3 pt-5 pb-1">
-        <Link href={`g/${guild.slug}/market`}>{guild.name} 삽니다</Link>
+        <Link href={`g/${guild.slug}/market/demands`}>{guild.name} 삽니다</Link>
       </div>
       <div className="grid gap-x-0 md:gap-x-6 gap-y-1 lg:gap-y-14 grid-cols-1 md:grid-cols-3 lg:grid-cols-3 px-0 md:px-0">
         {guild.demands.map((demand) => (
@@ -48,7 +48,7 @@ export default function GuildPreview({ guild }: Props) {
         ))}
       </div>
       <div className="flex justify-end text-base md:text-lg text-light-200 font-medium mx-2 md:mx-3 pt-2">
-        <Link href={`g/${guild.slug}/market`}>
+        <Link href={`g/${guild.slug}/market/demands`}>
           <span className="flex flex-row font-medium items-center gap-1">
             <PlayCircleOutlineOutlinedIcon fontSize="medium" />
             구매 채널 입장
