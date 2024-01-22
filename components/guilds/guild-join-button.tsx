c6fcaaed5620d31bb08b18eafd5d32e498aee51f
keypport <keypport@gmail.com>
2024-01-23 04:50:22 +0900

    feat: update join button name

--- components/guilds/guild-join-button.tsx
+++ components/guilds/guild-join-button.tsx
@@ -15,9 +15,10 @@ export default function GuildJoinButton({ slug }: { slug: string }) {
       <button
         type="submit"
         className="bg-star-500 hover:bg-star-400 text-sm font-bold px-2 py-1.5 rounded text-light-200"
+        name={`${slug} 길드에 입장하기`}
         onClick={() => join()}
       >
-        입장해요
+        입장
       </button>
     </div>
   );
