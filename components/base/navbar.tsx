6d77c394397625181f428fcd2c4f500383d12ff5
keypport <keypport@gmail.com>
2024-01-22 15:01:54 +0900

    feat: resize navbar height

--- components/base/navbar.tsx
+++ components/base/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   return (
     <Scrollbar upPosition="top-0" zIndex={50}>
       <header className="w-full border-b-2 border-dark-600">
-        <div className="max-w-5xl mx-auto flex items-center justify-between px-2 md:px-0 py-4 md:py-8 h-12 md:h-14">
+        <div className="max-w-5xl mx-auto flex items-center justify-between px-2 md:px-0 py-4 h-12">
           <Link href="/">
             <div className="flex flex-row gap-2 items-center">
               <Image
