6329c0d854ceeb7b433350a01256f41fe74b8b0d
keypport <keypport@gmail.com>
2024-01-27 03:47:33 +0900

    feat: update error page

--- app/error.tsx
+++ app/error.tsx
@@ -18,10 +18,12 @@ export default function GlobalError() {
         <span className="pt-2">
           일반적으로 페이지를 새로고침하면 오류가 해결돼요
         </span>
-        <span className="pt-2">그래도 문제가 해결되지 않는 경우</span>
+        <span className="pt-2">문제가 해결되지 않을 경우</span>
         <span>1. 인터넷 연결을 확인해 주세요</span>
         <span>2. 캐시를 삭제해 주세요</span>
-        <span className="mt-2">해결되지 않은 문제는 디코로 문의해 주세요</span>
+        <span className="mt-2">
+          도움이 더 필요하시다면 디코로 문의해 주세요
+        </span>
         <span className="mt-2 flex items-rows content-center gap-1">
           <DiscordGuildLink width={32} height={32}>
             <div className="text-star-500 font-bold">동물의 왕국</div>
