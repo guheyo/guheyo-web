9fd53be8e3e129bdb7b0947bcbc8e02babf44486
keypport <keypport@gmail.com>
2024-02-06 22:29:57 +0900

    feat: add authLink in client

--- lib/apollo/client.ts
+++ lib/apollo/client.ts
@@ -1,15 +1,32 @@
-import { HttpLink } from '@apollo/client';
+import { HttpLink, from } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { NextSSRApolloClient } from '@apollo/experimental-nextjs-app-support/ssr';
+import { getSession } from 'next-auth/react';
 import { cache } from './cache';
 
-export function makeClient() {
-  const link = new HttpLink({
-    uri: `${process.env.NEXT_PUBLIC_GRAPHQL_BASE_URL}:${process.env.NEXT_PUBLIC_GRAPHQL_PORT}/graphql`,
-  });
+export const httpLink = new HttpLink({
+  uri: `${process.env.NEXT_PUBLIC_GRAPHQL_BASE_URL}:${process.env.NEXT_PUBLIC_GRAPHQL_PORT}/graphql`,
+});
 
+export const getBearerToken = async () => {
+  const session = await getSession();
+  return session?.user?.accessToken ? `Bearer ${session.user.accessToken}` : '';
+};
+
+export const authLink = setContext(async (_, { headers }) => {
+  const modifiedHeader = {
+    headers: {
+      ...headers,
+      authorization: await getBearerToken(),
+    },
+  };
+  return modifiedHeader;
+});
+
+export function makeClient() {
   return new NextSSRApolloClient({
+    link: from([httpLink, authLink]),
     cache,
-    link,
   });
 }
 
