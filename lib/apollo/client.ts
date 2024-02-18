93310bb6ae004d6c154603f314d1fd4b9db733b9
keypport <keypport@gmail.com>
2024-02-18 17:28:03 +0900

    feat: remove authLink, set credential include in HttpLink

--- lib/apollo/client.ts
+++ lib/apollo/client.ts
@@ -1,31 +1,15 @@
 import { HttpLink, from } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 import { NextSSRApolloClient } from '@apollo/experimental-nextjs-app-support/ssr';
-import { getSession } from 'next-auth/react';
 import { cache } from './cache';
 
 export const httpLink = new HttpLink({
-  uri: `${process.env.NEXT_PUBLIC_GRAPHQL_BASE_URL}:${process.env.NEXT_PUBLIC_GRAPHQL_PORT}/graphql`,
-});
-
-export const getBearerToken = async () => {
-  const session = await getSession();
-  return session?.user?.accessToken ? `Bearer ${session.user.accessToken}` : '';
-};
-
-export const authLink = setContext(async (_, { headers }) => {
-  const modifiedHeader = {
-    headers: {
-      ...headers,
-      authorization: await getBearerToken(),
-    },
-  };
-  return modifiedHeader;
+  uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
+  credentials: 'include',
 });
 
 export function makeClient() {
   return new NextSSRApolloClient({
-    link: from([authLink, httpLink]),
+    link: from([httpLink]),
     cache,
   });
 }
