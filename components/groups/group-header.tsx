b11e219276f875d5ec4a1f96f9b0f3be75307282
keypport <keypport@gmail.com>
2024-02-23 19:05:42 +0900

    feat: divide api logic from group-info to group-header

---
+++ components/groups/group-header.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useFindGroupQuery } from '@/generated/graphql';
+import { groupVar } from '@/lib/apollo/cache';
+import Image from 'next/image';
+import { isMobile } from 'react-device-detect';
+
+export default function GroupHeader({ slug }: { slug: string }) {
+  const { loading, data } = useFindGroupQuery({
+    variables: {
+      slug,
+    },
+  });
+
+  if (loading) return <div />;
+  if (!data?.findGroup) return <div />;
+  const group = data?.findGroup;
+  groupVar(group);
+
+  return (
+    <div className="flex flex-row items-center gap-4">
+      <Image
+        src={!group.icon ? '/star/star.svg' : group.icon}
+        width={isMobile ? 48 : 56}
+        height={isMobile ? 48 : 56}
+        alt={`${group.name} logo`}
+        className="rounded-lg"
+      />
+      <div className="text-star-500 text-base md:text-lg font-bold">{`g/${group.name}`}</div>
+    </div>
+  );
+}
--- components/groups/group-info.tsx
+++ components/groups/group-info.tsx
@@ -1,33 +1,27 @@
 'use client';
 
-import { useFindGroupQuery } from '@/generated/graphql';
-import { groupVar } from '@/lib/apollo/cache';
 import Image from 'next/image';
 import { isMobile } from 'react-device-detect';
 
-export default function GroupInfo({ slug }: { slug: string }) {
-  const { loading, data } = useFindGroupQuery({
-    variables: {
-      slug,
-    },
-  });
-
-  if (loading) return <div />;
-  if (!data?.findGroup) return <div />;
-
-  const group = data?.findGroup;
-  groupVar(group);
-
+export default function GroupInfo({
+  name,
+  icon,
+}: {
+  name: string;
+  icon?: string | null;
+}) {
   return (
     <div className="flex flex-row items-center gap-4">
-      <Image
-        src={!group.icon ? '/star/star.svg' : group.icon}
-        width={isMobile ? 48 : 56}
-        height={isMobile ? 48 : 56}
-        alt={`${group.name} logo`}
-        className="rounded-lg"
-      />
-      <div className="text-star-500 text-base md:text-lg font-bold">{`g/${group.name}`}</div>
+      <div>
+        <Image
+          src={!icon ? '/star/star.svg' : icon}
+          width={isMobile ? 48 : 56}
+          height={isMobile ? 48 : 56}
+          alt={`${name} logo`}
+          className="rounded-lg"
+        />
+      </div>
+      <div className="text-star-500 text-base md:text-lg font-bold">{`g/${name}`}</div>
     </div>
   );
 }
