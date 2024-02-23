e58ea4048a596b6936546db57f1ff5b415b65ea9
keypport <keypport@gmail.com>
2024-02-24 05:36:59 +0900

    feat: add UseCreateQueryString

---
+++ hooks/use-create-query-string.ts
@@ -0,0 +1,19 @@
+import { useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
+
+export const useCreateQueryString = () => {
+  const searchParams = useSearchParams();
+
+  const createQueryString = useCallback(
+    (name: string, value: string | null) => {
+      const params = new URLSearchParams(searchParams.toString());
+      if (value) {
+        params.set(name, value);
+      }
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+  return createQueryString;
+};
