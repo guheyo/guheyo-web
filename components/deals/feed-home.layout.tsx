975c678fcdb25ae2bf959a3f641071c70da0d609
keypport <keypport@gmail.com>
2024-02-25 05:41:47 +0900

    feat: remove status in feed home layout

--- components/deals/feed-home.layout.tsx
+++ components/deals/feed-home.layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { useSearchParams } from 'next/navigation';
 import DealStatusSelector from './deal-status-selector';
 
 interface Props {
@@ -9,12 +8,10 @@ interface Props {
 }
 
 function FeedHomeLayout({ children }: Props) {
-  const searchParams = useSearchParams();
-  const status = searchParams.get('status');
   return (
     <div>
       <div className="flex justify-end pb-2">
-        <DealStatusSelector status={status} />
+        <DealStatusSelector />
       </div>
       <div className="grid gap-1 grid-cols-1 mx-2 md:mx-0">{children}</div>
     </div>
