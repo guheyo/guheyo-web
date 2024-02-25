e5ce0cffcb162a3ba23fc3a449684ba2160fd906
keypport <keypport@gmail.com>
2024-02-25 18:45:35 +0900

    feat: add useQueryString, refactor useDealStatus using useQueryString

--- hooks/use-deal-status.ts
+++ hooks/use-deal-status.ts
@@ -1,16 +1,6 @@
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useCreateQueryString } from './use-create-query-string';
+import { useQueryString } from './use-query-string';
 
 export const useDealStatus = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  const createQueryString = useCreateQueryString();
-  const searchParams = useSearchParams();
-  const dealStatus = searchParams.get('status');
-
-  if (!dealStatus) {
-    return router.push(`${pathname}?${createQueryString('status', 'OPEN')}`);
-  }
-
-  return dealStatus;
+  const queryString = useQueryString({ key: 'status', defaultValue: 'OPEN' });
+  return queryString;
 };
