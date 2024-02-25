5f0aa5b501cac13d9fb5eb4bd067134ed9fc7d6f
keypport <keypport@gmail.com>
2024-02-25 18:09:11 +0900

    feat: update layout

--- app/policy/layout.tsx
+++ app/policy/layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="max-w-5xl mx-auto mt-8 px-4 md:px-0 pb-12">{children}</div>
-  );
+  return <div className="mt-8 px-4 md:px-0">{children}</div>;
 }
--- components/base/navbar.tsx
+++ components/base/navbar.tsx
@@ -9,8 +9,8 @@ import SearchButton from '../search/search-button';
 export default function Navbar() {
   return (
     <Scrollbar upPosition="top-0" zIndex={50}>
-      <header className="w-full border-b-2 border-dark-600">
-        <div className="max-w-5xl mx-auto flex items-center justify-between px-2 md:px-0 py-4 h-12">
+      <header className="w-full border-b-2 border-dark-600 bg-dark-500">
+        <div className="flex items-center justify-between px-2 md:px-0 py-4 h-12">
           <Link href="/">
             <div className="flex flex-row gap-2 items-center">
               <Image
