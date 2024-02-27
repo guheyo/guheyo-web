08936dfc280fce4de0a39db3078f1063b465bc46
keypport <keypport@gmail.com>
2024-02-28 05:06:15 +0900

    feat: change date to dateString

--- lib/date/date.converter.ts
+++ lib/date/date.converter.ts
@@ -3,15 +3,15 @@ import dayjs from 'dayjs';
 export const convertPeriodToDateString = (period: string | null) => {
   switch (period) {
     case '1w': {
-      return dayjs().subtract(1, 'w').toString();
+      return dayjs().subtract(1, 'w').toDate().toDateString();
     }
     case '1m': {
-      return dayjs().subtract(1, 'M').toString();
+      return dayjs().subtract(1, 'M').toDate().toDateString();
     }
     case '1y': {
-      return dayjs().subtract(1, 'y').toString();
+      return dayjs().subtract(1, 'y').toDate().toDateString();
     }
     default:
-      return dayjs().subtract(100, 'y').toString();
+      return dayjs().subtract(100, 'y').toDate().toDateString();
   }
 };
