54f9057792386e97e1fe2748d5398b5c8177a029
keypport <keypport@gmail.com>
2024-03-05 03:15:32 +0900

    refactor: change return type to data, errors, extensions

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -1,9 +1,15 @@
 import { JwtResponse, RefreshTokensDocument } from '@/generated/graphql';
 import { client } from '@/lib/apollo/client';
+import { FetchResult } from '@apollo/client';
 
-export async function refreshTokens(): Promise<JwtResponse> {
-  const data = await client.mutate({
+export async function refreshTokens(): Promise<FetchResult<any>> {
+  const res = await client.mutate({
     mutation: RefreshTokensDocument,
   });
-  return data.data?.refreshTokens;
+
+  return {
+    data: res.data as JwtResponse,
+    errors: res.errors,
+    extensions: res.extensions,
+  };
 }
