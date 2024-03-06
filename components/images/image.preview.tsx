eca7da431d25e7859c5f8715c4983a8796cea341
keypport <keypport@gmail.com>
2024-03-06 23:05:07 +0900

    feat: update ImagePreviews with UserImage

--- components/images/image.preview.tsx
+++ components/images/image.preview.tsx
@@ -1,10 +1,17 @@
-import { UploadedUserImage } from './image.interfaces';
+import Image from 'next/image';
+import { isMobile } from 'react-device-detect';
+import { UserImage } from '../../lib/image/image.interfaces';
 
-export default function ImagePreview({ file }: UploadedUserImage) {
-  return (
-    <div className="flex flex-col gap-2">
-      <div>{file.name}</div>
-      <div>{file.size}</div>
-    </div>
-  );
+export default function ImagePreview({ image }: { image: UserImage }) {
+  if (image.url)
+    return (
+      <Image
+        src={image.url}
+        alt={image.name}
+        width={isMobile ? 36 : 48}
+        height={isMobile ? 36 : 48}
+        className="rounded"
+      />
+    );
+  return <div>{image.name}</div>;
 }
