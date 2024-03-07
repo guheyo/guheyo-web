28f81e77ac6e2c39363cc61fe2fd42fc7d180b27
keypport <keypport@gmail.com>
2024-03-08 06:33:16 +0900

    feat: add update mutation in api

--- lib/api/demand.ts
+++ lib/api/demand.ts
@@ -2,12 +2,24 @@ import {
   CreateDemandDocument,
   CreateDemandInput,
   CreateDemandMutation,
+  UpdateDemandDocument,
+  UpdateDemandInput,
+  UpdateDemandMutation,
 } from '@/generated/graphql';
 import { client } from '@/lib/apollo/client';
 
 export async function createDemand(input: CreateDemandInput) {
   return client.mutate<CreateDemandMutation>({
     mutation: CreateDemandDocument,
+    variables: {
+      input,
+    },
+  });
+}
+
+export async function updateDemand(input: UpdateDemandInput) {
+  return client.mutate<UpdateDemandMutation>({
+    mutation: UpdateDemandDocument,
     variables: {
       input,
     },
