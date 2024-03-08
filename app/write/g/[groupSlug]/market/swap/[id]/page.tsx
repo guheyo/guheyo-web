9dab3abc529c01ff8d21ee87e0bb888937475956
keypport <keypport@gmail.com>
2024-03-08 19:43:10 +0900

    feat: add demand, swap edit page

---
+++ app/write/g/[groupSlug]/market/swap/[id]/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import EditDealForm from '@/components/write/edit-deal-form';
+import { useFindSwapQuery } from '@/generated/graphql';
+import { parsePrevSwapFormValues } from '@/lib/deal/parse-prev-form-values';
+
+export default function Page({
+  params: { id },
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const { data, loading } = useFindSwapQuery({
+    variables: {
+      id,
+    },
+  });
+
+  if (loading) return <div />;
+  if (!data?.findSwap) return <div />;
+
+  const prevFormValues = parsePrevSwapFormValues(data.findSwap);
+
+  return <EditDealForm prevFormValues={prevFormValues} />;
+}
