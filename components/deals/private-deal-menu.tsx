fcd1d1d5b388fc0f12b419d992266c3ae1381a7f
keypport <113260136+keypport@users.noreply.github.com>
2024-03-13 02:31:39 +0900

    feat: add buttons to change the deal status (#99)
    * feat: move updateDeal to api folder
    
    * feat: update parseDealInput
    
    * feat: add handleChangeDealStatus in private deal menu

--- components/deals/private-deal-menu.tsx
+++ components/deals/private-deal-menu.tsx
@@ -5,28 +5,57 @@ import MenuItem from '@mui/material/MenuItem';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { useRouter } from 'next/navigation';
 import { parseEditLink } from '@/lib/deal/parse-edit-link';
-import { Deal } from '@/lib/deal/deal.types';
+import { Deal, DealStatus } from '@/lib/deal/deal.types';
 import { parseDealBumpLink } from '@/lib/deal/parse-deal-bump-link';
+import { updateDeal } from '@/lib/api/deal';
+import { useDeviceDetect } from '@/hooks/use-device-detect';
+import { parseUpdateDealInput } from '@/lib/deal/parse-update-deal-input';
 
 export default function PrivateDealMenu({
   dealType,
   dealId,
+  authorId,
 }: {
   dealType: Deal;
   dealId: string;
+  authorId: string;
 }) {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const router = useRouter();
+  const device = useDeviceDetect();
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     setAnchorEl(null);
   };
+
+  const handleChangeDealStatus = async (
+    event: React.MouseEvent<HTMLElement>,
+    dealStatus: DealStatus,
+  ) => {
+    event.preventDefault();
+    const updateDealInput = parseUpdateDealInput({
+      dealType,
+      input: {
+        id: dealId,
+        status: dealStatus,
+        source: device,
+      },
+      authorId,
+    });
+    await updateDeal({
+      dealType,
+      updateDealInput,
+    });
+    location.reload();
+  };
+
   const handleEditClick: React.MouseEventHandler = (event) => {
     event.preventDefault();
     router.push(
@@ -67,6 +96,12 @@ export default function PrivateDealMenu({
           'aria-labelledby': 'basic-button',
         }}
       >
+        <MenuItem onClick={(e) => handleChangeDealStatus(e, 'open')}>
+          거래 가능
+        </MenuItem>
+        <MenuItem onClick={(e) => handleChangeDealStatus(e, 'closed')}>
+          거래 완료
+        </MenuItem>
         <MenuItem onClick={handleEditClick}>게시글 수정</MenuItem>
         <MenuItem onClick={handleBumpClick}>끌어올리기</MenuItem>
       </Menu>
