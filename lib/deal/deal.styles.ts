8e2d0902c5be813a1345fdb9b50efacc9a0e418b
keypport <keypport@gmail.com>
2024-03-12 09:14:35 +0900

    style: update up button style

--- lib/deal/deal.styles.ts
+++ lib/deal/deal.styles.ts
@@ -1,5 +1,5 @@
 export const PRICE_UP_BUTTON_STYLE =
-  'p-2 text-light-200 bg-rose-500 hover:bg-rose-400 text-base font-bold rounded-lg';
+  'p-2 text-light-200 bg-star-500 hover:bg-star-400 text-base font-bold rounded-lg';
 
 export const PRICE_DOWN_BUTTON_STYLE =
   'p-2 text-light-200 bg-blue-500 hover:bg-blue-400 text-base font-bold rounded-lg';
