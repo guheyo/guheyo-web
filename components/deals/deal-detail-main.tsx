54e63e7192ab64f3a012a8f50b4dad04ec878817
keypport <keypport@gmail.com>
2024-03-15 07:06:58 +0900

    refactor: simplify using DealDetailMain

---
+++ components/deals/deal-detail-main.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import remarkGfm from 'remark-gfm';
+import { AuthorResponse, ReportResponse } from '@/generated/graphql';
+import { Deal } from '@/lib/deal/deal.types';
+import UserProfilePopper from '../users/user-profile-popper';
+import DealMenu from './deal-menu';
+import ReportsLink from '../reports/reports-link';
+import DealDetailPrice from './deal-detail-price';
+import DealDetailName from './deal-detail-name';
+import DealDetailBumpedAt from './deal-detail-bumped-at';
+
+export default function DealDetailMain({
+  dealType,
+  id,
+  name0,
+  name1,
+  price,
+  description,
+  bumpedAt,
+  author,
+  reports,
+}: {
+  dealType: Deal;
+  id: string;
+  name0: string;
+  name1?: string;
+  price: number;
+  description?: string | null;
+  bumpedAt: Date;
+  author: AuthorResponse;
+  reports: ReportResponse[];
+}) {
+  return (
+    <>
+      <div className="flex flex-row gap-2 md:gap-3 text-sm md:text-base items-center justify-between">
+        <div className="flex flex-row items-center gap-2">
+          <UserProfilePopper
+            user={author}
+            displayAvatar
+            displayUsername
+            displayDM
+            mode="standard"
+          />
+          <DealDetailBumpedAt bumpedAt={bumpedAt} />
+        </div>
+        <div className="mr-[-24px]">
+          <DealMenu dealType={dealType} dealId={id} authorId={author.id} />
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 md:gap-4 mt-4 md:mt-6">
+        <DealDetailName name0={name0} name1={name1} />
+        <DealDetailPrice price={price} />
+      </div>
+      <div className="pt-4">
+        <ReportsLink reports={reports} />
+      </div>
+      <div className="pt-4 text-base md:text-base md:h-fit overflow-y-auto pb-20">
+        {description && (
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {description}
+          </ReactMarkdown>
+        )}
+      </div>
+    </>
+  );
+}
--- components/deals/deal-detail.tsx
+++ components/deals/deal-detail.tsx
@@ -1,21 +1,14 @@
 'use client';
 
-import dayjs from 'dayjs';
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
-import remarkGfm from 'remark-gfm';
 import {
   AuthorResponse,
   ReportResponse,
   UserImageResponse,
 } from '@/generated/graphql';
 import { Deal } from '@/lib/deal/deal.types';
-import UserProfilePopper from '../users/user-profile-popper';
 import ImageSlider from '../base/image-slider';
 import PostDetail from '../posts/post-detail';
-import DealMenu from './deal-menu';
-import ReportsLink from '../reports/reports-link';
-import DealDetailPrice from './deal-detail-price';
-import DealDetailName from './deal-detail-name';
+import DealDetailMain from './deal-detail-main';
 
 export default function DealDetail({
   dealType,
@@ -48,37 +41,17 @@ export default function DealDetail({
         </div>
 
         <div className="flex-none line-break w-full md:w-[45%] px-4 md:px-0 py-4 md:py-0">
-          <div className="flex flex-row gap-2 md:gap-3 text-sm md:text-base items-center justify-between">
-            <div className="flex flex-row items-center gap-2">
-              <UserProfilePopper
-                user={author}
-                displayAvatar
-                displayUsername
-                displayDM
-                mode="standard"
-              />
-              <div className="justify-self-end text-[10px] md:text-xs text-gray-400">
-                {dayjs(bumpedAt).fromNow()}
-              </div>
-            </div>
-            <div className="mr-[-24px]">
-              <DealMenu dealType={dealType} dealId={id} authorId={author.id} />
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 md:gap-4 mt-4 md:mt-6">
-            <DealDetailName name0={name0} name1={name1} />
-            <DealDetailPrice price={price} />
-          </div>
-          <div className="pt-4">
-            <ReportsLink reports={reports} />
-          </div>
-          <div className="pt-4 text-base md:text-base md:h-[30rem] overflow-y-auto pb-20">
-            {description && (
-              <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                {description}
-              </ReactMarkdown>
-            )}
-          </div>
+          <DealDetailMain
+            dealType={dealType}
+            id={id}
+            name0={name0}
+            name1={name1}
+            price={price}
+            description={description}
+            bumpedAt={bumpedAt}
+            author={author}
+            reports={reports}
+          />
         </div>
       </PostDetail>
     );
@@ -86,37 +59,17 @@ export default function DealDetail({
   return (
     <PostDetail>
       <div className="flex-none line-break w-full md:w-[90%] px-4 md:px-0 py-4 md:py-0">
-        <div className="flex flex-row gap-2 md:gap-3 text-sm md:text-base items-center justify-between">
-          <div className="flex flex-row items-center gap-2">
-            <UserProfilePopper
-              user={author}
-              displayAvatar
-              displayUsername
-              displayDM
-              mode="standard"
-            />
-            <div className="justify-self-end text-[10px] md:text-xs text-gray-400">
-              {dayjs(bumpedAt).fromNow()}
-            </div>
-          </div>
-          <div className="mr-[-24px]">
-            <DealMenu dealType={dealType} dealId={id} authorId={author.id} />
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 md:gap-4 mt-4 md:mt-6">
-          <DealDetailName name0={name0} name1={name1} />
-          <DealDetailPrice price={price} />
-        </div>
-        <div className="pt-4">
-          <ReportsLink reports={reports} />
-        </div>
-        <div className="pt-4 text-base md:text-base md:h-fit overflow-y-auto pb-20">
-          {description && (
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-              {description}
-            </ReactMarkdown>
-          )}
-        </div>
+        <DealDetailMain
+          dealType={dealType}
+          id={id}
+          name0={name0}
+          name1={name1}
+          price={price}
+          description={description}
+          bumpedAt={bumpedAt}
+          author={author}
+          reports={reports}
+        />
       </div>
     </PostDetail>
   );
