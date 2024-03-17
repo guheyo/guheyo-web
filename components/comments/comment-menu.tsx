3a21cf4a8319541c10fdb3602015b436c44efe17
keypport <keypport@gmail.com>
2024-03-17 20:40:44 +0900

    feat: add comment-content, comment-menu, report-comment-title

---
+++ components/comments/comment-menu.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+export default function CommentMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setAnchorEl(null);
+  };
+
+  const handleEditClick: React.MouseEventHandler = (event) => {
+    event.preventDefault();
+    // TODO
+  };
+
+  const handleDeleteClick: React.MouseEventHandler = async (event) => {
+    event.preventDefault();
+    // TODO
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <MoreHorizIcon className="text-xl md:text-2xl text-light-200" />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleEditClick}>수정</MenuItem>
+        <MenuItem onClick={handleDeleteClick}>삭제</MenuItem>
+      </Menu>
+    </div>
+  );
+}
