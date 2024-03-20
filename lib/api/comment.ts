700de4cad2262adadc600d3d7d5638b247091097
keypport <keypport@gmail.com>
2024-03-21 01:19:37 +0900

    feat: remove writeQuery in updateComment

--- lib/api/comment.ts
+++ lib/api/comment.ts
@@ -2,9 +2,6 @@ import {
   CreateCommentDocument,
   CreateCommentInput,
   CreateCommentMutation,
-  FindCommentDocument,
-  FindCommentQuery,
-  FindCommentQueryVariables,
   UpdateCommentDocument,
   UpdateCommentInput,
   UpdateCommentMutation,
@@ -25,30 +22,6 @@ export async function updateComment(input: UpdateCommentInput) {
     mutation: UpdateCommentDocument,
     variables: {
       input,
-    },
-    update: (cache, { data }) => {
-      if (!data?.updateComment) return;
-
-      const comment = data.updateComment;
-      let refId;
-      if (comment.type === 'post') {
-        refId = comment.postId;
-      } else if (comment.type === 'report') {
-        refId = comment.reportId;
-      } else if (comment.type === 'auction') {
-        refId = comment.auctionId;
-      } else {
-        return;
-      }
-
-      cache.writeQuery({
-        query: FindCommentDocument,
-        variables: {
-          type: data.updateComment.type,
-          refId,
-        },
-        data: data?.updateComment,
-      });
     },
   });
 }
