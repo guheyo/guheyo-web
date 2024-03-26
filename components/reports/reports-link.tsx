c02ad65ab87f38f37f6f1b0425656aff10f4bbbf
keypport <keypport@gmail.com>
2024-03-27 06:29:28 +0900

    feat: add effect in reports link

--- components/reports/reports-link.tsx
+++ components/reports/reports-link.tsx
@@ -33,19 +33,17 @@ export default function ReportsLink({
     <button
       type="button"
       onClick={handleClick}
-      className="flex flex-col gap-1 text-xs md:text-sm bg-pink-50 w-full p-3 rounded"
+      className="flex flex-col gap-1 text-xs md:text-sm w-full p-3 rounded"
     >
-      <div className="flex flex-row gap-1 items-center text-red-500 font-semibold">
-        <div>
-          <WarningIcon fontSize="inherit" />
-        </div>
+      <div className="  flex flex-row gap-2 items-center text-red-500 font-semibold">
+        <WarningIcon fontSize="medium" />
         {`해당 게시글에 ${reportCount}건의 신고 기록이 있어요`}
       </div>
-      <div className="flex flex-row gap-1 items-center text-yellow-500 font-semibold">
-        <div>
-          <NewReleasesIcon fontSize="inherit" />
+      <div className="flex flex-row gap-2 items-center text-yellow-500 font-semibold">
+        <div className="animate-spin">
+          <NewReleasesIcon fontSize="medium" />
         </div>
-        {`이에 대한 작성자의 소명서 ${reportCommentCount}개를 확인해 주세요`}
+        {`작성자의 소명서 ${reportCommentCount}개를 확인해 주세요`}
       </div>
     </button>
   );
