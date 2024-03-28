954c832be1e93eca79c7d943aba4a62604ccf551
keypport <keypport@gmail.com>
2024-03-29 02:39:40 +0900

    feat: remove padding style from userProfile

--- app/setting/profile/layout.tsx
+++ app/setting/profile/layout.tsx
@@ -19,7 +19,9 @@ function Layout({ children }: Props) {
     <div className="flex px-2 pb-2 pt-4 md:pt-6 justify-center">
       <div className="flex flex-col gap-8 max-w-xl w-full">
         <div className="text-base md:text-lg text-light-200 font-bold px-2 md:px-0">
-          <UserProfile username={jwtPayload.username} />
+          <div className="px-2 pb-6">
+            <UserProfile username={jwtPayload.username} />
+          </div>
           <SettingProfileNavbar />
         </div>
         {children}
--- app/user/[username]/(home)/layout.tsx
+++ app/user/[username]/(home)/layout.tsx
@@ -13,7 +13,9 @@ function Layout({ children, params }: Props) {
   return (
     <div className="pb-4 md:pb-6 px-2 md:px-0">
       <div className="pt-6">
-        <UserProfile username={params.username} />
+        <div className="px-2 pb-6">
+          <UserProfile username={params.username} />
+        </div>
         <UserHomeNavbar username={params.username} />
       </div>
       {children}
