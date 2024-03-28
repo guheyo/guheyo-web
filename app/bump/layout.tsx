7587f560d863b7389be8d3254baf7f8291bf28aa
keypport <keypport@gmail.com>
2024-03-29 02:32:44 +0900

    feat: move px style from deal-bump form to bump layout

--- app/bump/layout.tsx
+++ app/bump/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 function Layout({ children }: { children: ReactNode }) {
-  return <div className="pt-0 md:pt-12 pb-12">{children}</div>;
+  return <div className="pt-0 md:pt-12 pb-12 px-2 md:px-0">{children}</div>;
 }
 
 export default Layout;
