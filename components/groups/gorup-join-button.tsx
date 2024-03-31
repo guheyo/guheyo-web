aca9f4be5257db828f9af107601c500f8a40d4c2
keypport <keypport@gmail.com>
2024-04-01 03:01:49 +0900

    feat: change button to loading button

--- components/groups/gorup-join-button.tsx
+++ components/groups/gorup-join-button.tsx
@@ -1,25 +1,29 @@
 'use client';
 
+import { LoadingButton } from '@mui/lab';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function GroupJoinButton({ slug }: { slug: string }) {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   const join = (): void => {
+    setLoading(true);
     router.push(`g/${slug}`);
   };
 
   return (
     <div className="inline-flex items-center">
-      <button
+      <LoadingButton
         type="submit"
+        loading={loading}
         className="bg-star-500 hover:bg-star-400 text-sm font-bold px-2 py-1.5 rounded text-light-200"
         name={`${slug} 그룹에 입장하기`}
         onClick={() => join()}
       >
         입장
-      </button>
+      </LoadingButton>
     </div>
   );
 }
