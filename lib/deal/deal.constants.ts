3055d8278f8098f596b6449f3b32e57c5b9e2863
keypport <keypport@gmail.com>
2024-04-05 06:04:02 +0900

    feat: add isPostingLimitExceeded error

--- lib/deal/deal.constants.ts
+++ lib/deal/deal.constants.ts
@@ -44,3 +44,5 @@ export const DEAL_EDIT_SUBMIT_BUTTON_NAME = '수정 완료';
 export const DEAL_AUTO_SAVE_INTERVAL_MS = 60 * 1000;
 
 export const DAILY_DEAL_POSTING_LIMIT = 5;
+
+export const POSTING_LIMIT_EXCEEDED_MESSAGE = 'posting limit exceeded';
