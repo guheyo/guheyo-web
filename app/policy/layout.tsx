4de9aad443afa3faab6b2aa07d338acbcad8139d
keypport <keypport@gmail.com>
2024-04-05 20:11:28 +0900

    feat: set pb in policy layout

--- app/policy/layout.tsx
+++ app/policy/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="mt-8 px-4 md:px-0">{children}</div>;
+  return <div className="mt-8 px-4 md:px-0 pb-10">{children}</div>;
 }
