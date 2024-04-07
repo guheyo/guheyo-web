05da753fbb778e0b94df219cf3dc4c8184cee41c
keypport <keypport@gmail.com>
2024-04-07 18:13:51 +0900

    feat: move detail page from user folder to root folder

--- app/user/[username]/(deal-detail)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-
-function Layout({ children }: { children: ReactNode }) {
-  return <div className="pt-0 md:pt-12">{children}</div>;
-}
-
-export default Layout;
