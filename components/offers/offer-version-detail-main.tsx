bae2f921a52b6bb0c7b007307f68faa7e4518c39
keypport <keypport@gmail.com>
2024-04-20 04:13:40 +0900

    feat: change post.content to offer.content

--- components/offers/offer-version-detail-main.tsx
+++ components/offers/offer-version-detail-main.tsx
@@ -57,9 +57,9 @@ export default function OfferVersionDetailMain({
         </div>
       </div>
       <div className="pt-4 text-base md:text-base md:h-fit overflow-y-auto pb-20">
-        {post.content && (
+        {offer.content && (
           <ReactMarkdown remarkPlugins={[remarkGfm]}>
-            {post.content}
+            {offer.content}
           </ReactMarkdown>
         )}
       </div>
