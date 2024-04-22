caef49cc24e46b593297083d5b6dc8b8e44e6803
keypport <keypport@gmail.com>
2024-04-22 17:05:19 +0900

    feat: add SearchResultCardLayout, refactor using InfoFeedLayout

---
+++ components/search/search-result-card.layout.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+
+export default function SearchCardResultLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <div className="p-3 bg-dark-400 rounded-lg text-light-200 ">{children}</div>
+  );
+}
