98703fe5c7653a6cb2a7727125aac89e1ed8abf7
keypport <keypport@gmail.com>
2024-04-23 04:53:57 +0900

    feat: add toggleMenu in layout, move metadata from layout to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-'use client';
-
 import HomeFooter from '@/components/footers/home-footer';
 import GroupFeed from '@/components/groups/group-feed';
+
+export const metadata = {
+  title: '구해요',
+  description: '디스코드 거래 장터, 구해요',
+};
 
 function Page() {
   return (
