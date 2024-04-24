407591b78bcc2f072f8d75dc25491f318b81c499
keypport <keypport@gmail.com>
2024-04-25 05:38:10 +0900

    feat: add user/[userId]/offer/[offerId] page and layout

---
+++ app/write/review/user/[userId]/offer/[offerId]/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import UserReviewForm from '@/components/user-review/user-review-form';
+import { useFindOfferQuery, useFindTagsQuery } from '@/generated/graphql';
+
+export default function Page({
+  params: { userId, offerId },
+}: {
+  params: {
+    userId: string;
+    offerId: string;
+  };
+}) {
+  const { data: offerData, loading: offerLoading } = useFindOfferQuery({
+    variables: {
+      id: offerId,
+    },
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const { data: tagsData, loading: tagsLoading } = useFindTagsQuery();
+
+  const offer = offerData?.findOffer;
+  const tags = tagsData?.findTags;
+
+  if (offerLoading || tagsLoading) return <div />;
+  if (!offer || !tags) return <div />;
+
+  return (
+    <UserReviewForm
+      offerId={offerId}
+      title={offer.post.title}
+      reviewedUserId={userId}
+      tags={tags}
+    />
+  );
+}
