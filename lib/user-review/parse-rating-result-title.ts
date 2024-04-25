5026cf97ee54866beb8342ef875711807554a97a
keypport <keypport@gmail.com>
2024-04-25 20:01:04 +0900

    feat: update rating result title

--- lib/user-review/parse-rating-result-title.ts
+++ lib/user-review/parse-rating-result-title.ts
@@ -1,6 +1,6 @@
 export const parseRatingResultTitle = ({ rating }: { rating: number }) => {
   if (rating === 1) {
-    return `거래하며 겪은 불쾌함과 사기, 기만 행위를 비매너 후기로 공유해 주세요`;
+    return `비매너 행위를 당하셨나요?`;
   }
   if (rating === 2) {
     return `좋았던 점을 모두 표현해 주세요`;
