44a8eea8e72c7545b057511c76d453bb732c0ad2
keypport <113260136+keypport@users.noreply.github.com>
2024-04-27 04:10:23 +0900

    feat: search user and link to write userReviewForm page (#161)
    * feat: change OfferCheckboxFormValues to CheckboxFormValues
    
    * feat: add user-checkbox-results
    
    * feat: add SearchInput, UserCheckboxResults in write/review/offer/[offerId] page

--- components/users/user-checkbox-results.tsx
+++ components/users/user-checkbox-results.tsx
@@ -2,69 +2,58 @@
 
 import { useRef } from 'react';
 import { Mocks } from '@/components/mock/mock';
-import { useInfiniteOfferFeed } from '@/hooks/use-infinite-offer-feed';
-import OfferPreview from '@/components/offers/offer-preview';
-import {
-  FindOffersOrderByArgs,
-  FindOffersWhereArgs,
-} from '@/interfaces/offer.interfaces';
 import { Checkbox } from '@mui/material';
 import tailwindConfig from '@/tailwind.config';
 import { Control, useController } from 'react-hook-form';
+import { useInfiniteUsers } from '@/hooks/use-infinite-users';
+import {
+  FindUsersOrderByArgs,
+  FindUsersWhereArgs,
+} from '@/interfaces/user.interfaces';
 import { CheckboxFormValues } from '@/lib/search/search.types';
+import UserPreview from './user-preview';
 
 const {
   theme: { colors },
 } = tailwindConfig;
 
-function OfferCheckboxResults({
+function UserCheckboxResults({
   where,
   orderBy,
   keyword,
-  type,
-  status,
-  distinct,
+  userIdToExclude,
   control,
   handleOfferSelection,
 }: {
-  where: FindOffersWhereArgs;
-  orderBy?: FindOffersOrderByArgs;
+  where: FindUsersWhereArgs;
+  orderBy?: FindUsersOrderByArgs;
   keyword?: string;
-  type: 'text' | 'thumbnail';
-  status?: string;
-  distinct: boolean;
+  userIdToExclude?: string;
   control: Control<CheckboxFormValues>;
   handleOfferSelection: (seletedId: string) => void;
 }) {
   const ref = useRef<HTMLDivElement>(null);
   const { field } = useController({ name: 'selectedId', control });
-  const { loading, data } = useInfiniteOfferFeed({
+  const { loading, data } = useInfiniteUsers({
     ref,
-    where: {
-      businessFunction: where?.businessFunction,
-      status,
-      userId: where?.userId,
-    },
+    where,
     orderBy: {
-      bumpedAt: orderBy?.bumpedAt || 'desc',
-      price: orderBy?.price,
+      createdAt: orderBy?.createdAt || 'asc',
     },
     keyword,
-    distinct,
     take: 12,
   });
 
-  const handleClick = (offerId: string) => {
-    handleOfferSelection(offerId);
+  const handleClick = (userId: string) => {
+    handleOfferSelection(userId);
   };
 
   if (loading) return <Mocks length={12} height={72} color="bg-dark-400" />;
-  if (!data?.findOfferPreviews) return <div />;
+  if (!data?.findUsers) return <div />;
 
-  const edges = data.findOfferPreviews.edges.filter((edge) =>
-    status ? edge.node.status === status : true,
+  const edges = data.findUsers.edges.filter(
+    (edge) => edge.node.id !== userIdToExclude,
   );
-
   return (
     <>
       {edges.map((edge) => (
@@ -75,7 +64,12 @@ function OfferCheckboxResults({
             onChange={() => handleClick(edge.node.id)}
           />
           <div className="w-full">
-            <OfferPreview offer={edge.node} type={type} />
+            <UserPreview
+              key={edge.node.id}
+              username={edge.node.username}
+              avatarURL={edge.node.avatarURL}
+              about={edge.node.about}
+            />
           </div>
         </div>
       ))}
@@ -84,4 +78,4 @@ function OfferCheckboxResults({
   );
 }
 
-export default OfferCheckboxResults;
+export default UserCheckboxResults;
