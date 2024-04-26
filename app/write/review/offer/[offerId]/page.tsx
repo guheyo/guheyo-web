ab7876af15f8b349c8707ec08bf6c6086ba50569
keypport <keypport@gmail.com>
2024-04-27 04:05:06 +0900

    feat: add SearchInput, UserCheckboxResults in write/review/offer/[offerId] page

--- app/write/review/offer/[offerId]/page.tsx
+++ app/write/review/offer/[offerId]/page.tsx
@@ -1,5 +1,90 @@
-export default function Page() {
-  // TODO
-  // Find a review target user.
-  return <div>offer reivew</div>;
+'use client';
+
+import { AuthContext } from '@/components/auth/auth.provider';
+import NextDialog from '@/components/auth/next-dialog';
+import TextFeedLayout from '@/components/posts/text-feed.layout';
+import SearchInput from '@/components/search/search-input';
+import { DEBOUNCE } from '@/components/search/search.constants';
+import UserCheckboxResults from '@/components/users/user-checkbox-results';
+import { CheckboxFormValues } from '@/lib/search/search.types';
+import { parseUserReviewTargetOfferFormLink } from '@/lib/user-review/parse-user-review-target-offer-form-link';
+import { useRouter } from 'next/navigation';
+import { MouseEventHandler, useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDebounce } from 'use-debounce';
+
+export default function Page({
+  params: { offerId },
+}: {
+  params: {
+    offerId: string;
+  };
+}) {
+  const { jwtPayload } = useContext(AuthContext);
+  const [text, setText] = useState('');
+  const [keyword] = useDebounce(text, DEBOUNCE);
+  const router = useRouter();
+
+  const { setValue, getValues, control } = useForm<CheckboxFormValues>({
+    defaultValues: {
+      selectedId: '',
+    },
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setText(event.target.value);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent): void => {
+    // Do nothing
+  };
+
+  const handleOfferSelection = (seletedId: string) => {
+    setValue('selectedId', seletedId);
+  };
+
+  const handleAuthorization: MouseEventHandler = (e) => {
+    const { selectedId } = getValues();
+    if (!selectedId) return;
+
+    router.push(
+      parseUserReviewTargetOfferFormLink({ userId: selectedId, offerId }),
+    );
+  };
+
+  const handleUnAuthorization: MouseEventHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const where = {};
+
+  return (
+    <div className="grid max-w-3xl w-full">
+      <SearchInput
+        text={text}
+        setText={setText}
+        placeholder="매너 평가할 멤버를 선택해 주세요"
+        handleKeyDown={handleKeyDown}
+        handleChange={handleChange}
+      />
+      <div className="pt-4 overflow-y-scroll">
+        <div style={{ maxHeight: '75vh' }}>
+          <TextFeedLayout>
+            <UserCheckboxResults
+              where={where}
+              keyword={keyword}
+              userIdToExclude={jwtPayload?.id}
+              control={control}
+              handleOfferSelection={handleOfferSelection}
+            />
+          </TextFeedLayout>
+        </div>
+      </div>
+      <div className="pt-4" />
+      <NextDialog
+        onAuthorization={handleAuthorization}
+        onUnAuthorization={handleUnAuthorization}
+      />
+    </div>
+  );
 }
