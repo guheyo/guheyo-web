98ad869c9de7d4580d05b484359c78756b96032b
keypport <keypport@gmail.com>
2024-04-28 03:36:24 +0900

    feat: add user review preview header, footer

---
+++ components/user-review/user-review-preview-footer.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { UserReviewPreviewResponse } from '@/generated/graphql';
+import PostAddons from '../posts/post-addons';
+
+interface Props {
+  userReview: UserReviewPreviewResponse;
+}
+
+export default function UserReviewPreviewFooter({ userReview }: Props) {
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="flex flex-row gap-2 items-center text-xs md:text-sm">
+        {userReview.post.tags.map((tag) => (
+          <div
+            key={tag.id}
+            className={`rounded px-1 py-0.5 ${
+              userReview.rating === 1
+                ? 'bg-rose-800 text-light-200'
+                : 'bg-green-800 text-light-200'
+            }`}
+          >
+            {tag.name}
+          </div>
+        ))}
+      </div>
+      <div className="absolute bottom-4 right-4 md:right-5">
+        <PostAddons
+          createdAt={userReview.createdAt}
+          reportCount={userReview.post.reportCount}
+          reportCommentCount={userReview.post.reportCommentCount}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ components/user-review/user-review-preview-header.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { UserReviewPreviewResponse } from '@/generated/graphql';
+import UserProfileRedirectButton from '../users/user-profile-redirect-button';
+
+interface Props {
+  userReview: UserReviewPreviewResponse;
+}
+
+export default function UserReviewPreviewHeader({ userReview }: Props) {
+  return (
+    <div className="flex flex-row justify-between items-center">
+      <div className="grid grid-cols-12 gap-2 items-center text-xs md:text-sm">
+        <div className="col-span-12 flex flex-row gap-1 items-center">
+          <UserProfileRedirectButton
+            user={userReview.post.user}
+            displayAvatar
+            displayUsername
+            mode="light"
+          />
+          <div className="text-dark-200 pr-2 md:pr-2">님이</div>
+          <UserProfileRedirectButton
+            user={userReview.reviewedUser}
+            displayAvatar
+            displayUsername
+            mode="light"
+          />
+          <div className="text-dark-200">님께</div>
+        </div>
+        <div className="col-span-12 text-dark-200">
+          {userReview.post.title} 거래 후기를 남겼어요
+        </div>
+      </div>
+      {/* Menu */}
+    </div>
+  );
+}
