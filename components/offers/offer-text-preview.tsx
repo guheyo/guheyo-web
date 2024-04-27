406587eef92b2937719b1f3252bf90a1845a0de6
keypport <keypport@gmail.com>
2024-04-27 15:49:48 +0900

    refactor: move common components to OfferPreviewHeader, OfferPreviewFooter

--- components/offers/offer-text-preview.tsx
+++ components/offers/offer-text-preview.tsx
@@ -1,13 +1,10 @@
 'use client';
 
 import Link from 'next/link';
-import { OfferStatus } from '@/lib/offer/offer.types';
 import { parseOfferDetailLink } from '@/lib/offer/parse-offer-detail-link';
 import { OfferPreviewResponse } from '@/generated/graphql';
-import PostPreviewTitle from '../posts/post-preview-title';
-import OfferMenu from './offer-menu';
-import PostAddons from '../posts/post-addons';
-import OfferPreviewPrice from './offer-preview-price';
+import OfferPreviewFooter from './offer-preview-footer';
+import OfferPreviewHeader from './offer-preview-header';
 
 interface Props {
   offer: OfferPreviewResponse;
@@ -22,37 +19,9 @@ export default function OfferTextPreview({ offer }: Props) {
         })}
         className="w-full text-start"
       >
-        <div className="grid grid-cols-1 gap-0">
-          <div className="flex flex-row justify-between items-center pt-4">
-            <div className="w-fit">
-              <PostPreviewTitle title={offer.post.title} />
-            </div>
-            <div className="h-8">
-              <OfferMenu
-                offerId={offer.id}
-                postId={offer.post.id}
-                offerStatus={offer.status as OfferStatus}
-                userId={offer.post.user.id}
-                privateOnly
-                reportCount={offer.post.reportCount}
-                reportCommentCount={offer.post.reportCommentCount}
-                archivedAt={offer.post.archivedAt}
-              />
-            </div>
-          </div>
-          <div className="flex flex-row justify-between items-center pb-4">
-            <OfferPreviewPrice
-              offerStatus={offer.status as OfferStatus}
-              totalPrice={offer.totalPrice}
-            />
-            <div className="absolute bottom-4 right-4 md:right-5">
-              <PostAddons
-                createdAt={offer.bumpedAt}
-                reportCount={offer.post.reportCount}
-                reportCommentCount={offer.post.reportCommentCount}
-              />
-            </div>
-          </div>
+        <div className="grid grid-cols-1 gap-0 pt-4 pb-4">
+          <OfferPreviewHeader offer={offer} />
+          <OfferPreviewFooter offer={offer} />
         </div>
       </Link>
     </div>
