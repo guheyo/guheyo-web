1687b36128e0e96c1b6b455ba470b5560ef874c6
keypport <keypport@gmail.com>
2024-04-28 04:43:45 +0900

    feat: add user-review-text-preview

---
+++ components/user-review/user-review-text-preview.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Link from 'next/link';
+import { UserReviewPreviewResponse } from '@/generated/graphql';
+import { parseUserReviewDetailLink } from '@/lib/user-review/parse-user-review-detail-link';
+import UserReviewPreviewHeader from './user-review-preview-header';
+import UserReviewPreviewFooter from './user-review-preview-footer';
+
+interface Props {
+  userReview: UserReviewPreviewResponse;
+}
+
+export default function UserReviewTextPreview({ userReview }: Props) {
+  return (
+    <div className="relative overflow-hidden line-break bg-dark-400 px-4 md:px-5 rounded-lg">
+      <Link
+        href={parseUserReviewDetailLink({
+          slug: userReview.post.slug!,
+        })}
+        className="w-full text-start"
+      >
+        <div className="grid grid-cols-1 gap-2 pt-4 pb-4">
+          <UserReviewPreviewHeader userReview={userReview} />
+          <UserReviewPreviewFooter userReview={userReview} />
+        </div>
+      </Link>
+    </div>
+  );
+}
