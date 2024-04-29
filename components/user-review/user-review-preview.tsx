ef7a42e82cf1e09325150bd99b724479048f0757
keypport <keypport@gmail.com>
2024-04-29 20:25:13 +0900

    feat: move user-review-text-preview to user-review-preview

--- components/user-review/user-review-preview.tsx
+++ components/user-review/user-review-preview.tsx
@@ -1,20 +1,31 @@
 'use client';
 
+import Link from 'next/link';
 import { UserReviewPreviewResponse } from '@/generated/graphql';
-import UserReviewTextPreview from './user-review-text-preview';
+import { parseUserReviewDetailLink } from '@/lib/user-review/parse-user-review-detail-link';
+import UserReviewPreviewHeader from './user-review-preview-header';
+import UserReviewPreviewFooter from './user-review-preview-footer';
+import UserReviewPreviewContent from './user-review-preview-content';
 
 interface Props {
-  type: 'text' | 'thumbnail';
   userReview: UserReviewPreviewResponse;
 }
 
-export default function UserReviewPreview({ type, userReview }: Props) {
-  switch (type) {
-    case 'text': {
-      return <UserReviewTextPreview userReview={userReview} />;
-    }
-    default: {
-      return <div />;
-    }
-  }
+export default function UserReviewPreview({ userReview }: Props) {
+  return (
+    <div className="relative overflow-hidden line-break bg-dark-400 px-4 md:px-5 rounded-lg">
+      <Link
+        href={parseUserReviewDetailLink({
+          slug: userReview.post.slug!,
+        })}
+        className="w-full text-start"
+      >
+        <div className="grid grid-cols-1 gap-2 pt-4 pb-4">
+          <UserReviewPreviewHeader userReview={userReview} />
+          <UserReviewPreviewContent userReview={userReview} />
+          <UserReviewPreviewFooter userReview={userReview} />
+        </div>
+      </Link>
+    </div>
+  );
 }
