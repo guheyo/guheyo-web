9007bbc1736f0be0d2c971c3e8945d231e677f18
keypport <keypport@gmail.com>
2024-04-29 16:55:55 +0900

    feat: add thumbnailSize in Thumbnail component props

--- components/base/thumbnail.tsx
+++ components/base/thumbnail.tsx
@@ -5,13 +5,22 @@ import Image from 'next/image';
 export default function Thumbnail({
   url,
   name,
+  thumbnailSize,
 }: {
   url: string;
   name: string;
+  thumbnailSize: 'small' | 'large';
 }) {
+  if (thumbnailSize === 'large') {
+    return (
+      <div className="flex justify-center items-center relative overflow-hidden w-fit h-32 md:h-48 lg:h-52 rounded-md">
+        <Image src={url} alt={name} width={320} height={320} />
+      </div>
+    );
+  }
   return (
-    <div className="flex justify-center items-center relative overflow-hidden w-fit h-32 md:h-48 lg:h-52 rounded-md">
-      <Image src={url} alt={name} width={320} height={320} />
+    <div className="flex justify-center items-center relative overflow-hidden w-fit h-16 rounded-md">
+      <Image src={url} alt={name} width={72} height={64} />
     </div>
   );
 }
