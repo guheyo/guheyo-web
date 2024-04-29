0e8c089ac5b20fb54e066f69484343c0389e4838
keypport <keypport@gmail.com>
2024-04-30 05:06:22 +0900

    feat: update group review feed layout

--- app/g/[groupSlug]/(community)/review/layout.tsx
+++ app/g/[groupSlug]/(community)/review/layout.tsx
@@ -11,7 +11,7 @@ function Layout({ children }: Props) {
   return (
     <div>
       <MannerTagsNavbar />
-      <div className="grid gap-1 grid-cols-1">{children}</div>
+      {children}
     </div>
   );
 }
