3cce03c372cf407731b0087725c194585365b224
keypport <keypport@gmail.com>
2024-04-30 03:15:04 +0900

    feat: update comment output layout

--- components/comments/comment-output.tsx
+++ components/comments/comment-output.tsx
@@ -2,38 +2,52 @@
 
 import { CRUD } from '@/lib/crud/crud.types';
 import { parseCommentDate } from '@/lib/comment/parse-comment-date';
+import { AuthorResponse } from '@/generated/graphql';
+import { useDeviceDetect } from '@/hooks/use-device-detect';
 import CommentMenu from './comment-menu';
+import UserProfileRedirectButton from '../users/user-profile-redirect-button';
 
 export default function CommentOutput({
+  user,
   content,
   createdAt,
   updatedAt,
   displayMenu,
   handleMenuClick,
 }: {
+  user: AuthorResponse;
   content?: string;
   createdAt?: Date;
   updatedAt?: Date;
   displayMenu: boolean;
   handleMenuClick: (mode: CRUD) => void;
 }) {
+  const device = useDeviceDetect();
+
   return (
-    <div className="flex flex-col gap-1 items-start w-full">
-      <div className="flex justify-between w-full">
-        <div className="text-light-200 font-light text-xs md:text-sm">
-          {content || '무응답'}
+    <div className="flex flex-row gap-4 items-center">
+      <UserProfileRedirectButton
+        user={user}
+        displayAvatar
+        displayUsername={false}
+        fontSize={device === 'mobile' ? 'text-sm' : 'text-base'}
+      />
+      <div className="flex flex-col gap-0 w-full">
+        <div className="flex justify-between items-center text-xs md:text-sm">
+          <div className="flex flex-row gap-2">
+            <div className="text-light-200">{user.username}</div>
+            <div>{parseCommentDate({ createdAt, updatedAt })}</div>
+          </div>
+          <div className="mr-[-18px]">
+            {displayMenu && (
+              <CommentMenu
+                allowDelete={false}
+                handleMenuClick={handleMenuClick}
+              />
+            )}
+          </div>
         </div>
-        <div className="mr-[-12px]">
-          {displayMenu && (
-            <CommentMenu
-              allowDelete={false}
-              handleMenuClick={handleMenuClick}
-            />
-          )}
-        </div>
-      </div>
-      <div className="text-dark-200 text-xs md:text-sm">
-        {parseCommentDate({ createdAt, updatedAt })}
+        <div className="flex text-sm md:text-base text-gray-400">{content}</div>
       </div>
     </div>
   );
