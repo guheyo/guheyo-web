0ca154eb80fad26c0556351cb980b7384c9050ce
keypport <keypport@gmail.com>
2024-04-30 15:36:37 +0900

    feat: update layout using PostDetailMainLayout

--- components/offers/offer-detail.tsx
+++ components/offers/offer-detail.tsx
@@ -4,29 +4,27 @@ import { OfferResponse } from '@/generated/graphql';
 import ImageSlider from '../base/image-slider';
 import PostDetail from '../posts/post-detail';
 import OfferDetailMain from './offer-detail-main';
+import PostDetailMainLayout from '../posts/post-detail-main.layout';
 
 export default function OfferDetail({ offer }: { offer: OfferResponse }) {
   if (offer.post.images.length > 0)
     return (
       <PostDetail>
-        <div className="w-full md:w-[45%]">
-          <ImageSlider
-            images={offer.post.images}
-            sizes="h-[360px] md:h-[524px]"
-          />
-        </div>
-
-        <div className="flex-none line-break w-full md:w-[45%] px-4 md:px-0 py-4 md:py-0">
+        <ImageSlider
+          images={offer.post.images}
+          sizes="h-[360px] md:h-[500px]"
+        />
+        <PostDetailMainLayout>
           <OfferDetailMain offer={offer} />
-        </div>
+        </PostDetailMainLayout>
       </PostDetail>
     );
 
   return (
     <PostDetail>
-      <div className="flex-none line-break w-full md:w-[90%] px-4 md:px-0 py-4 md:py-0">
+      <PostDetailMainLayout>
         <OfferDetailMain offer={offer} />
-      </div>
+      </PostDetailMainLayout>
     </PostDetail>
   );
 }
