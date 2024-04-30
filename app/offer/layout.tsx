33fe166d951becc3c84092a2d068ba8339760f15
keypport <keypport@gmail.com>
2024-04-30 15:37:17 +0900

    feat: resize max width

--- app/offer/layout.tsx
+++ app/offer/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 function Layout({ children }: { children: ReactNode }) {
-  return <div className="pt-0 md:pt-12">{children}</div>;
+  return <div className="pt-0 md:pt-0 mx-auto max-w-2xl">{children}</div>;
 }
 
 export default Layout;
