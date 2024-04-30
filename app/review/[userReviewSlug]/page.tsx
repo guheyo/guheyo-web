85253b91242d514d98239f73151b57824a9c9bb7
keypport <keypport@gmail.com>
2024-04-30 15:38:28 +0900

    feat: update review detail page layout

--- app/review/[userReviewSlug]/page.tsx
+++ app/review/[userReviewSlug]/page.tsx
@@ -34,7 +34,7 @@ function Page({
   };
 
   return (
-    <div className="flex flex-col gap-12">
+    <div className="flex flex-col gap-0 md:gap-0">
       <UserReviewDetail userReview={userReview} />
       <div className="px-4 md:px-2">
         <CommentFeed where={where} orderBy={orderBy} />
