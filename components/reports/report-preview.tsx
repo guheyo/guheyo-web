419798abb92c88ddb821a0ae709e03b6c733bdad
keypport <keypport@gmail.com>
2024-05-01 19:21:36 +0900

    feat: add PostCreatedAt

--- components/reports/report-preview.tsx
+++ components/reports/report-preview.tsx
@@ -1,8 +1,8 @@
-import dayjs from 'dayjs';
 import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
 import Link from 'next/link';
 import { parseReportTypeName } from '@/lib/report/parse-report-type-name';
 import { ReportType } from '@/lib/report/report.types';
+import PostCreatedAt from '../posts/post-created-at';
 
 export default function ReportPreview({
   reportId,
@@ -19,8 +19,13 @@ export default function ReportPreview({
 }) {
   return (
     <div className="flex flex-col gap-2 rounded bg-dark-400 p-4">
-      <div className="text-gray-300 text-xs md:text-sm font-semibold">
-        {`[${parseReportTypeName(type)} 신고] ${reason}`}
+      <div className="flex flex-row items-center text-gray-300 text-xs md:text-sm font-semibold">
+        <div className="flex-1">
+          {`[${parseReportTypeName(type)} 신고] ${reason}`}
+        </div>
+        <div className="flex-none">
+          <PostCreatedAt createdAt={createdAt} />
+        </div>
       </div>
       <div className="flex flex-col gap-1">
         <div className="flex justify-between w-full">
@@ -32,9 +37,6 @@ export default function ReportPreview({
               <HistoryEduIcon />
             </Link>
           </div>
-        </div>
-        <div className="text-dark-200 text-xs md:text-sm">
-          {dayjs(createdAt).fromNow()}
         </div>
       </div>
     </div>
