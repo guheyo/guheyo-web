40f38f581621226918015aa8d0c76c3641c93ffa
keypport <keypport@gmail.com>
2024-05-05 06:11:06 +0900

    feat: add ReactionBar in User review detail page

--- app/review/[userReviewSlug]/page.tsx
+++ app/review/[userReviewSlug]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import CommentFeed from '@/components/comments/comment-feed';
+import ReactionBar from '@/components/reaction/reaction-bar';
 import UserReviewDetail from '@/components/user-review/user-review-detail';
 import { useFindUserReviewQuery } from '@/generated/graphql';
 import {
@@ -36,6 +37,12 @@ function Page({
   return (
     <div className="flex flex-col gap-16 md:gap-16">
       <UserReviewDetail userReview={userReview} />
+      <div className="w-full px-4 md:px-0">
+        <ReactionBar
+          postId={userReview.post.id}
+          reactionSummaries={userReview.post.reactions}
+        />
+      </div>
       <div className="px-4 md:px-0">
         <CommentFeed where={where} orderBy={orderBy} />
       </div>
