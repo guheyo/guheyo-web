a62a6d719d2395ee98f3f7e29dfb8c0668db4d76
keypport <keypport@gmail.com>
2024-05-10 20:27:57 +0900

    feat: update layout

--- components/reports/report-header.tsx
+++ components/reports/report-header.tsx
@@ -22,21 +22,19 @@ export default function ReportHeader({
 
   return (
     <button type="button" onClick={handleClick}>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 items-start">
         <UserProfileRedirectButton
           user={author}
           displayAvatar
           displayUsername
           fontSize={device === 'mobile' ? 'text-base' : 'text-lg'}
         />
-        <div className="grid grid-cols-1 gap-1 justify-items-start">
-          <div className="flex flex-row gap-2">
-            <div className="flex-1 text-gray-300 text-base font-semibold">
-              {title}
-            </div>
-            <div className="felx-none text-dark-200 text-sm font-normal">
-              최근 수정 : {dayjs(updatedAt).fromNow()}
-            </div>
+        <div className="flex flex-row gap-2 items-center">
+          <div className="flex-1 text-gray-300 text-base font-semibold">
+            {title}
+          </div>
+          <div className="felx-none text-dark-200 text-sm font-normal">
+            최근 수정 : {dayjs(updatedAt).fromNow()}
           </div>
         </div>
       </div>
