df2afa8b1a8a56fecf90965ae8b64b1d19d092b9
keypport <keypport@gmail.com>
2024-05-10 17:48:17 +0900

    feat: update layout

--- components/version/recent-version-link.tsx
+++ components/version/recent-version-link.tsx
@@ -10,12 +10,10 @@ export default function RecentVersionLink({
   slug: string;
 }) {
   return (
-    <div className="flex flex-col gap-1 text-xs md:text-sm bg-dark-500 w-fit p-3 rounded">
-      <div className="">
-        {`[주의] 문서의 이전 버전(${dayjs(versionCreatedAt).format(
-          'YYYY-MM-DD HH:mm:ss',
-        )} 수정)을 보고 있어요`}
-      </div>
+    <div className="flex flex-col gap-1 text-xs md:text-sm w-full text-red-400">
+      {`[주의] 문서의 이전 버전(${dayjs(versionCreatedAt).format(
+        'YYYY-MM-DD HH:mm:ss',
+      )} 수정)을 보고 있어요`}
       <Link
         href={parseOfferDetailLink({
           slug,
