18e7665194bfbbdd221ed63c0bff2fb511a17c4c
keypport <keypport@gmail.com>
2024-05-10 17:47:48 +0900

    feat: remove report comment count in reports link

--- components/reports/reports-link.tsx
+++ components/reports/reports-link.tsx
@@ -1,17 +1,14 @@
 import WarningIcon from '@mui/icons-material/Warning';
-import NewReleasesIcon from '@mui/icons-material/NewReleases';
 import { useRouter } from 'next/navigation';
 import { parseReportFeedLink } from '@/lib/report/parse-report-feed-link';
 import { ReportFeedType } from '@/lib/report/report.types';
 
 export default function ReportsLink({
   reportCount,
-  reportCommentCount,
   type,
   slug,
 }: {
   reportCount: number;
-  reportCommentCount: number;
   type: ReportFeedType;
   slug: string;
 }) {
@@ -31,15 +28,11 @@ export default function ReportsLink({
     <button
       type="button"
       onClick={handleClick}
-      className="flex flex-col gap-1 text-xs md:text-sm w-full p-3 rounded"
+      className="flex flex-col gap-1 text-xs md:text-sm w-full"
     >
-      <div className="  flex flex-row gap-2 items-center text-red-500 font-semibold">
+      <div className="  flex flex-row gap-2 items-center text-red-400">
         <WarningIcon fontSize="medium" />
         {`해당 게시글에 ${reportCount}건의 신고 기록이 있어요`}
-      </div>
-      <div className="flex flex-row gap-2 items-center text-yellow-500 font-semibold">
-        <NewReleasesIcon fontSize="medium" />
-        {`작성자의 소명서 ${reportCommentCount}개를 확인해 주세요`}
       </div>
     </button>
   );
