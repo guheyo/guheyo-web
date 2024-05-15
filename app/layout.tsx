d43b3d17ec92b68fe10fc286fc3b5b929535e838
keypport <keypport@gmail.com>
2024-05-15 23:29:25 +0900

    feat: add pb-6 in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         <meta property="og:image:height" content="400" />
       </head>
       <body
-        className={`${notoSansKr.className} text-dark-200 bg-dark-500 line-break`}
+        className={`${notoSansKr.className} text-dark-200 bg-dark-500 line-break pb-6`}
       >
         <Providers>
           <Navbar handleMenuToggle={handleMenuToggle} />
