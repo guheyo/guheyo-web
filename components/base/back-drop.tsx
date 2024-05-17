c6b14dd9c4dca88e5706eb712d5b8960af0780b4
keypport <113260136+keypport@users.noreply.github.com>
2024-05-18 04:15:25 +0900

    feat: add backgroundColor, opacity as props in BackDrop (#193)
    * feat: update pt
    
    * feat: ad backgroundColor, opacity as props in BackDrop

--- components/base/back-drop.tsx
+++ components/base/back-drop.tsx
@@ -1,15 +1,24 @@
 import { MouseEventHandler } from 'react';
 
 export default function BackDrop({
+  backgroundColor,
+  opacity,
   handleBackdropClick,
   handleToggle,
 }: {
+  backgroundColor?: string;
+  opacity?: number;
   handleBackdropClick: MouseEventHandler;
   handleToggle: () => void;
 }) {
   return (
     <div
-      className="fixed inset-0 z-50 bg-black opacity-50"
+      className="fixed inset-0 z-50"
+      style={{
+        backgroundColor: backgroundColor || 'black',
+        opacity: opacity || 0,
+        cursor: 'default',
+      }}
       onClick={handleBackdropClick}
       onKeyDown={(event) => {
         if (event.key === 'Escape') {
@@ -19,7 +28,6 @@ export default function BackDrop({
       role="button" // Add role="button" to make it accessible for screen reader users
       aria-label="Toggle Menu"
       tabIndex={0} // Ensure element can receive keyboard focus
-      style={{ cursor: 'default' }}
     />
   );
 }
