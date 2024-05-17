7b0a5a485dcd5848a29e29b397286e15e25d01f1
keypport <keypport@gmail.com>
2024-05-18 02:52:25 +0900

    feat: add post detail addons

---
+++ components/posts/post-detail-addons.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import ShareButton from '../share/share-button';
+
+export default function PostDetailAddons({
+  commentCount,
+}: {
+  commentCount?: number;
+}) {
+  return (
+    <div className="flex gap-2 items-center text-base md:text-lg text-gray-300 font-bold">
+      {commentCount !== undefined && `댓글 ${commentCount}개`}
+      <ShareButton />
+    </div>
+  );
+}
