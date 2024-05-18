db1d94c72d128c7d047448331403fd1f394d7f09
keypport <keypport@gmail.com>
2024-05-18 21:51:30 +0900

    feat: add metadata in userReview page

---
+++ app/review/[userReviewSlug]/layout.tsx
@@ -0,0 +1,16 @@
+import { parseSlugFromURL } from '@/lib/url/parse-slug-from-url';
+import React from 'react';
+
+export async function generateMetadata({ params }: { params: any }) {
+  const { userReviewSlug } = params;
+  const slug = parseSlugFromURL(userReviewSlug);
+
+  return {
+    title: `${slug} | 구해요`,
+    Description: `${slug} 거래 후기`,
+  };
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return children;
+}
