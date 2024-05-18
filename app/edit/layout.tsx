63be403f6a05cf61a862b93ec7fe8443db4eb9f5
keypport <keypport@gmail.com>
2024-05-18 22:20:49 +0900

    feat: edit, policy metadata

--- app/edit/layout.tsx
+++ app/edit/layout.tsx
@@ -1,6 +1,9 @@
-'use client';
-
 import { ReactNode } from 'react';
+
+export const metadata = {
+  title: '포스트 수정 | 구해요',
+  description: '포스트를 수정해 보세요',
+};
 
 interface Props {
   children: ReactNode;
