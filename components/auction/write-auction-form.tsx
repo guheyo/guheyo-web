eb69de92923da473636fa97e276343ebfd54637a
keypport <keypport@gmail.com>
2024-05-21 21:55:44 +0900

    feat: add auction form, write auction form

---
+++ components/auction/write-auction-form.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { SubmitHandler } from 'react-hook-form';
+import { deleteUserImage } from '@/lib/api/user-image';
+import { GroupResponse } from '@/generated/graphql';
+import { useContext } from 'react';
+import { parseTempOfferFormKey } from '@/lib/offer/parse-temp-offer-form-key';
+import { useRouter } from 'next/navigation';
+import secureLocalStorage from 'react-secure-storage';
+import { createAuction } from '@/lib/api/auction';
+import { AuthContext } from '../auth/auth.provider';
+import { AuctionFormValues } from './auction.interfaces';
+import parseCreateAuctionInput from './parse-create-auction-input';
+import AuctionForm from './auction-form';
+
+export default function WriteAuctionForm({ group }: { group: GroupResponse }) {
+  const { jwtPayload } = useContext(AuthContext);
+  const router = useRouter();
+  const localStorageKey = parseTempOfferFormKey({
+    userId: jwtPayload?.id || 'ghost',
+    groupSlug: group.slug,
+  });
+
+  const handleSubmitValid: SubmitHandler<AuctionFormValues> = async (
+    values,
+  ) => {
+    if (!jwtPayload) return;
+
+    const input = parseCreateAuctionInput({
+      auctionFormValues: values,
+    });
+    await createAuction(input);
+    secureLocalStorage.removeItem(localStorageKey);
+    router.back();
+  };
+
+  const handleOnClickImagePreviewCallback = async (imageId: string) => {
+    await deleteUserImage(imageId);
+  };
+
+  return (
+    <AuctionForm
+      localStorageKey={localStorageKey}
+      userId={jwtPayload?.id}
+      group={group}
+      handleSubmitValid={handleSubmitValid}
+      onClickImagePreviewCallback={handleOnClickImagePreviewCallback}
+    />
+  );
+}
