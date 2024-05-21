3e97f30be7d76f23c62f58d59af1eabba83f61c4
keypport <keypport@gmail.com>
2024-05-22 00:28:26 +0900

    feat: move auction types, interfaces into lib/auction folder

---
+++ lib/auction/auction.interfaces.ts
@@ -0,0 +1,22 @@
+import { SortOrder } from '@/types/sort.types';
+
+export interface FindAuctionsWhereArgs {
+  // post
+  groupId?: string;
+
+  categoryId?: string;
+
+  userId?: string;
+
+  status?: string;
+
+  createdAt?: {
+    gt: string;
+  };
+}
+
+export interface FindAuctionsOrderByArgs {
+  price?: SortOrder;
+
+  createdAt?: SortOrder;
+}
