371ed7eea16de4d3f821cd7f251bc2e31aec2667
keypport <keypport@gmail.com>
2024-05-23 07:05:59 +0900

    feat: update price button styles

--- lib/offer/offer.styles.ts
+++ lib/offer/offer.styles.ts
@@ -1,5 +1,5 @@
 export const PRICE_UP_BUTTON_STYLE =
-  'p-2 text-gray-300 bg-green-700 hover:bg-green-600 text-base font-bold rounded-lg';
+  'p-2 text-gray-300 bg-green-700 hover:bg-green-600 text-sm md:text-base font-bold rounded-lg';
 
 export const PRICE_DOWN_BUTTON_STYLE =
-  'p-2 text-gray-300 bg-red-700 hover:bg-red-600 text-base font-bold rounded-lg';
+  'p-2 text-gray-300 bg-red-700 hover:bg-red-600 text-sm md:text-base font-bold rounded-lg';
