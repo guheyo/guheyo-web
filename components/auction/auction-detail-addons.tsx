b6928cbdc133810f6ce47bbee9647506c6212b60
keypport <keypport@gmail.com>
2024-05-23 06:10:29 +0900

    feat: add bidCount in auction response

--- components/auction/auction-detail-addons.tsx
+++ components/auction/auction-detail-addons.tsx
@@ -11,7 +11,7 @@ export default function AuctionDetailAddons({
 }) {
   return (
     <div className="flex gap-4 items-center text-base md:text-lg text-gray-300 font-bold">
-      <div>{`입찰 ${bidCount}개`}</div>
+      <div>{`입찰 ${bidCount}건`}</div>
       <div>{`댓글 ${commentCount}개`}</div>
       <ShareButton />
     </div>
