752f442fe19b32b87d72da4b4e3fb7d5cdcdfddd
keypport <keypport@gmail.com>
2024-05-27 05:28:36 +0900

    feat: remove outside div tag

--- components/posts/post-detail.tsx
+++ components/posts/post-detail.tsx
@@ -2,7 +2,6 @@
 
 import { ReactNode } from 'react';
 import { UserImageResponse } from '@/generated/graphql';
-import PostDetailMainLayout from './post-detail-main.layout';
 import ImageSlider from '../base/image-slider';
 
 export default function PostDetail({
@@ -13,13 +12,13 @@ export default function PostDetail({
   postDetailMain: ReactNode;
 }) {
   return (
-    <div className="w-full flex flex-col gap-6 md:gap-6 text-gray-300 rounded-none md:rounded-lg">
-      {images?.length ? (
-        <ImageSlider images={images} sizes="h-[360px] md:h-[500px]" />
-      ) : (
-        <div className="pt-0" />
-      )}
-      <PostDetailMainLayout>{postDetailMain}</PostDetailMainLayout>
-    </div>
+    <>
+      <div className="pb-4">
+        {!!images?.length && (
+          <ImageSlider images={images} sizes="h-[360px] md:h-[500px]" />
+        )}
+      </div>
+      {postDetailMain}
+    </>
   );
 }
