924fa95ebecc3d259c80cbc80d09fa7cbd8f30a3
keypport <keypport@gmail.com>
2024-05-27 13:17:24 +0900

    feat: change sticky header components to AuctionDetailStickyHeader in AuctionDetailMain

--- components/auction/auction-detail-main.tsx
+++ components/auction/auction-detail-main.tsx
@@ -8,11 +8,8 @@ import { AuctionStatus } from '@/lib/auction/auction.types';
 import ReportsLink from '../reports/reports-link';
 import UserProfileRedirectButton from '../users/user-profile-redirect-button';
 import AuctionDetailName from './auction-detail-name';
-import AuctionDetailPrice from './auction-detail-price';
-import AuctionCountdown from './auction-count-down';
 import PostDetailDate from '../posts/post-detail-date';
-import AuctionBidCount from './auction-bid-count';
-import AuctionCommentCount from './auction-comment-count';
+import AuctionDetailStickyHeader from './auction-detail-sticky-header';
 
 export default function AuctionDetailMain({
   auction,
@@ -29,46 +26,41 @@ export default function AuctionDetailMain({
 
   return (
     <>
-      <div className="flex flex-row justify-between items-center bg-zinc-600 rounded-lg py-3 px-6 text-sm md:text-base">
-        <AuctionCountdown targetDate={auction.extendedEndDate} />
-        <AuctionDetailPrice
-          auctionStatus={auction.status as AuctionStatus}
-          currentBidPrice={currentBidPrice}
-        />
-        <div className="hidden md:flex">
-          <AuctionBidCount bidCount={bidCount} />
+      <AuctionDetailStickyHeader
+        auction={auction}
+        currentBidPrice={currentBidPrice}
+        bidCount={bidCount}
+        commentCount={commentCount}
+      />
+      <div className="px-4 md:px-0">
+        <div className="flex flex-row gap-2 md:gap-3 text-sm md:text-base items-start justify-between">
+          <div className="flex flex-row items-center gap-2">
+            <UserProfileRedirectButton
+              user={auction.post.user}
+              displayAvatar
+              displayUsername
+              fontSize={device === 'mobile' ? 'text-base' : 'text-lg'}
+            />
+            <PostDetailDate date={auction.createdAt} />
+          </div>
+          <div className="h-8">{/* AuctionMenu */}</div>
         </div>
-        <div className="hidden md:flex">
-          <AuctionCommentCount commentCount={commentCount} />
-        </div>
-      </div>
-      <div className="flex flex-row gap-2 md:gap-3 text-sm md:text-base items-start justify-between mt-4 md:mt-6">
-        <div className="flex flex-row items-center gap-2">
-          <UserProfileRedirectButton
-            user={auction.post.user}
-            displayAvatar
-            displayUsername
-            fontSize={device === 'mobile' ? 'text-base' : 'text-lg'}
+        <div className="flex flex-col gap-4 md:gap-4 mt-4 md:mt-6">
+          {auction.post.reportCount > 0 && (
+            <ReportsLink reportCount={auction.post.reportCount} />
+          )}
+          <AuctionDetailName
+            auctionStatus={auction.status as AuctionStatus}
+            title={auction.post.title}
           />
-          <PostDetailDate date={auction.createdAt} />
         </div>
-        <div className="h-8">{/* AuctionMenu */}</div>
-      </div>
-      <div className="flex flex-col gap-4 md:gap-4 mt-4 md:mt-6">
-        {auction.post.reportCount > 0 && (
-          <ReportsLink reportCount={auction.post.reportCount} />
-        )}
-        <AuctionDetailName
-          auctionStatus={auction.status as AuctionStatus}
-          title={auction.post.title}
-        />
-      </div>
-      <div className="pt-8 text-sm md:text-base md:h-fit overflow-y-auto text-dark-100">
-        {auction.content && (
-          <ReactMarkdown remarkPlugins={[remarkGfm]}>
-            {auction.content}
-          </ReactMarkdown>
-        )}
+        <div className="pt-8 text-sm md:text-base md:h-fit overflow-y-auto text-dark-100">
+          {auction.content && (
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              {auction.content}
+            </ReactMarkdown>
+          )}
+        </div>
       </div>
     </>
   );
