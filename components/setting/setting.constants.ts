c5a013b3b038c29956a1525eccee61164b67d2e1
keypport <keypport@gmail.com>
2024-05-27 17:50:14 +0900

    feat: disable discord profile setting page

--- components/setting/setting.constants.ts
+++ components/setting/setting.constants.ts
@@ -1,6 +1,6 @@
 export const SETTING_PROFILE_OPTIONS = [
   { value: 'about', label: '내 소개' },
-  { value: 'discord', label: '디스코드 프로필' },
+  // { value: 'discord', label: '디스코드 프로필' },
   // { value: 'naver', label: '네이버 인증' },
   { value: 'kakao', label: '카카오 인증' },
 ];
