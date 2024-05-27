1c66624a71fc6c7351ef4b504eaee7082dc19736
keypport <keypport@gmail.com>
2024-05-27 17:12:54 +0900

    feat: return dummy kakao account for guest member

--- app/setting/profile/kakao/page.tsx
+++ app/setting/profile/kakao/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { AuthContext } from '@/components/auth/auth.provider';
+import DummyKakaoAccount from '@/components/socials/dummy-kakao-account';
 import KakaoAccount from '@/components/socials/kakao-account';
 import { useFindMyUserQuery } from '@/generated/graphql';
 import { useContext } from 'react';
@@ -12,8 +13,7 @@ export default function Page() {
   });
 
   if (loading || userLoading) return <div />;
-  if (!jwtPayload) return <div />;
-  if (!data?.findMyUser) return <div />;
+  if (!jwtPayload || !data?.findMyUser) return <DummyKakaoAccount />;
   const user = data.findMyUser;
 
   return <KakaoAccount socialAccounts={user.socialAccounts} />;
