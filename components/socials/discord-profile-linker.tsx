0c89afe60b79a901c03347a5a719a98c2fca3997
keypport <113260136+keypport@users.noreply.github.com>
2024-05-27 18:15:58 +0900

    feat: add kakao auth alert (#215)
    * feat: update font size
    
    * feat: update about page placeholder
    
    * feat: disable discord profile setting page
    
    * feat: add hasSocialAccount
    
    * feat: add parse social account setting page link
    
    * feat: add KakaoAuthAlertDialog
    
    * feat: add KakaoAlertDialog in LoggedInUserAlertDialog

--- components/socials/discord-profile-linker.tsx
+++ components/socials/discord-profile-linker.tsx
@@ -30,7 +30,7 @@ export default function DiscordProfileLinker() {
     <div className="flex flex-col gap-8">
       <div className="flex flex-row gap-3">
         <SocialLogo provider="discord" width={24} height={24} />
-        <div className="text-lg text-gray-300">디스코드 프로필</div>
+        <div className="text-gray-300">디스코드 프로필</div>
       </div>
       <div className="flex flex-col gap-2 text-gray-300">
         <div className="text-base text-gray-300">업데이트할 정보</div>
