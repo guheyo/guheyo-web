3dc298b85a5638ef8b5d5ae2d22c0f7f3e8da0c6
keypport <keypport@gmail.com>
2024-05-27 19:35:51 +0900

    feat: remove pb-6 in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         <meta property="og:image:height" content="400" />
       </head>
       <body
-        className={`${notoSansKr.className} text-dark-200 bg-dark-500 line-break pb-6`}
+        className={`${notoSansKr.className} text-dark-200 bg-dark-500 line-break`}
       >
         <Providers>
           <Navbar handleMenuToggle={handleMenuToggle} />
@@ -50,7 +50,7 @@ export default function RootLayout({
             isMenuOpen={isMenuOpen}
             handleMenuToggle={handleMenuToggle}
           />
-          <div ref={feedRef} className="w-full md:max-w-2xl mx-auto pb-0">
+          <div ref={feedRef} className="w-full md:max-w-2xl mx-auto">
             {children}
           </div>
         </Providers>
