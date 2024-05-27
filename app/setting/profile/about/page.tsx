0903d3616ac300c7851fdc23f9ffdf7039ed4bdb
keypport <keypport@gmail.com>
2024-05-27 17:11:36 +0900

    feat: add message for guest member

--- app/setting/profile/about/page.tsx
+++ app/setting/profile/about/page.tsx
@@ -8,7 +8,12 @@ export default function Page() {
   const { jwtPayload, loading } = useContext(AuthContext);
 
   if (loading) return <div />;
-  if (!jwtPayload) return <div />;
+  if (!jwtPayload)
+    return (
+      <div className="text-gray-300">
+        이곳은 로그인하면 설정할 수 있는 소개 페이지에요
+      </div>
+    );
 
   return <SettingAbout userId={jwtPayload.id} />;
 }
