aba51de3ad5c28a6f35e7dc03622a2bbe5797c6f
keypport <keypport@gmail.com>
2024-05-28 12:26:02 +0900

    feat: update naver account messages, styles

--- components/socials/naver-account.tsx
+++ components/socials/naver-account.tsx
@@ -20,24 +20,23 @@ export default function NaverAccount({
     return (
       <div className="flex flex-col gap-4">
         <IconText>
-          <SocialLogo provider="naver" width={24} height={24} />
-          <div className="text-red-400">네이버 미인증</div>
+          <SocialLogo provider="naver" width={20} height={20} />
+          <div className="text-red-400 text-sm md:text-base">네이버 미인증</div>
         </IconText>
-        <div className="text-gray-400">
-          <div>다중 계정 악용 방지를 위해 네이버 인증이 필요해요</div>
+        <div className="text-gray-400 text-xs md:text-sm">
           <div className="pt-4">
-            로그인이 성공하면 구해요 전용 식별값이 생성돼요
+            인증이 완료되면 구해요 전용 식별값이 생성돼요
           </div>
-          <div>ex. 123456789</div>
-          <div className="pt-4 text-gray-400">
-            *구해요 식별값 이외의 개인정보는 수집되지 않아요
+          <div>ex. 12345</div>
+          <div className="pt-4">
+            중복 가입 확인을 위한 식별값 이외의 정보는 수집하지 않아요
           </div>
           <div className="pt-8 text-gray-300 font-semibold">
             시작하기 버튼을 눌러 인증을 완료해 주세요
           </div>
           <button
             type="submit"
-            className="mt-4 bg-green-500 hover:bg-green-400 text-sm font-bold p-2 rounded text-light-200"
+            className="mt-4 bg-green-500 hover:bg-green-400 text-xs md:text-sm font-bold p-2 rounded text-light-200"
             onClick={() => signIn(router, 'naver')}
           >
             네이버 로그인
@@ -48,8 +47,8 @@ export default function NaverAccount({
 
   return (
     <IconText>
-      <SocialLogo provider="naver" width={24} height={24} />
-      <div className="text-gray-300">네이버 인증 완료</div>
+      <SocialLogo provider="naver" width={20} height={20} />
+      <div className="text-gray-300 text-sm md:text-base">네이버 인증 완료</div>
     </IconText>
   );
 }
