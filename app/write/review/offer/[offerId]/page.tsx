b7933ccbe316242f8676aacaf2bb5c074fb02e24
keypport <keypport@gmail.com>
2024-06-04 00:44:52 +0900

    refactor: move search component to SearchUsersCheckbox

--- app/write/review/offer/[offerId]/page.tsx
+++ app/write/review/offer/[offerId]/page.tsx
@@ -1,16 +1,9 @@
 'use client';
 
-import { AuthContext } from '@/components/auth/auth.provider';
-import NextDialog from '@/components/auth/next-dialog';
-import SearchInput from '@/components/search/search-input';
-import { DEBOUNCE } from '@/components/search/search.constants';
-import UserCheckboxResults from '@/components/users/user-checkbox-results';
-import { CheckboxFormValues } from '@/lib/search/search.types';
-import { useSearchQuery } from '@/lib/search/use-search-query';
+import SearchUsersCheckbox from '@/components/search/search-users-checkbox';
 import { parseUserReviewTargetOfferFormLink } from '@/lib/user-review/parse-user-review-target-offer-form-link';
 import { useRouter } from 'next/navigation';
-import { MouseEventHandler, Suspense, useContext } from 'react';
-import { useForm } from 'react-hook-form';
+import { MouseEventHandler, Suspense } from 'react';
 
 export default function Page({
   params: { offerId },
@@ -20,29 +13,8 @@ export default function Page({
   };
 }) {
   const router = useRouter();
-  const { jwtPayload } = useContext(AuthContext);
-  const { text, setText, keyword } = useSearchQuery(DEBOUNCE);
 
-  const { setValue, getValues, control } = useForm<CheckboxFormValues>({
-    defaultValues: {
-      selectedId: '',
-    },
-  });
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setText(event.target.value);
-  };
-
-  const handleKeyDown = (e: KeyboardEvent): void => {
-    // Do nothing
-  };
-
-  const handleOfferSelection = (seletedId: string) => {
-    setValue('selectedId', seletedId);
-  };
-
-  const handleAuthorization: MouseEventHandler = (e) => {
-    const { selectedId } = getValues();
+  const handleAuthorization = (selectedId: string) => {
     if (!selectedId) return;
 
     router.push(
@@ -54,33 +26,13 @@ export default function Page({
     e.preventDefault();
   };
 
-  const where = {};
-
   return (
-    <div className="grid w-full">
-      <SearchInput
-        text={text}
-        setText={setText}
+    <Suspense>
+      <SearchUsersCheckbox
         placeholder="어떤 멤버와 거래했나요?"
-        handleKeyDown={handleKeyDown}
-        handleChange={handleChange}
+        handleAuthorization={handleAuthorization}
+        handleUnAuthorization={handleUnAuthorization}
       />
-      <div className="pt-4 overflow-y-scroll max-h-[75vh] grid gap-2 grid-cols-1">
-        <Suspense>
-          <UserCheckboxResults
-            where={where}
-            keyword={keyword}
-            userIdToExclude={jwtPayload?.id}
-            control={control}
-            handleOfferSelection={handleOfferSelection}
-          />
-        </Suspense>
-      </div>
-      <div className="pt-4" />
-      <NextDialog
-        onAuthorization={handleAuthorization}
-        onUnAuthorization={handleUnAuthorization}
-      />
-    </div>
+    </Suspense>
   );
 }
---
+++ components/search/search-users-checkbox.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { AuthContext } from '@/components/auth/auth.provider';
+import NextDialog from '@/components/auth/next-dialog';
+import SearchInput from '@/components/search/search-input';
+import { DEBOUNCE } from '@/components/search/search.constants';
+import UserCheckboxResults from '@/components/users/user-checkbox-results';
+import { CheckboxFormValues } from '@/lib/search/search.types';
+import { useSearchQuery } from '@/lib/search/use-search-query';
+import { MouseEventHandler, useContext } from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function SearchUsersCheckbox({
+  placeholder,
+  handleAuthorization,
+  handleUnAuthorization,
+}: {
+  placeholder: string;
+  handleAuthorization: (selectedId: string) => void;
+  handleUnAuthorization: MouseEventHandler;
+}) {
+  const { jwtPayload } = useContext(AuthContext);
+  const { text, setText, keyword } = useSearchQuery(DEBOUNCE);
+
+  const { setValue, getValues, control } = useForm<CheckboxFormValues>({
+    defaultValues: {
+      selectedId: '',
+    },
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setText(event.target.value);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent): void => {
+    // Do nothing
+  };
+
+  const handleCheckboxClick = (seletedId: string) => {
+    setValue('selectedId', seletedId);
+  };
+
+  const where = {};
+
+  return (
+    <div className="grid w-full">
+      <SearchInput
+        text={text}
+        setText={setText}
+        placeholder={placeholder}
+        handleKeyDown={handleKeyDown}
+        handleChange={handleChange}
+      />
+      <div className="pt-4 overflow-y-scroll max-h-[75vh] grid gap-2 grid-cols-1">
+        <UserCheckboxResults
+          where={where}
+          keyword={keyword}
+          userIdToExclude={jwtPayload?.id}
+          control={control}
+          handleCheckboxClick={handleCheckboxClick}
+        />
+      </div>
+      <div className="pt-4" />
+      <NextDialog
+        onAuthorization={() => handleAuthorization(getValues().selectedId)}
+        onUnAuthorization={handleUnAuthorization}
+      />
+    </div>
+  );
+}
