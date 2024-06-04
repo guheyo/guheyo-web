34f2ce8681f8472a3f7629ccffc1e21f10a219ec
keypport <keypport@gmail.com>
2024-06-05 04:06:58 +0900

    feat: update user home options

--- components/users/user-home.constants.ts
+++ components/users/user-home.constants.ts
@@ -1,13 +1,13 @@
 export const PUBLIC_USER_HOME_OPTIONS = [
-  { value: 'auction', label: '경매글' },
-  { value: 'offer', label: '거래글' },
+  { value: 'offer', label: '거래' },
+  { value: 'auction', label: '경매' },
   { value: 'review', label: '거래 후기' },
   { value: 'report', label: '신고 기록' },
 ];
 
 export const PRIVATE_USER_HOME_OPTIONS = [
-  { value: 'auction', label: '내 경매글' },
-  { value: 'offer', label: '내 거래글' },
-  { value: 'review', label: '내 거래 후기' },
-  { value: 'report', label: '내 신고 기록' },
+  { value: 'offer', label: '내 거래' },
+  { value: 'auction', label: '경매' },
+  { value: 'review', label: '거래 후기' },
+  { value: 'report', label: '신고 기록' },
 ];
