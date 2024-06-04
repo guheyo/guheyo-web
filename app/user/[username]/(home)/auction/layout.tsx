e50d4a8140abd773299f0f6d56fadeb1f85d13e6
keypport <keypport@gmail.com>
2024-06-05 03:22:17 +0900

    feat: add auction page, layout

---
+++ app/user/[username]/(home)/auction/layout.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import PrivateUserAuctionFeedLayout from '@/components/auction/private-user-auction-feed.layout';
+import PublicUserAuctionFeedLayout from '@/components/auction/public-user-auction-feed.layout';
+import { AuthContext } from '@/components/auth/auth.provider';
+import { ReactNode, useContext } from 'react';
+
+interface Props {
+  children: ReactNode;
+  params: {
+    username: string;
+  };
+}
+
+function Layout({ children, params }: Props) {
+  const { jwtPayload } = useContext(AuthContext);
+
+  if (jwtPayload?.username !== params.username)
+    return (
+      <PublicUserAuctionFeedLayout>{children}</PublicUserAuctionFeedLayout>
+    );
+  return (
+    <PrivateUserAuctionFeedLayout>{children}</PrivateUserAuctionFeedLayout>
+  );
+}
+
+export default Layout;
