96a1a1d6638e56b85ef01abb3460c4f82364197b
keypport <keypport@gmail.com>
2024-06-04 21:39:53 +0900

    feat: add ToggleAuctionInteractionItem

---
+++ components/auction/toggle-auction-interaction-item.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { useBidInput } from '@/lib/bid/use-bid-input';
+import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
+
+export default function ToggleAuctionInteractionItem() {
+  const { isBidMode, toggleBidMode } = useBidInput();
+
+  return (
+    <button type="button" onClick={toggleBidMode}>
+      <div className="flex flex-row">
+        <SwapHorizIcon className="opacity-50" />
+        <div className="font-semibold">{isBidMode ? '댓글' : '입찰'}</div>
+      </div>
+    </button>
+  );
+}
