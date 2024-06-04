1534a2ac09aa285f003ea310f74366daf9a83608
keypport <keypport@gmail.com>
2024-06-05 03:21:25 +0900

    feat: private, public user auction feed layout

---
+++ components/auction/public-user-auction-feed.layout.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { AUCTION_STATUS_OPTIONS } from '@/lib/auction/auction.constants';
+import AuctionStatusNavbar from './auction-status-navbar';
+
+interface Props {
+  children: ReactNode;
+}
+
+function PublicUserAuctionFeedLayout({ children }: Props) {
+  return (
+    <div>
+      <div className="flex flex-col justify-start pb-4">
+        <AuctionStatusNavbar options={AUCTION_STATUS_OPTIONS} />
+      </div>
+      <div className="grid gap-1 grid-cols-1">{children}</div>
+    </div>
+  );
+}
+
+export default PublicUserAuctionFeedLayout;
