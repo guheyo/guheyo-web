4732ff41cec6eee952e8eb955d9245025c82ab69
keypport <keypport@gmail.com>
2024-06-05 21:39:13 +0900

    feat: add ImagesInput in CommentInput

--- components/comments/comment-card.tsx
+++ components/comments/comment-card.tsx
@@ -8,18 +8,24 @@ import {
   useState,
 } from 'react';
 import { CommentValues } from '@/lib/comment/comment.types';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
 import { CRUD } from '@/lib/crud/crud.types';
 import { TextFieldProps } from '@mui/material';
 import { AuthorResponse, ReactionResponse } from '@/generated/graphql';
 import { useDeviceDetect } from '@/hooks/use-device-detect';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { MOBILE_FILE_INPUT_LABEL_STYLE } from '@/lib/input/input.styles';
+import parseUploadedImages from '@/lib/image/parse-uploaded-user-images';
+import uploadAndSaveImages from '@/lib/image/upload-and-save-images';
+import { deleteUserImage } from '@/lib/api/user-image';
 import CommentInput from './comment-input';
 import CommentOutput from './comment-output';
 import UserProfileRedirectButton from '../users/user-profile-redirect-button';
 import Avatar from '../avatar/avatar';
 import NoCommentOutput from './no-comment-output';
 import DiscordLoginDialog from '../auth/discord-login-dialog';
+import ImagesInput from '../inputs/images-input';
+import ImagePreviews from '../images/image.previews';
 
 export default function CommentCard({
   user,
@@ -55,13 +61,22 @@ export default function CommentCard({
   const [mode, setMode] = useState<CRUD>('read');
   const device = useDeviceDetect();
 
-  const { handleSubmit, control, getValues, setValue, reset } =
+  const { handleSubmit, control, watch, getValues, setValue, reset } =
     useForm<CommentValues>({
       defaultValues: {
         id: '',
         content: '',
+        images: [],
       },
     });
+
+  const { remove } = useFieldArray({
+    control,
+    name: 'images',
+  });
+
+  const id = watch('id');
+  const images = watch('images');
 
   useEffect(() => {
     setMode(defaultMode);
@@ -71,6 +86,8 @@ export default function CommentCard({
     if (commentId) {
       setValue('id', commentId);
       setValue('content', content || '');
+    } else {
+      setValue('id', uuid4());
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [commentId, content]);
@@ -86,9 +103,13 @@ export default function CommentCard({
     if (mode === 'create') {
       handleWrite({
         ...values,
-        id: uuid4(),
+        id,
       });
-      reset();
+      reset({
+        id: uuid4(),
+        content: '',
+        images: [],
+      });
     } else if (mode === 'update') {
       handleEdit(values);
       setMode('read');
@@ -101,6 +122,35 @@ export default function CommentCard({
       event.preventDefault();
       handleSubmitValid(getValues(), event);
     }
+  };
+
+  const handleChangeFileInput = async (files: FileList | null) => {
+    if (!files?.length) return;
+    if (!isCurrentUser || !user) return;
+
+    const uploadedImages = parseUploadedImages({
+      files,
+      offset: images.length,
+    });
+
+    const userImages = await uploadAndSaveImages({
+      uploadedImages,
+      type: 'comment',
+      refId: id,
+      userId: user.id,
+    });
+
+    userImages.map((userImage) =>
+      setValue(`images.${userImage.position}`, userImage),
+    );
+  };
+
+  const handleClickImagePreview = async (position: number) => {
+    const imageId = images.find((image) => image.position === position)?.id;
+    if (!imageId) return;
+
+    await deleteUserImage(imageId);
+    remove(position);
   };
 
   const handleAuthorization: MouseEventHandler = () => {
@@ -144,7 +194,33 @@ export default function CommentCard({
               }}
             />
           </div>
-          <div className="flex-none">
+          <div className="flex-none flex flex-row gap-2 items-center">
+            <ImagePreviews
+              images={images}
+              previewsProp={{
+                onClick: handleClickImagePreview,
+              }}
+            />
+            <ImagesInput
+              name="images"
+              control={control}
+              rules={{
+                required: false,
+              }}
+              imagesInputProps={{
+                label: {
+                  style: MOBILE_FILE_INPUT_LABEL_STYLE,
+                },
+                icon: {
+                  fontSize: 'medium',
+                },
+                onChange: handleChangeFileInput,
+              }}
+              inputProps={{
+                multiple: true,
+                hidden: true,
+              }}
+            />
             <DiscordLoginDialog
               icon={
                 <ArrowUpwardIcon className="bg-gray-600 text-gray-400 hover:text-gray-300 rounded-lg" />
