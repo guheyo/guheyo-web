69c98082dfe3ec75e9aa21dc1db024c0d563607a
keypport <keypport@gmail.com>
2024-06-05 17:05:26 +0900

    feat: initialize isBidModeVar only once

--- lib/bid/use-bid-input.ts
+++ lib/bid/use-bid-input.ts
@@ -1,7 +1,12 @@
 import { useReactiveVar } from '@apollo/client';
 import { isBidModeVar } from './is-bid-mode-var';
 
-export const useBidInput = () => {
+export const useBidInput = (defaultIsBidMode: boolean) => {
+  // initialize isBidModeVar only once
+  if (isBidModeVar() === undefined) {
+    isBidModeVar(defaultIsBidMode);
+  }
+
   const isBidMode = useReactiveVar(isBidModeVar);
 
   const toggleBidMode = () => isBidModeVar(!isBidModeVar());
