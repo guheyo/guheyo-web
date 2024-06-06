29e72300d2c32422624cefbdb9524d6f78c974c1
keypport <keypport@gmail.com>
2024-06-06 13:44:57 +0900

    feat: add discord login dialog

---
+++ components/auth/discord-login-dialog.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Dialog, DialogTitle, DialogActions } from '@mui/material';
+import LoginButton from './login-button';
+
+export default function DiscordLoginDialog({
+  open,
+  onClose,
+}: {
+  open: boolean;
+  onClose: () => void;
+}) {
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      maxWidth="xs"
+      fullWidth
+      className="backdrop-blur-sm"
+    >
+      <DialogTitle className="text-md md:text-xl text-center">
+        디스코드 아이디로 로그인 해주세요
+      </DialogTitle>
+      <DialogActions className="flex flex-row gap-1 text-lg items-center justify-center pt-0">
+        <LoginButton />
+      </DialogActions>
+    </Dialog>
+  );
+}
