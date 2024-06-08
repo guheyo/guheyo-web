4f93c386f7caa7634561d0fdbd26ca28ac42f219
keypport <keypport@gmail.com>
2024-06-08 18:03:24 +0900

    feat: update py

--- components/auction/auction-detail-sticky-header.tsx
+++ components/auction/auction-detail-sticky-header.tsx
@@ -29,7 +29,7 @@ export default function AuctionDetailStickyHeader({
   return (
     <div className="sticky top-12 z-50 flex flex-row items-center gap-2 mx-2 md:mx-0 bg-dark-500">
       <div
-        className={`flex flex-row flex-grow justify-between items-center text-gray-200 rounded-lg py-2 md:py-3 px-4 md:px-3 text-sm md:text-base ${
+        className={`flex flex-row flex-grow justify-between items-center text-gray-200 rounded-lg py-3 md:py-3 px-4 md:px-3 text-sm md:text-base ${
           auction.status === AUCTION_CLOSED ? 'bg-black' : 'bg-star-500'
         }`}
       >
@@ -50,7 +50,7 @@ export default function AuctionDetailStickyHeader({
       </div>
       {/* Show the ToggleAuctionInteractionItem only if the current user is not the seller */}
       {jwtPayload?.id !== auction.post.user.id && (
-        <div className="flex flex-row justify-between items-center bg-gray-500 text-gray-200 rounded-lg py-2 md:py-3 px-2 md:px-3 text-sm md:text-base">
+        <div className="flex flex-row justify-between items-center bg-gray-500 text-gray-200 rounded-lg py-3 md:py-3 px-2 md:px-3 text-sm md:text-base">
           <ToggleAuctionInteractionItem />
         </div>
       )}
