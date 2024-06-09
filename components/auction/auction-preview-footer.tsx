27f2102dfd985db9a44253a3146467cba8a49b94
keypport <keypport@gmail.com>
2024-06-10 04:43:16 +0900

    feat: parse status by extendedEndDate

--- components/auction/auction-preview-footer.tsx
+++ components/auction/auction-preview-footer.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import dayjs from 'dayjs';
 import { AuctionPreviewResponse } from '@/generated/graphql';
 import { AuctionStatus } from '@/lib/auction/auction.types';
-import { AUCTION_LIVE } from '@/lib/auction/auction.constants';
 import AuctionPreviewPrice from './auction-preview-price';
 import AuctionCountdown from './auction-count-down';
 import AuctionPreviewStatusLabel from './auction-preview-status-label';
@@ -12,13 +12,15 @@ interface Props {
 }
 
 export default function AuctionPreviewFooter({ auction }: Props) {
+  const status: AuctionStatus = dayjs().isAfter(dayjs(auction.extendedEndDate))
+    ? 'closed'
+    : 'live';
+
   return (
     <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
       <div className="flex flex-row gap-2 text-xs md:text-sm text-gray-200">
-        {auction.status !== AUCTION_LIVE && (
-          <AuctionPreviewStatusLabel
-            auctionStatus={auction.status as AuctionStatus}
-          />
+        {status === 'closed' && (
+          <AuctionPreviewStatusLabel auctionStatus={status} />
         )}
         <AuctionCountdown
           targetDate={auction.extendedEndDate}
