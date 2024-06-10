6e48d64d1c1e63364a1f8aba34c498af139f6548
keypport <keypport@gmail.com>
2024-06-10 20:25:16 +0900

    feat: add orderBy in AuctionFeed args

--- app/g/[groupSlug]/(market)/auction/page.tsx
+++ app/g/[groupSlug]/(market)/auction/page.tsx
@@ -2,6 +2,7 @@
 
 import AuctionFeed from '@/components/auction/auction-feed';
 import ThumbnailFeedLayout from '@/components/posts/thumbnail-feed.layout';
+import { getFindAuctionsOrderByArgs } from '@/lib/auction/get-find-auctions-order-by-args';
 import { parseAuctionStatus } from '@/lib/auction/parse-auction-status';
 import { useSearchParams } from 'next/navigation';
 
@@ -19,11 +20,16 @@ function Page({ params: { groupSlug } }: AuctionsPageProps) {
   const where = {
     status,
   };
+
+  const orderBy = getFindAuctionsOrderByArgs({
+    sortOrder: searchParams.get('sort') || undefined,
+  });
+
   const distinct = searchParams.get('distinct') !== 'false';
 
   return (
     <ThumbnailFeedLayout>
-      <AuctionFeed where={where} distinct={distinct} />
+      <AuctionFeed where={where} orderBy={orderBy} distinct={distinct} />
     </ThumbnailFeedLayout>
   );
 }
