716bc5f00de1b758d97dc7bcd4e8c60a4b4e028b
keypport <113260136+keypport@users.noreply.github.com>
2024-06-16 22:00:45 +0900

    feat: hide auctions before testing completed (#241)

--- components/users/user-home.constants.ts
+++ components/users/user-home.constants.ts
@@ -1,13 +1,11 @@
 export const PUBLIC_USER_HOME_OPTIONS = [
   { value: 'offer', label: '거래' },
-  { value: 'auction', label: '경매' },
   { value: 'review', label: '거래 후기' },
   { value: 'report', label: '신고 기록' },
 ];
 
 export const PRIVATE_USER_HOME_OPTIONS = [
   { value: 'offer', label: '내 거래' },
-  { value: 'auction', label: '경매' },
   { value: 'review', label: '거래 후기' },
   { value: 'report', label: '신고 기록' },
 ];
