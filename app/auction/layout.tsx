c9ce3bb2502a5e4739f8297cf2430fbeb09ff367
keypport <keypport@gmail.com>
2024-06-18 21:31:31 +0900

    feat: move HomeAuctionFeedLayout to auction page

--- app/auction/layout.tsx
+++
@@ -1,8 +0,0 @@
-import HomeAuctionFeedLayout from '@/components/auction/home-auction-feed.layout';
-import { ReactNode } from 'react';
-
-function Layout({ children }: { children: ReactNode }) {
-  return <HomeAuctionFeedLayout>{children}</HomeAuctionFeedLayout>;
-}
-
-export default Layout;
