f58e6abe1ce5bf02de87076f8611bc8595c0d897
keypport <keypport@gmail.com>
2024-06-18 21:15:17 +0900

    feat: add write auction page

---
+++ app/write/auction/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import SearchGroups from '@/components/search/search-groups';
+import { parseWriteLink } from '@/lib/write/parse-write-link';
+import { Suspense } from 'react';
+
+export default function page() {
+  const generateLink = (slug: string) =>
+    parseWriteLink({ groupSlug: slug, channelSlug: 'auction' });
+
+  return (
+    <Suspense>
+      <SearchGroups
+        placeholder="경매할 제품의 그룹을 선택해 주세요"
+        generateLink={generateLink}
+      />
+    </Suspense>
+  );
+}
