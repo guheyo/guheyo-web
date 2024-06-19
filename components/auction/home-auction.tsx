f9af69e55df1a3d2e027b2d5bb9c00eee1637f44
keypport <113260136+keypport@users.noreply.github.com>
2024-06-19 21:08:55 +0900

    feat: implement HomeAuction (#261)
    * feat: add showSelector in props, add Link to auction name
    
    * feat: update marginRight
    
    * feat: pass showSelector as props in auction page
    
    * feat: add HomeAuction
    
    * feat: add HomeAuction in root page
    
    * feat: remove auctions in groupPreviews

---
+++ components/auction/home-auction.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import Link from 'next/link';
+import PlayCircleOutlineOutlinedIcon from '@mui/icons-material/PlayCircleOutlineOutlined';
+import { useFindAuctionPreviewsQuery } from '@/generated/graphql';
+import { isMobile } from 'react-device-detect';
+import AuctionPreview from './auction-preview';
+import HomeAuctionFeedLayout from './home-auction-feed.layout';
+import ThumbnailFeedLayout from '../posts/thumbnail-feed.layout';
+
+export default function HomeAuction() {
+  const { loading, data } = useFindAuctionPreviewsQuery({
+    variables: {
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 4,
+      skip: 0,
+    },
+    fetchPolicy: 'cache-first',
+  });
+
+  if (loading) return <div />;
+  if (!data?.findAuctionPreviews) return <div />;
+  const auctions = data.findAuctionPreviews.edges.map((edge) => edge.node);
+
+  return (
+    <HomeAuctionFeedLayout showSelector={false}>
+      <ThumbnailFeedLayout>
+        {auctions.map((auction) => (
+          <AuctionPreview
+            key={auction.id}
+            auction={auction}
+            isInGroup={false}
+          />
+        ))}
+      </ThumbnailFeedLayout>
+      <div className="flex justify-end text-sm md:text-base text-dark-200 font-medium mx-0 md:mx-1 pt-2">
+        <Link href="/auction">
+          <span className="flex flex-row items-center gap-1">
+            <PlayCircleOutlineOutlinedIcon
+              fontSize={isMobile ? 'small' : 'medium'}
+            />
+            경매장
+          </span>
+        </Link>
+      </div>
+    </HomeAuctionFeedLayout>
+  );
+}
