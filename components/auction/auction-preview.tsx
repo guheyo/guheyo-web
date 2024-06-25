b4e73b501ae59562ef50df809c0bfa47af668ffe
keypport <keypport@gmail.com>
2024-06-26 01:17:34 +0900

    feat: add type in AuctionPreview props

--- components/auction/auction-preview.tsx
+++ components/auction/auction-preview.tsx
@@ -2,12 +2,30 @@
 
 import { AuctionPreviewResponse } from '@/generated/graphql';
 import AuctionThumbnailPreview from './auction-thumbnail-preview';
+import AuctionTextPreview from './auction-text-preview';
+import AuctionListViewPreview from './auction-listview-preview';
 
 interface Props {
+  type: 'text' | 'thumbnail' | 'listview';
   auction: AuctionPreviewResponse;
   isInGroup: boolean;
 }
 
-export default function AuctionPreview({ auction, isInGroup }: Props) {
-  return <AuctionThumbnailPreview auction={auction} isInGroup={isInGroup} />;
+export default function AuctionPreview({ type, auction, isInGroup }: Props) {
+  switch (type) {
+    case 'text': {
+      return <AuctionTextPreview auction={auction} isInGroup={isInGroup} />;
+    }
+    case 'thumbnail': {
+      return (
+        <AuctionThumbnailPreview auction={auction} isInGroup={isInGroup} />
+      );
+    }
+    case 'listview': {
+      return <AuctionListViewPreview auction={auction} isInGroup={isInGroup} />;
+    }
+    default: {
+      return <div />;
+    }
+  }
 }
