83bb0ab20dd7284b9fd64317765654f8add6ab7b
keypport <keypport@gmail.com>
2024-06-29 23:01:03 +0900

    feat: set w-fit to LinkAvatar

--- components/users/private-user-profile.tsx
+++ components/users/private-user-profile.tsx
@@ -23,7 +23,7 @@ export default function PrivateUserProfile() {
 
   return (
     <div className="grid grid-cols-12 gap-0 text-sm md:text-base items-center">
-      <div className="col-span-3 md:col-span-3">
+      <div className="col-span-3 md:col-span-3 w-fit">
         <Link href={parseUserHomeLink({ username: user.username })}>
           <Avatar
             name={user.username}
--- components/users/public-user-profile.tsx
+++ components/users/public-user-profile.tsx
@@ -34,7 +34,7 @@ export default function PublicUserProfile({
 
   return (
     <div className="grid grid-cols-12 gap-0 text-sm md:text-base items-center">
-      <div className="col-span-3 md:col-span-3">
+      <div className="col-span-3 md:col-span-3 w-fit">
         <Link href={parseUserHomeLink({ username })}>
           <Avatar
             name={user.username}
