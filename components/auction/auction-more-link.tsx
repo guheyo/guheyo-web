5e293ebe45325b96e896491dae2ffe8c9691eebb
keypport <keypport@gmail.com>
2024-06-29 23:39:01 +0900

    feat: add AuctionMoreLink

---
+++ components/auction/auction-more-link.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Link from 'next/link';
+import PlayCircleOutlineOutlinedIcon from '@mui/icons-material/PlayCircleOutlineOutlined';
+import { isMobile } from 'react-device-detect';
+
+export default function AuctionMoreLink() {
+  return (
+    <Link href="/auction">
+      <span className="flex flex-row items-center gap-1">
+        <PlayCircleOutlineOutlinedIcon
+          fontSize={isMobile ? 'small' : 'medium'}
+        />
+        경매장
+      </span>
+    </Link>
+  );
+}
