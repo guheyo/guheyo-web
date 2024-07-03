fd0cd11525ab77c86f842038415b546fe90b4689
keypport <keypport@gmail.com>
2024-07-04 02:27:55 +0900

    feat: add PostDetailAddons, CommentFeed in Article detail page

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import ArticleDetail from '@/components/article/article-detail';
+import CommentFeed from '@/components/comments/comment-feed';
+import PostDetailAddons from '@/components/posts/post-detail-addons';
 import { useFindArticleQuery } from '@/generated/graphql';
+import {
+  FindCommentsOrderByArgs,
+  FindCommentsWhereArgs,
+} from '@/interfaces/comment.interfaces';
 
 function Page({
   params: { slug },
@@ -22,9 +28,22 @@ function Page({
 
   const article = data.findArticle;
 
+  const where: FindCommentsWhereArgs = {
+    postId: article.post.id,
+  };
+  const orderBy: FindCommentsOrderByArgs = {
+    createdAt: 'desc',
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <ArticleDetail article={article} />
+      <div className="pt-14 px-4 md:px-0 text-base md:text-lg text-gray-300 font-bold">
+        <PostDetailAddons commentCount={article.post.commentCount || 0} />
+      </div>
+      <div className="px-4 md:px-0">
+        <CommentFeed defaultWhere={where} defaultOrderBy={orderBy} />
+      </div>
     </div>
   );
 }
