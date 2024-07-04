ecc3c8cf07fff891728a2afeacb1681859e41fde
keypport <keypport@gmail.com>
2024-07-04 20:25:38 +0900

    feat: update PostCreatedAt font size

--- components/posts/post-created-at.tsx
+++ components/posts/post-created-at.tsx
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 
 export default function PostCreatedAt({ createdAt }: { createdAt: Date }) {
   return (
-    <div className="text-xs md:text-sm text-gray-500">
+    <div className="text-[10px] md:text-xs text-gray-500">
       {dayjs(createdAt).fromNow()}
     </div>
   );
