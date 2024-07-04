818d850067ba83830a10bef572210781cd5a1329
keypport <keypport@gmail.com>
2024-07-05 03:47:01 +0900

    feat: add BusinessFunctionSelector in OfferFeedHomeLayout

--- components/offers/offer-feed-home.layout.tsx
+++ components/offers/offer-feed-home.layout.tsx
@@ -1,20 +1,43 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { BusinessFunction } from '@/lib/offer/offer.types';
 import OfferSelectors from '../selectors/offer-selectors';
 import ProductCategoriesNavbar from '../categories/product-categories-navbar';
+import BusinessFunctionSelector from './business-function-selector';
 
 interface Props {
   children: ReactNode;
 }
 
 function OfferFeedHomeLayout({ children }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const businessFunction = pathname.split('/').at(-1) as BusinessFunction;
+
+  const handleBusinessFunctionChange = (
+    selectedBusinessFunction: BusinessFunction,
+  ): void => {
+    const params = new URLSearchParams(searchParams.toString());
+    const segments = pathname.split('/');
+    segments[segments.length - 1] = selectedBusinessFunction;
+    const newURL = segments.join('/');
+
+    router.push(`${newURL}?${params.toString()}`);
+  };
+
   return (
     <div>
       <div className="mx-2.5 md:mx-1">
         <ProductCategoriesNavbar types={['product', 'service']} hideSelector />
       </div>
-      <div className="flex justify-end pb-2">
+      <div className="flex justify-end pb-2 flex flex-row justify-between">
+        <BusinessFunctionSelector
+          onChange={handleBusinessFunctionChange}
+          businessFunction={businessFunction}
+        />
         <OfferSelectors />
       </div>
       <div className="grid gap-1 grid-cols-1">{children}</div>
