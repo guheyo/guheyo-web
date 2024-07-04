c5b582242e73c40fd648e9bea8dc98a3db49d487
keypport <keypport@gmail.com>
2024-07-04 20:25:07 +0900

    feat: add OfferCreditBar

---
+++ components/offers/offer-credit-bar.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { OfferPreviewResponse } from '@/generated/graphql';
+import { BusinessFunction, OfferStatus } from '@/lib/offer/offer.types';
+import BusinessFunctionLabel from './business-function-label';
+import OfferMenu from './offer-menu';
+import PostCreatedAt from '../posts/post-created-at';
+
+interface Props {
+  offer: OfferPreviewResponse;
+}
+
+export default function OfferCredditBar({ offer }: Props) {
+  return (
+    <div className="flex flex-row items-center justify-between">
+      <div className="flex flex-row gap-2 items-center">
+        <BusinessFunctionLabel
+          businessFunction={offer.businessFunction as BusinessFunction}
+        />
+        <PostCreatedAt createdAt={offer.bumpedAt} />
+      </div>
+      <div className="h-4 mt-[-22px]">
+        <OfferMenu
+          offerId={offer.id}
+          postId={offer.post.id}
+          offerStatus={offer.status as OfferStatus}
+          userId={offer.post.user.id}
+          privateOnly
+          archivedAt={offer.post.archivedAt}
+        />
+      </div>
+    </div>
+  );
+}
