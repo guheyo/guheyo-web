34133f558bb936de78e6d973c5085bbfb1d92c38
keypport <keypport@gmail.com>
2024-07-06 07:14:52 +0900

    feat: change PostCategory bg to gray-600

--- components/posts/post-category.tsx
+++ components/posts/post-category.tsx
@@ -6,7 +6,7 @@ export default function PostCategory({
   category: CategoryResponse;
 }) {
   return (
-    <div className="w-fit rounded-lg px-1 py-0.5 text-[10px] md:text-xs bg-slate-800 text-gray-300">
+    <div className="w-fit rounded-lg px-1 py-0.5 text-[10px] md:text-xs bg-gray-600 text-gray-300">
       {category.name}
     </div>
   );
