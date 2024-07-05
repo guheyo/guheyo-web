5ef612e2b2fbf112290575d54a06ab7136c6fece
keypport <keypport@gmail.com>
2024-07-06 03:35:53 +0900

    feat: add UserReviewHomeLink

---
+++ lib/user-review/user-review-home-link.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
+import HomeLinkLayout from '@/components/home/home-link.layout';
+
+function UserReviewHomeLink() {
+  return (
+    <HomeLinkLayout path="review">
+      <StickyNote2Icon />
+      <div>거래 후기</div>
+    </HomeLinkLayout>
+  );
+}
+
+export default UserReviewHomeLink;
