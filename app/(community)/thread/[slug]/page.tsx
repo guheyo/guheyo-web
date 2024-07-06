aa7a023103db376c76fe64faebeb9f75fdb35ca3
keypport <keypport@gmail.com>
2024-07-07 02:18:28 +0900

    feat: rename article detail page to thread detail page

--- app/(community)/thread/[slug]/page.tsx
+++ app/(community)/thread/[slug]/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import ArticleDetail from '@/components/article/article-detail';
+import ThreadDetail from '@/components/thread/thread-detail';
 import CommentFeed from '@/components/comments/comment-feed';
 import PostDetailAddons from '@/components/posts/post-detail-addons';
-import { useFindArticleQuery } from '@/generated/graphql';
+import { useFindThreadQuery } from '@/generated/graphql';
 import {
   FindCommentsOrderByArgs,
   FindCommentsWhereArgs,
@@ -16,7 +16,7 @@ function Page({
     slug: string;
   };
 }) {
-  const { loading, data } = useFindArticleQuery({
+  const { loading, data } = useFindThreadQuery({
     variables: {
       slug: decodeURI(slug),
     },
@@ -24,12 +24,12 @@ function Page({
   });
 
   if (loading) return <div />;
-  if (!data?.findArticle) return <div />;
+  if (!data?.findThread) return <div />;
 
-  const article = data.findArticle;
+  const thread = data.findThread;
 
   const where: FindCommentsWhereArgs = {
-    postId: article.post.id,
+    postId: thread.post.id,
   };
   const orderBy: FindCommentsOrderByArgs = {
     createdAt: 'desc',
@@ -37,9 +37,9 @@ function Page({
 
   return (
     <div className="flex flex-col gap-4">
-      <ArticleDetail article={article} />
+      <ThreadDetail thread={thread} />
       <div className="pt-14 px-4 md:px-0 text-base md:text-lg text-gray-300 font-bold">
-        <PostDetailAddons commentCount={article.post.commentCount || 0} />
+        <PostDetailAddons commentCount={thread.post.commentCount || 0} />
       </div>
       <div className="px-4 md:px-0">
         <CommentFeed defaultWhere={where} defaultOrderBy={orderBy} />
