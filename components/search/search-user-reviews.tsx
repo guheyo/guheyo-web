a2845aa0376214c1b8a13d80ce99bb008a3d7ef3
keypport <keypport@gmail.com>
2024-07-07 17:08:37 +0900

    feat: add SearchUserReviews

---
+++ components/search/search-user-reviews.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useSearchQuery } from '@/lib/search/use-search-query';
+import { FindUserReviewsOrderByArgs } from '@/interfaces/user-review.interfaces';
+import SearchInput from './search-input';
+import { DEBOUNCE } from './search.constants';
+import TextFeedLayout from '../posts/text-feed.layout';
+import UserReviewFeed from '../user-review/user-review-feed';
+import MannerTagsNavbar from '../user-review/manner-tags-navbar';
+
+export default function SearchUserReviews() {
+  const { text, setText, keyword } = useSearchQuery(DEBOUNCE);
+
+  const where = {};
+  const orderBy: FindUserReviewsOrderByArgs = {
+    createdAt: 'desc',
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setText(event.target.value);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent): void => {
+    // Do nothing
+  };
+
+  return (
+    <div className="grid w-full">
+      <SearchInput
+        text={text}
+        setText={setText}
+        placeholder="어떤 거래 후기를 찾고 있나요?"
+        handleKeyDown={handleKeyDown}
+        handleChange={handleChange}
+      />
+      <div className="pt-4 mx-2.5 md:mx-1">
+        <MannerTagsNavbar />
+      </div>
+      <TextFeedLayout>
+        <UserReviewFeed
+          defaultWhere={where}
+          defaultOrderBy={orderBy}
+          keyword={keyword}
+        />
+      </TextFeedLayout>
+    </div>
+  );
+}
