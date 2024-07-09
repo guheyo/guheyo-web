8026398cc9e0bf1cb6145beac0f8df1928244914
keypport <keypport@gmail.com>
2024-07-09 17:01:25 +0900

    feat: update props to paramsToUpdate

--- lib/query-string/create-query-string.ts
+++ lib/query-string/create-query-string.ts
@@ -1,18 +1,19 @@
 export default function createQueryString({
   searchParamsString,
-  name,
-  value,
+  paramsToUpdate,
 }: {
   searchParamsString: string;
-  name: string;
-  value?: string | null;
+  paramsToUpdate: { name: string; value?: string | null }[];
 }) {
   const params = new URLSearchParams(searchParamsString);
 
-  if (value) {
-    params.set(name, value);
-  } else {
-    params.delete(name);
-  }
+  paramsToUpdate.forEach(({ name, value }) => {
+    if (value) {
+      params.set(name, value);
+    } else {
+      params.delete(name);
+    }
+  });
+
   return params.toString();
 }
