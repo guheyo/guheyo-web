c8feaa35d7e248b7cbb26613eaedcdf5db418721
keypport <keypport@gmail.com>
2024-07-12 02:24:21 +0900

    feat: router.push to marketLink

--- app/bump/offer/[id]/page.tsx
+++ app/bump/offer/[id]/page.tsx
@@ -14,6 +14,7 @@ import { useContext, useState } from 'react';
 import { SubmitHandler } from 'react-hook-form';
 import OfferBumpFormLayout from '@/components/offers/offer-bump-form.layout';
 import AlertDialog from '@/components/base/alert-dialog';
+import { parseMarketLink } from '@/lib/offer/parse-market-link';
 
 function Page({
   params: { id },
@@ -43,13 +44,18 @@ function Page({
 
     const input: BumpOfferInput = {
       id: values.id,
-      offerId: values.id,
+      offerId: values.offerId,
       newPrice: values.price,
     };
 
     try {
       await bumpOffer(input);
-      router.back();
+      router.push(
+        parseMarketLink({
+          groupSlug: offer.post.group.slug,
+          businessFunction: offer.businessFunction as BusinessFunction,
+        }),
+      );
     } catch (e: any) {
       if (isPostingLimitExceededError(e.message)) {
         const message = parseOfferTermAlertMessage({
