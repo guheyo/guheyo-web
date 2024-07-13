3c414bb4f35d6d51366d2eaf36e5bd335a3d6c40
keypport <keypport@gmail.com>
2024-07-14 00:16:13 +0900

    feat: rename to 경매

--- components/auction/auction-home-link.tsx
+++ components/auction/auction-home-link.tsx
@@ -8,7 +8,7 @@ function AuctionHomeLink() {
     <Link href="/auction">
       <div className="flex flex-row gap-4 items-center text-gray-200 text-base md:text-lg font-semibold pt-4 pb-2">
         <GavelIcon />
-        <div>경매장</div>
+        <div>경매</div>
       </div>
     </Link>
   );
