99ba87417c05f2377adfa63d32d27fab1465e954
keypport <keypport@gmail.com>
2024-07-15 23:02:45 +0900

    feat: add HomeCommunityFeedLayout

---
+++ components/community/home-community-feed.layout.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { ReactNode } from 'react';
+import GroupProfileSidebarItems from '@/components/groups/group-profile-sidebar-items';
+import CommunityHomeLink from './community-home-link';
+
+interface Props {
+  children: ReactNode;
+}
+
+function HomeCommunityFeedLayout({ children }: Props) {
+  return (
+    <div>
+      <div className="pt-0 pb-2 px-3 md:px-1 w-fit">
+        <CommunityHomeLink />
+      </div>
+      <div className="flex flex-row gap-2 md:gap-6 py-2 mb-6 mx-3 md:mx-1">
+        <GroupProfileSidebarItems
+          paddingX={0}
+          paddingY={0}
+          pathFormatter={(slug) => `/g/${slug}/community`}
+        />
+      </div>
+      <div className="grid gap-2 grid-cols-1">{children}</div>
+    </div>
+  );
+}
+
+export default HomeCommunityFeedLayout;
