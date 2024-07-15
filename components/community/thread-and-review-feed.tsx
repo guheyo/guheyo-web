514e6e3d27b97955df1f3e3801375f5bd453e3f3
keypport <keypport@gmail.com>
2024-07-15 22:53:07 +0900

    feat: add ThreadAndReviewFeed

---
+++ components/community/thread-and-review-feed.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { useRef } from 'react';
+import { Mocks } from '@/components/mock/mock';
+import { useGroup } from '@/hooks/use-group';
+import { FindThreadPreviewsWhereInput } from '@/generated/graphql';
+import { PostPreviewType } from '@/lib/post/post.types';
+import { useSearchParams } from 'next/navigation';
+import { findCategory } from '@/lib/group/find-category';
+import { useInfiniteThreadAndReviewFeed } from '@/hooks/use-infinite-thread-and-reviews-feed';
+import { SortOrder } from '@/types/sort.types';
+import { FindUserReviewsWhereArgs } from '@/interfaces/user-review.interfaces';
+import ThreadPreview from '../thread/thread-preview';
+import UserReviewPreview from '../user-review/user-review-preview';
+
+function ThreadAndReviewFeed({
+  defaultThreadWhere,
+  defaultReviewWhere,
+  defaultOrderBy,
+  keyword,
+  type,
+}: {
+  defaultThreadWhere: FindThreadPreviewsWhereInput;
+  defaultReviewWhere: FindUserReviewsWhereArgs;
+  defaultOrderBy?: { createdAt: SortOrder };
+  keyword?: string;
+  type: PostPreviewType;
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+  const { group } = useGroup();
+  const searchParams = useSearchParams();
+  const categorySlug = searchParams.get('category');
+
+  const category = findCategory(group?.categories, {
+    slug: categorySlug,
+  });
+  const tagType = searchParams.get('tagType') || defaultReviewWhere.tagType;
+
+  const { loading, items } = useInfiniteThreadAndReviewFeed({
+    ref,
+    threadWhere: {
+      groupId: group?.id,
+      userId: defaultThreadWhere.userId,
+      categoryId: category?.id,
+    },
+    reviewWhere: {
+      groupId: group?.id,
+      userId: defaultReviewWhere.userId,
+      tagType,
+      reviewedUserId: defaultReviewWhere.reviewedUserId,
+    },
+    orderBy: {
+      createdAt: (defaultOrderBy?.createdAt || 'desc') as SortOrder,
+    },
+    keyword,
+    take: 12,
+  });
+
+  if (loading) return <Mocks length={12} height={72} color="bg-dark-400" />;
+
+  return (
+    <>
+      {items.map((item) => {
+        switch (item.__typename) {
+          case 'ThreadPreviewResponseEdge':
+            return (
+              <ThreadPreview
+                key={item.node.id}
+                type={type}
+                thread={item.node}
+                isInGroup={!!group}
+              />
+            );
+          case 'UserReviewPreviewResponseEdge':
+            return (
+              <UserReviewPreview
+                key={item.node.id}
+                userReview={item.node}
+                isInGroup={!!group}
+              />
+            );
+          default:
+            return <div key={item.node.id} />;
+        }
+      })}
+      <div ref={ref} />
+    </>
+  );
+}
+
+export default ThreadAndReviewFeed;
