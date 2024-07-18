6ab7a58f03147a9055d620d233d02bdddc6b6740
keypport <keypport@gmail.com>
2024-07-18 17:31:49 +0900

    feat: pass ProductCategoriesNavbar in categories props

--- components/auction/auction-home-feed.layout.tsx
+++ components/auction/auction-home-feed.layout.tsx
@@ -5,17 +5,28 @@ import AuctionHomeLink from './auction-home-link';
 import AuctionSelectors from './auction-selectors';
 import BusinessFunctionPathUpdater from '../offers/business-function-path-updater';
 import HomeFeedLayout from '../home/home-feed.layout';
+import ProductCategoriesNavbar from '../categories/product-categories-navbar';
 
 interface Props {
   children: ReactNode;
+  showCategories: boolean;
   showSelector: boolean;
 }
 
-function AuctionHomeFeedLayout({ children, showSelector }: Props) {
+function AuctionHomeFeedLayout({
+  children,
+  showCategories,
+  showSelector,
+}: Props) {
   return (
     <HomeFeedLayout
       homeLink={<AuctionHomeLink />}
       path="auction"
+      categories={
+        showCategories ? (
+          <ProductCategoriesNavbar types={['product']} />
+        ) : undefined
+      }
       selectors={
         showSelector && (
           <>
