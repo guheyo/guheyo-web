7b30aca13cd3e77607cf5b2389ee697b6215ee54
keypport <keypport@gmail.com>
2024-07-19 04:26:06 +0900

    feat: add city names constant

--- lib/community/community.constants.ts
+++ lib/community/community.constants.ts
@@ -2,3 +2,16 @@ export const COMMUNITY_TYPE_OPTIONS = [
   { value: 'thread', label: '스레드' },
   { value: 'review', label: '거래 후기' },
 ];
+
+export const CITIY_NAMES = [
+  '전체',
+  '서울',
+  '경기',
+  '부산',
+  '대구',
+  '인천',
+  '대전',
+  '광주',
+  '강원',
+  '제주',
+];
