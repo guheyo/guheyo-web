3ec8708b14174058215646ad3f51f9fb927767fe
keypport <keypport@gmail.com>
2024-07-19 02:18:09 +0900

    feat: pass MannerTagsNavbar to HomeFeedLayout

--- components/community/community-home-feed.layout.tsx
+++ components/community/community-home-feed.layout.tsx
@@ -1,20 +1,26 @@
 'use client';
 
 import { ReactNode } from 'react';
+import { useSearchParams } from 'next/navigation';
 import CommunityHomeLink from './community-home-link';
 import CommunityCategoriesNavbar from '../thread/community-categories-navbar';
 import HomeFeedLayout from '../home/home-feed.layout';
+import MannerTagsNavbar from '../user-review/manner-tags-navbar';
 
 interface Props {
   children: ReactNode;
 }
 
 function CommunityHomeFeedLayout({ children }: Props) {
+  const searchParams = useSearchParams();
+  const categorySlug = searchParams.get('category');
+
   return (
     <HomeFeedLayout
       homeLink={<CommunityHomeLink />}
       path="community"
       categories={<CommunityCategoriesNavbar />}
+      tags={categorySlug === 'review' ? <MannerTagsNavbar /> : undefined}
     >
       {children}
     </HomeFeedLayout>
