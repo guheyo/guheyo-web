ab8363f99372cf146e004519312b7c3ae60532d9
keypport <keypport@gmail.com>
2024-07-18 17:56:59 +0900

    feat: use useGroup for passing currentGroupId to GroupProfileSidebarItems props

--- components/home/home-feed.layout.tsx
+++ components/home/home-feed.layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ReactNode, Suspense } from 'react';
+import { useGroup } from '@/hooks/use-group';
 import GroupProfileSidebarItems from '../groups/group-profile-sidebar-items';
 
 interface Props {
@@ -18,12 +19,15 @@ function HomeFeedLayout({
   categories,
   selectors,
 }: Props) {
+  const { group } = useGroup();
+
   return (
     <div>
       <div className="pt-0 px-3 md:px-1 w-fit">{homeLink}</div>
       <div className="flex flex-row gap-2 md:gap-6 py-2 mb-6 mx-3 md:mx-1">
         <Suspense>
           <GroupProfileSidebarItems
+            currentGroupId={group?.id}
             paddingX={0}
             paddingY={0}
             pathFormatter={(slug) => `/g/${slug}/${path}`}
