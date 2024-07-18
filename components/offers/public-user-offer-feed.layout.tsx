31eaa5e36f824b941c92abc08ca55f987b3e6f6c
keypport <113260136+keypport@users.noreply.github.com>
2024-07-19 05:24:45 +0900

    feat: merge offer, auction into offer in UserHome tabs (#322)
    * feat: merge offer and auction to offer
    
    * feat: move user home auction to user home offer/auction
    
    * feat: move offer layout to offer/businessFunction layout, add OfferBusinessFunctionsNavbar in offer Layout
    
    * feat: change OFFER_OPTIONS to BUSINESS_FUNCTION_OPTIONS in OfferBusinessFunctionsNavbar
    
    * feat: update private, public user offer feed layout
    
    * feat: update private, public layouts

--- components/offers/public-user-offer-feed.layout.tsx
+++ components/offers/public-user-offer-feed.layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ReactNode } from 'react';
-import OfferBusinessFunctionsNavbar from './offer-business-functions-navbar';
 
 interface Props {
   children: ReactNode;
@@ -9,12 +8,10 @@ interface Props {
 
 function PublicUserOfferFeedLayout({ children }: Props) {
   return (
-    <div>
-      <div className="flex flex-col justify-start pb-4">
-        <OfferBusinessFunctionsNavbar />
-      </div>
-      <div className="grid gap-1 grid-cols-1">{children}</div>
-    </div>
+    <>
+      <div className="pb-4" />
+      {children}
+    </>
   );
 }
 
