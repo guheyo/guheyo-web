3d9853df07f230fb6056ebb8a4a225fc7f24dfff
keypport <keypport@gmail.com>
2024-07-19 05:09:40 +0900

    feat: update private, public user offer feed layout

--- components/offers/private-user-offer-feed.layout.tsx
+++ components/offers/private-user-offer-feed.layout.tsx
@@ -2,7 +2,6 @@
 
 import { ReactNode } from 'react';
 import { PRIVATE_USER_OFFER_STATUS_OPTIONS } from '@/lib/offer/offer.constants';
-import OfferBusinessFunctionsNavbar from './offer-business-functions-navbar';
 import OfferStatusNavbar from './offer-status-navbar';
 
 interface Props {
@@ -11,12 +10,9 @@ interface Props {
 
 function PrivateUserofferFeedLayout({ children }: Props) {
   return (
-    <div>
-      <div className="flex flex-col justify-start pb-4">
-        <OfferBusinessFunctionsNavbar />
-        <OfferStatusNavbar options={PRIVATE_USER_OFFER_STATUS_OPTIONS} />
-      </div>
-      <div className="grid gap-1 grid-cols-1">{children}</div>
+    <div className="grid gap-1 grid-cols-1">
+      <OfferStatusNavbar options={PRIVATE_USER_OFFER_STATUS_OPTIONS} />
+      {children}
     </div>
   );
 }
