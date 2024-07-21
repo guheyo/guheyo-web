512549854f5ece20f77eef67f4e846c233cec807
keypport <keypport@gmail.com>
2024-07-22 05:40:11 +0900

    feat: update mobile selector font size to xs

--- components/auction/auction-status-selector.tsx
+++ components/auction/auction-status-selector.tsx
@@ -9,7 +9,7 @@ export default function AuctionStatusSelector() {
       queryKey="status"
       defaultValue="all"
       options={AUCTION_STATUS_OPTIONS}
-      inputClassName="px-3 py-2 text-xs md:text-sm w-16 md:w-20"
+      inputClassName="px-3 py-2 text-xs md:text-sm"
     />
   );
 }
