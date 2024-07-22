106813a0697e08f12b41fd530b649cb36358be6f
keypport <keypport@gmail.com>
2024-07-22 22:51:44 +0900

    feat: add OfferHome with sell, buy, swap in root page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AuctionHome from '@/components/auction/auction-home';
 import CommunityHome from '@/components/community/community-home';
 import HomeFooter from '@/components/footers/home-footer';
 import GroupFeed from '@/components/groups/group-feed';
+import OfferHome from '@/components/offers/offer-home';
 
 export const metadata = {
   title: '구해요',
@@ -11,10 +12,13 @@ export const metadata = {
 function Page() {
   return (
     <>
-      <div className="min-h-screen mb-12 flex flex-col gap-12">
+      <div className="min-h-screen mb-12 flex flex-col gap-4 md:gap-12">
         <AuctionHome />
+        <OfferHome businessFunction="sell" />
+        <OfferHome businessFunction="buy" />
+        <OfferHome businessFunction="swap" />
         <CommunityHome />
-        <div className="px-2 md:px-0">
+        <div className="px-2 md:px-0 pt-0 md:pt-0">
           <GroupFeed />
         </div>
       </div>
