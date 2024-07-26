b6eba3c426cdfacc86b2d0f70fdc1de47868fab7
keypport <keypport@gmail.com>
2024-07-26 20:14:30 +0900

    feat: implement ThreadDetailContainer

---
+++ components/thread/thread-detail-container.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import ThreadDetail from '@/components/thread/thread-detail';
+import CommentFeed from '@/components/comments/comment-feed';
+import PostDetailAddons from '@/components/posts/post-detail-addons';
+import {
+  FindCommentsOrderByInput,
+  FindCommentsWhereInput,
+  ThreadResponse,
+} from '@/generated/graphql';
+import CommentSelector from '@/components/comments/comment-selector';
+
+export default function ThreadDetailContainer({
+  thread,
+}: {
+  thread: ThreadResponse;
+}) {
+  const where: FindCommentsWhereInput = {
+    postId: thread.post.id,
+  };
+  const orderBy: FindCommentsOrderByInput = {
+    createdAt: 'desc',
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <ThreadDetail thread={thread} />
+      <div className="pt-14 px-4 md:px-0 flex flex-row justify-between items-center text-base md:text-lg text-gray-300 font-bold">
+        <PostDetailAddons commentCount={thread.post.commentCount || 0} />
+        <CommentSelector />
+      </div>
+      <div className="px-4 md:px-0">
+        <CommentFeed defaultWhere={where} defaultOrderBy={orderBy} />
+      </div>
+    </div>
+  );
+}
