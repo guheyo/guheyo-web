9566426c410ba64d9c8631f902f31fe02df75297
keypport <keypport@gmail.com>
2024-08-04 21:49:05 +0900

    feat: add gb, group gb page, layout

---
+++ app/g/[groupSlug]/gb/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import GbHomeFeedLayout from '@/components/gb/gb-home-feed.layout';
+import ThreadFeed from '@/components/thread/thread-feed';
+import {
+  FindThreadPreviewsOrderByInput,
+  FindThreadPreviewsWhereInput,
+} from '@/generated/graphql';
+import { useGroup } from '@/hooks/use-group';
+import { SortOrder } from '@/types/sort.types';
+import { Suspense } from 'react';
+
+export default function Page() {
+  const { group, loading } = useGroup();
+
+  if (loading) return <div />;
+  if (!group) return <div />;
+
+  const where: FindThreadPreviewsWhereInput = {
+    groupId: group.id,
+    categoryType: 'gb',
+  };
+  const orderBy: FindThreadPreviewsOrderByInput = {
+    createdAt: 'desc' as SortOrder,
+  };
+
+  return (
+    <Suspense>
+      <GbHomeFeedLayout showCategories showTags showMoreLink={false}>
+        <ThreadFeed
+          type="listview"
+          defaultWhere={where}
+          defaultOrderBy={orderBy}
+        />
+      </GbHomeFeedLayout>
+    </Suspense>
+  );
+}
