1942f373abd85056b09462ee84bba972bed28501
keypport <keypport@gmail.com>
2024-09-04 02:27:46 +0900

    feat: add showSelectors prop in CommunityHomeFeedLayout

--- components/community/community-home-feed.layout.tsx
+++ components/community/community-home-feed.layout.tsx
@@ -8,12 +8,14 @@ import HomeFeedLayout from '../home/home-feed.layout';
 import MannerTagsNavbar from '../user-review/manner-tags-navbar';
 import CommunityTagsNavbar from './community-tags-navbar';
 import CommunityMoreLink from './community-more-link';
+import CommunitySelectors from './community-selectors';
 
 interface Props {
   children: ReactNode;
   hideGroupProfileSidebarItems?: boolean;
   showCategories: boolean;
   showTags: boolean;
+  showSelectors: boolean;
   showMoreLink: boolean;
 }
 
@@ -22,6 +24,7 @@ function CommunityHomeFeedLayout({
   hideGroupProfileSidebarItems,
   showCategories,
   showTags,
+  showSelectors,
   showMoreLink,
 }: Props) {
   const searchParams = useSearchParams();
@@ -41,6 +44,14 @@ function CommunityHomeFeedLayout({
             <CommunityTagsNavbar categorySlug={categorySlug || ''} />
           )
         ) : undefined
+      }
+      selectors={
+        showSelectors && (
+          <>
+            <div />
+            <CommunitySelectors />
+          </>
+        )
       }
       moreLink={showMoreLink && <CommunityMoreLink />}
     >
