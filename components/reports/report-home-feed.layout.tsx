93bbe8c24d45802904ab16e440eb1a6b5b6790b8
keypport <keypport@gmail.com>
2024-09-03 23:55:17 +0900

    feat: add showSelectors in ReportHomeFeedLayout props

--- components/reports/report-home-feed.layout.tsx
+++ components/reports/report-home-feed.layout.tsx
@@ -4,18 +4,28 @@ import ReportTypeNavbar from '@/components/reports/report-type-navbar';
 import { ReactNode } from 'react';
 import ReportHomeLink from './report-home-link';
 import HomeFeedLayout from '../home/home-feed.layout';
+import ReportStatausSelector from './report-status-selector';
 
 interface Props {
   children: ReactNode;
+  showSelectors: boolean;
 }
 
-function ReportHomeFeedLayout({ children }: Props) {
+function ReportHomeFeedLayout({ children, showSelectors }: Props) {
   return (
     <HomeFeedLayout
       postPreviewType="text"
       homeLink={<ReportHomeLink />}
       path="report"
       categories={<ReportTypeNavbar />}
+      selectors={
+        showSelectors && (
+          <>
+            <div />
+            <ReportStatausSelector />
+          </>
+        )
+      }
     >
       {children}
     </HomeFeedLayout>
