93417d89e257654bb3e10868c4144c7ae1c92c86
keypport <keypport@gmail.com>
2024-09-10 17:47:05 +0900

    feat: add image in BrandFormValues

--- lib/brand/brand.interfaces.ts
+++ lib/brand/brand.interfaces.ts
@@ -1,7 +1,10 @@
+import { UserImage } from '../image/image.interfaces';
+
 export type BrandFormValues = {
   id: string;
   name: string;
   slug?: string;
   description?: string;
   logo?: string;
+  image?: UserImage;
 };
