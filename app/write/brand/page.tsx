01dff506f38ae28f1d5fecc6d1e7aa48dd170d30
keypport <keypport@gmail.com>
2024-09-12 03:47:09 +0900

    feet: wrap WriteBrandForm with Suspense

--- app/write/brand/page.tsx
+++ app/write/brand/page.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import WriteBrandForm from '@/components/brand/write-brand-form';
+import { Suspense } from 'react';
 
 export default function Page() {
-  return <WriteBrandForm />;
+  return (
+    <Suspense>
+      <WriteBrandForm />
+    </Suspense>
+  );
 }
