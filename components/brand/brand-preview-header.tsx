ca985cb7c5d4dc30ade3a5ed267075883fe161a1
keypport <keypport@gmail.com>
2024-09-11 18:11:15 +0900

    feat: implement BrandPreviewHeader

---
+++ components/brand/brand-preview-header.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { BrandResponse } from '@/generated/graphql';
+import PostPreviewTitle from '../posts/post-preview-title';
+
+interface Props {
+  brand: BrandResponse;
+}
+
+export default function BrandPreviewHeader({ brand }: Props) {
+  return (
+    <div className="flex flex-row justify-between items-start">
+      <div className="flex flex-col md:flex-row gap-1 md:gap-2 md:items-start">
+        <PostPreviewTitle title={brand.name} />
+      </div>
+      {/* BrandMenu */}
+    </div>
+  );
+}
