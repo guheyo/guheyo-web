8976734aedbb133bf2e99519e732ede768fd0fb2
keypport <keypport@gmail.com>
2024-09-13 21:41:56 +0900

    feat: add BrandHome layout

---
+++ app/brand/[slug]/layout.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { ReactNode } from 'react';
+import BrandHome from '@/components/brand/brand-home';
+import { useFindBrandQuery } from '@/generated/graphql';
+
+interface Props {
+  children: ReactNode;
+  params: {
+    slug: string;
+  };
+}
+
+function Layout({ children, params }: Props) {
+  const { data, loading } = useFindBrandQuery({
+    variables: {
+      slug: decodeURI(params.slug!),
+    },
+    fetchPolicy: 'cache-first',
+  });
+
+  const brand = data?.findBrand;
+
+  if (loading) return <div />;
+  if (!brand) return <div />;
+
+  return (
+    <div className="pt-6 pb-4 md:pb-6 px-2 lg:px-0">
+      <div className="px-2 pb-6">
+        <BrandHome brand={brand} />
+      </div>
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
--- app/brand/[slug]/page.tsx
+++ app/brand/[slug]/page.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import BrandHome from '@/components/brand/brand-home';
-import { useFindBrandQuery } from '@/generated/graphql';
-
 function Page({
   params: { slug },
 }: {
@@ -10,22 +7,7 @@ function Page({
     slug: string;
   };
 }) {
-  const { data, loading } = useFindBrandQuery({
-    variables: {
-      slug: decodeURI(slug),
-    },
-    fetchPolicy: 'cache-and-network',
-  });
-  if (loading) return <div />;
-  if (!data?.findBrand) return <div />;
-
-  const brand = data.findBrand;
-
-  return (
-    <div className="flex flex-col gap-4">
-      <BrandHome brand={brand} />
-    </div>
-  );
+  return <div />;
 }
 
 export default Page;
