1f54da10b45641ed23e6bce5c5891c58c7d130db
keypport <keypport@gmail.com>
2024-09-15 01:57:35 +0900

    feat: update label

--- lib/brand/brand.constants.ts
+++ lib/brand/brand.constants.ts
@@ -3,6 +3,6 @@ export const BRAND = 'brand';
 export const BRAND_AUTO_SAVE_INTERVAL_MS = 60 * 1000;
 
 export const BRAND_SORT_ORDER_OPTIONS = [
-  { value: 'follower', label: '팔로워' },
+  { value: 'follower', label: '랭킹순' },
   { value: 'newest', label: '최신순' },
 ];
