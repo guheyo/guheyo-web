178cb62db9544a27d2725f7ec60d042a6b524cf2
keypport <keypport@gmail.com>
2024-09-14 14:46:28 +0900

    feat: add followed in QueryFindBrandArgs

--- generated/graphql.ts
+++ generated/graphql.ts
@@ -1138,6 +1138,7 @@ export type QueryFindBidsArgs = {
 
 
 export type QueryFindBrandArgs = {
+  followed?: InputMaybe<Scalars['Boolean']['input']>;
   id?: InputMaybe<Scalars['ID']['input']>;
   slug?: InputMaybe<Scalars['String']['input']>;
 };
