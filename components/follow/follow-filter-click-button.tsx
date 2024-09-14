b7756557af885790c124bdf4eb308a5cb8fa02cc
keypport <keypport@gmail.com>
2024-09-14 22:32:20 +0900

    feat: implement FollowFilterClickButton

---
+++ components/follow/follow-filter-click-button.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
+import QueryClickButton from '../click/query-click-button';
+
+export default function FollowFilterClickButton() {
+  return (
+    <QueryClickButton
+      queryKey="followed"
+      defaultClicked={false}
+      clickedNode={
+        <div className="flex flex-row items-center gap-1 text-sm md:text-base text-blurple-500">
+          <PersonAddAlt1Icon />
+          팔로잉
+        </div>
+      }
+      unClickedNode={
+        <div className="flex flex-row items-center gap-1 text-sm md:text-base text-gray-300">
+          <PersonAddAlt1Icon />
+          팔로잉
+        </div>
+      }
+    />
+  );
+}
