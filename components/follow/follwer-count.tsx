2b470ada2f3b217ddde14afb4236d1c7297fca2f
keypport <keypport@gmail.com>
2024-09-15 01:10:53 +0900

    feat: use compact notation for followerCount

--- components/follow/follwer-count.tsx
+++ components/follow/follwer-count.tsx
@@ -5,7 +5,12 @@ export default function FollowerCount({
 }) {
   return (
     <div className="flex flex-row gap-1">
-      <div className="text-gray-300 font-semibold">{followerCount}</div>
+      <div className="text-gray-300 font-semibold">
+        {Intl.NumberFormat('en-US', {
+          notation: 'compact',
+          maximumFractionDigits: 1,
+        }).format(followerCount)}
+      </div>
       팔로워
     </div>
   );
