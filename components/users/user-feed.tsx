4086382f2e6069b0a59104e40a5a092dd6b15391
keypport <113260136+keypport@users.noreply.github.com>
2024-09-17 05:45:45 +0900

    feat: add FollowFilterClickButton in UserFeed (#388)
    * feat: add showCategories, showSelectors in MemberHomeFeedLayout props, pass FollowFilterClickButton to selectors props
    
    * feat: add followed in FindUsersWhereInput
    
    * feat: get followed from searchParams in UserFeed

--- components/users/user-feed.tsx
+++ components/users/user-feed.tsx
@@ -19,12 +19,18 @@ function UserFeed({
 }) {
   const ref = useRef<HTMLDivElement>(null);
   const searchParams = useSearchParams();
+  const followed = [null, 'all'].includes(searchParams.get('followed'))
+    ? undefined
+    : searchParams.get('followed') === 'true';
   const keyword = searchParams.get('q') || undefined;
   const target = searchParams.get('target') || undefined;
 
   const { loading, data } = useInfiniteUsers({
     ref,
-    where: defaultWhere,
+    where: {
+      ...defaultWhere,
+      followed,
+    },
     orderBy: {
       createdAt: defaultOrderBy?.createdAt || 'asc',
     },
