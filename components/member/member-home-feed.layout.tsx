030d9f0619279bac9859b2b0d4c44bd5750f1aea
keypport <keypport@gmail.com>
2024-09-17 05:37:16 +0900

    feat: add showCategories, showSelectors in MemberHomeFeedLayout props, pass FollowFilterClickButton to selectors props

--- components/member/member-home-feed.layout.tsx
+++ components/member/member-home-feed.layout.tsx
@@ -4,18 +4,26 @@ import MemberHomeLink from '@/components/member/member-home-link';
 import MemberRolesNavbar from '@/components/member/member-roles-navbar';
 import { ReactNode } from 'react';
 import HomeFeedLayout from '../home/home-feed.layout';
+import FollowFilterClickButton from '../follow/follow-filter-click-button';
 
 interface Props {
   children: ReactNode;
+  showCategories: boolean;
+  showSelectors: boolean;
 }
 
-function MemberHomeFeedLayout({ children }: Props) {
+function MemberHomeFeedLayout({
+  children,
+  showCategories,
+  showSelectors,
+}: Props) {
   return (
     <HomeFeedLayout
       postPreviewType="text"
       homeLink={<MemberHomeLink />}
       path="member"
-      categories={<MemberRolesNavbar />}
+      categories={showCategories && <MemberRolesNavbar />}
+      selectors={showSelectors && <FollowFilterClickButton />}
     >
       {children}
     </HomeFeedLayout>
