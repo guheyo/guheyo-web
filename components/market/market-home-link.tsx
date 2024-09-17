a9c8ab55f1a8637196ca5ce1937723d4510d2da3
keypport <keypport@gmail.com>
2024-09-17 16:46:16 +0900

    feat: implement MarketHomeLink

---
+++ components/market/market-home-link.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { BusinessFunction } from '@/lib/offer/offer.types';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import HomeLinkLayout from '../home/home-link.layout';
+
+function MarketHomeLink({
+  businessFunction,
+}: {
+  businessFunction: BusinessFunction;
+}) {
+  return (
+    <HomeLinkLayout path={businessFunction}>
+      <StorefrontIcon />
+      <div>장터</div>
+    </HomeLinkLayout>
+  );
+}
+
+export default MarketHomeLink;
