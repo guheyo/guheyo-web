d5d31c91f30fc2ae4b59ba6e61f750da375e529c
keypport <keypport@gmail.com>
2024-09-23 23:02:22 +0900

    feat: add size in props, add parseSize

--- components/groups/group-profile.tsx
+++ components/groups/group-profile.tsx
@@ -2,17 +2,30 @@
 
 import Image from 'next/image';
 import { isMobile } from 'react-device-detect';
+import { ComponentSize } from '@/lib/component/component.types';
 import InfoCard from '../info/info-card';
 
 export default function GroupProfile({
   name,
   icon,
   description,
+  size,
 }: {
   name: string;
   icon?: string | null;
   description?: string | null;
+  size: ComponentSize;
 }) {
+  const parseSize = (imageSize: ComponentSize) => {
+    if (imageSize === 'small') {
+      return isMobile ? 24 : 24;
+    }
+    if (imageSize === 'medium') {
+      return isMobile ? 30 : 36;
+    }
+    return isMobile ? 36 : 42;
+  };
+
   return (
     <div className="flex flex-row justify-between items-center p-4 bg-dark-400 rounded-lg text-gray-300">
       <InfoCard
@@ -20,8 +33,8 @@ export default function GroupProfile({
         icon={
           <Image
             src={!icon ? '/guheyo/guheyo-logo.svg' : icon}
-            width={isMobile ? 30 : 36}
-            height={isMobile ? 30 : 36}
+            width={parseSize(size)}
+            height={parseSize(size)}
             alt={`${name} logo`}
             className="rounded-full"
           />
