18007d90e7b17b1d4859bb37de099391af2a1aa4
keypport <keypport@gmail.com>
2024-09-24 17:30:54 +0900

    feat: implement GroupSelector

---
+++ components/groups/group-selecter.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useRef } from 'react';
+import { SelectChangeEvent } from '@mui/material';
+import { useSearchParams } from 'next/navigation';
+import { useInfiniteGroupProfiles } from '@/hooks/use-infinite-group-profiles';
+import InfiniteScrollSelector from '../selectors/infinite-scroll-selector';
+
+export default function GroupSelector({
+  handleClick,
+  selectedId,
+}: {
+  handleClick: (id: string) => void;
+  selectedId: string;
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get('q') || undefined;
+  const target = searchParams.get('target') || undefined;
+
+  const { loading, data } = useInfiniteGroupProfiles({
+    ref,
+    orderBy: {
+      position: 'asc',
+    },
+    keyword,
+    target,
+    take: 12,
+  });
+
+  const groupProfiles = data?.findGroupProfiles.edges;
+  if (loading || !groupProfiles) return <div />;
+
+  const handleChange = (e: SelectChangeEvent) => {
+    const id = e.target.value;
+    handleClick(id);
+  };
+
+  return (
+    <InfiniteScrollSelector
+      name="group"
+      placeholder="그룹"
+      selectedValue={selectedId}
+      options={groupProfiles.map((groupProfile) => ({
+        value: groupProfile.node.id,
+        label: groupProfile.node.name,
+        imageUrl: groupProfile.node.icon || undefined,
+      }))}
+      inputClassName="text-xs md:text-sm font-medium"
+      ref={ref}
+      handleChange={handleChange}
+    />
+  );
+}
