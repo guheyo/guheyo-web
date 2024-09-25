91849d5eff3a85ffdf632092ba561fe260afa8fa
keypport <keypport@gmail.com>
2024-09-26 02:29:52 +0900

    feat: implement UserReviewHomeFeedLayout

---
+++ components/user-review/user-review-home-feed.layout.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { CommunityChannelType } from '@/lib/community/community.types';
+import HomeFeedLayout from '../home/home-feed.layout';
+import MannerTagsNavbar from './manner-tags-navbar';
+import FollowFilterClickButton from '../follow/follow-filter-click-button';
+import CommunityHomeLink from '../community/community-home-link';
+import CommunityChannelNavbar from '../community/community-channel-navbar';
+import CommunitySelectors from '../community/community-selectors';
+import CommunityMoreLink from '../community/community-more-link';
+
+interface Props {
+  children: ReactNode;
+  communityChannelType: CommunityChannelType;
+  hideGroupProfileSidebarItems?: boolean;
+  showChannels: boolean;
+  showCategories: boolean;
+  showTags: boolean;
+  showSelectors: boolean;
+  showMoreLink: boolean;
+}
+
+function UserReviewHomeFeedLayout({
+  children,
+  communityChannelType,
+  hideGroupProfileSidebarItems,
+  showChannels,
+  showCategories,
+  showTags,
+  showSelectors,
+  showMoreLink,
+}: Props) {
+  return (
+    <HomeFeedLayout
+      postPreviewType="text"
+      homeLink={
+        <CommunityHomeLink communityChannelType={communityChannelType} />
+      }
+      path={hideGroupProfileSidebarItems ? undefined : communityChannelType}
+      channels={showChannels ? <CommunityChannelNavbar /> : undefined}
+      tags={showTags ? <MannerTagsNavbar /> : undefined}
+      selectors={
+        showSelectors && (
+          <>
+            <FollowFilterClickButton />
+            <CommunitySelectors />
+          </>
+        )
+      }
+      moreLink={showMoreLink && <CommunityMoreLink />}
+    >
+      {children}
+    </HomeFeedLayout>
+  );
+}
+
+export default UserReviewHomeFeedLayout;
