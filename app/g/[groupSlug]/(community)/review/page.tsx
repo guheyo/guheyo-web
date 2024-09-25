001090fc7d47e84d97ee8797cef66382d8a2299f
keypport <keypport@gmail.com>
2024-09-26 02:09:46 +0900

    feat: change ThreadFeed to ReviewFeed

--- app/g/[groupSlug]/(community)/review/page.tsx
+++ app/g/[groupSlug]/(community)/review/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import CommunityHomeFeedLayout from '@/components/community/community-home-feed.layout';
-import ThreadFeed from '@/components/thread/thread-feed';
+import UserReviewFeed from '@/components/user-review/user-review-feed';
 import { SortOrder } from '@/types/sort.types';
 import { Suspense } from 'react';
 
@@ -21,12 +21,7 @@ export default function Page() {
         showSelectors
         showMoreLink={false}
       >
-        <ThreadFeed
-          type="listview"
-          defaultWhere={where}
-          defaultOrderBy={orderBy}
-          showInput={false}
-        />
+        <UserReviewFeed defaultWhere={where} defaultOrderBy={orderBy} />
       </CommunityHomeFeedLayout>
     </Suspense>
   );
