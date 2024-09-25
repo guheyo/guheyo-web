da4b59c8fe27546cb678c1cc6c3101fe32e2a4b2
keypport <keypport@gmail.com>
2024-09-26 02:11:23 +0900

    feat: add showChannels, showTabs in BrandHomeFeedLayout

--- components/brand/brand-home-feed.layout.tsx
+++ components/brand/brand-home-feed.layout.tsx
@@ -2,14 +2,17 @@
 
 import { ReactNode } from 'react';
 import HomeFeedLayout from '../home/home-feed.layout';
-import ProductCategoriesNavbar from '../categories/product-categories-navbar';
 import BrandMoreLink from './brand-more-link';
 import BrandHomeLink from './brand-home-link';
 import FollowFilterClickButton from '../follow/follow-filter-click-button';
 import BrandSelectors from './brand-selectors';
+import BrandTabNavbar from './brand-tab-navbar';
+import CommunityChannelNavbar from '../community/community-channel-navbar';
 
 interface Props {
   children: ReactNode;
+  showChannels: boolean;
+  showTabs: boolean;
   showCategories: boolean;
   showSelectors: boolean;
   showMoreLink: boolean;
@@ -17,6 +20,8 @@ interface Props {
 
 function BrandHomeFeedLayout({
   children,
+  showChannels,
+  showTabs,
   showCategories,
   showSelectors,
   showMoreLink,
@@ -26,11 +31,8 @@ function BrandHomeFeedLayout({
       postPreviewType="thumbnail"
       homeLink={<BrandHomeLink />}
       path="brand"
-      categories={
-        showCategories ? (
-          <ProductCategoriesNavbar types={['product']} />
-        ) : undefined
-      }
+      channels={showChannels ? <CommunityChannelNavbar /> : undefined}
+      tabs={showTabs ? <BrandTabNavbar /> : undefined}
       selectors={
         showSelectors && (
           <>
