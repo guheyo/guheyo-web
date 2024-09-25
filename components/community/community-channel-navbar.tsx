6937e7f8ccfa94d8805402b6b7a9eb4392c2d03c
keypport <keypport@gmail.com>
2024-09-26 03:50:05 +0900

    feat: refactor CommunityChannelNavbar using parseChannelLink

--- components/community/community-channel-navbar.tsx
+++ components/community/community-channel-navbar.tsx
@@ -1,18 +1,25 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
 import { COMMUNITY_CHANNEL_OPTIONS } from '@/lib/community/community.constants';
+import { useGroup } from '@/hooks/use-group';
+import { usePathname } from 'next/navigation';
+import { parseChannelLink } from '@/lib/channel/parse-channel-link';
 import TextNavbar from '../base/text-navbar';
 
 export default function CommunityChannelNavbar() {
   const pathname = usePathname();
-  const selectedValue = pathname.split('/').at(-1);
+  const { group } = useGroup();
+  const selectedValue = group
+    ? pathname.split('/').at(3)
+    : pathname.split('/').at(1);
 
   return (
     <TextNavbar
       options={COMMUNITY_CHANNEL_OPTIONS}
       selectedValue={selectedValue}
-      parseNewURL={(value) => value}
+      parseNewURL={(value) =>
+        parseChannelLink({ channelName: value, groupSlug: group?.slug })
+      }
       size="medium"
     />
   );
