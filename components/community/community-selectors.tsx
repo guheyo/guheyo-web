571ec3860ea87853e2e097035b6e577eecc0cbf4
keypport <keypport@gmail.com>
2024-09-26 20:32:59 +0900

    feat: add CitySelector in CommunitySelectors

--- components/community/community-selectors.tsx
+++ components/community/community-selectors.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
 import PeriodSelector from '../selectors/period-selector';
+import CitySelector from '../selectors/city-selector';
 
 export default function CommunitySelectors() {
+  const searchParams = useSearchParams();
+  const categorySlug = searchParams.get('category') || undefined;
+
   return (
     <div className="flex gap-1 md:gap-2 justify-end">
+      {categorySlug === 'meetup' && <CitySelector />}
       <PeriodSelector />
     </div>
   );
