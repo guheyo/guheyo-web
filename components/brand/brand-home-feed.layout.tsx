1493f851c1ef9a7c98bfe0dfeed31087dfca4b75
keypport <keypport@gmail.com>
2024-09-26 22:59:27 +0900

    feat: remove BrandTabNavbar, CommunityChannelNavbar, CommunityHomeLink in BrandHomeFeedLayout

--- components/brand/brand-home-feed.layout.tsx
+++ components/brand/brand-home-feed.layout.tsx
@@ -6,9 +6,7 @@ import HomeFeedLayout from '../home/home-feed.layout';
 import BrandMoreLink from './brand-more-link';
 import FollowFilterClickButton from '../follow/follow-filter-click-button';
 import BrandSelectors from './brand-selectors';
-import BrandTabNavbar from './brand-tab-navbar';
-import CommunityChannelNavbar from '../community/community-channel-navbar';
-import CommunityHomeLink from '../community/community-home-link';
+import BrandHomeLink from './brand-home-link';
 
 interface Props {
   children: ReactNode;
@@ -32,10 +30,8 @@ function BrandHomeFeedLayout({
   return (
     <HomeFeedLayout
       postPreviewType={postPreviewType}
-      homeLink={<CommunityHomeLink communityChannelType="brand" />}
+      homeLink={<BrandHomeLink />}
       path="brand"
-      channels={showChannels ? <CommunityChannelNavbar /> : undefined}
-      tabs={showTabs ? <BrandTabNavbar /> : undefined}
       selectors={
         showSelectors && (
           <>
