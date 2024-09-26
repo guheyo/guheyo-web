0cf7bdf58e749a3958a7460725735765128bb7f9
keypport <keypport@gmail.com>
2024-09-27 04:51:36 +0900

    feat: add setGroupId in props, set selectedId using groupProfiles

--- components/groups/group-selector.tsx
+++ components/groups/group-selector.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { SelectChangeEvent } from '@mui/material';
 import { useSearchParams } from 'next/navigation';
 import { useInfiniteGroupProfiles } from '@/hooks/use-infinite-group-profiles';
@@ -11,10 +11,12 @@ export default function GroupSelector({
   handleClick,
   defaultWhere,
   selectedId,
+  setGroupId,
 }: {
   handleClick: (id: string) => void;
   defaultWhere?: FindGroupProfilesWhereInput;
   selectedId: string;
+  setGroupId: (id: string) => void;
 }) {
   const ref = useRef<HTMLDivElement>(null);
   const searchParams = useSearchParams();
@@ -35,6 +37,11 @@ export default function GroupSelector({
   });
 
   const groupProfiles = data?.findGroupProfiles.edges;
+
+  useEffect(() => {
+    if (groupProfiles?.length === 1) setGroupId(groupProfiles[0].node.id);
+  }, [groupProfiles, groupProfiles?.length, setGroupId]);
+
   if (loading || !groupProfiles) return <div />;
 
   const handleChange = (e: SelectChangeEvent) => {
@@ -46,7 +53,11 @@ export default function GroupSelector({
     <InfiniteScrollSelector
       name="group"
       placeholder="그룹"
-      selectedValue={selectedId}
+      selectedValue={
+        selectedId ||
+        (groupProfiles.length === 1 && groupProfiles[0].node.id) ||
+        ''
+      }
       options={groupProfiles.map((groupProfile) => ({
         value: groupProfile.node.id,
         label: groupProfile.node.name,
