bac3804ad853ebc956c2accdf5c1a38dc8e961a6
keypport <keypport@gmail.com>
2024-09-28 04:11:53 +0900

    feat: return empty div if category is general

--- components/posts/post-category.tsx
+++ components/posts/post-category.tsx
@@ -5,6 +5,8 @@ export default function PostCategory({
 }: {
   category: CategoryResponse;
 }) {
+  if (category.name === '일반') return <div />;
+
   return (
     <div className="w-fit rounded-lg px-1 py-0.5 text-[10px] md:text-xs bg-gray-600 text-gray-300">
       {category.name}
