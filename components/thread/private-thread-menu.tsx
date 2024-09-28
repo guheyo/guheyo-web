e915261f4c9db32145d8644dbcd3bd39e8db02b3
keypport <keypport@gmail.com>
2024-09-28 16:59:52 +0900

    feat: implement PrivateThreadMenu

---
+++ components/thread/private-thread-menu.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useRouter } from 'next/navigation';
+import { IconButton } from '@mui/material';
+import { deleteThread } from '@/lib/api/thread';
+import { parseThreadLink } from '@/lib/thread/parse-thread-link';
+import PostDeleteDialog from '../posts/post-delete-dialog';
+import AlertDialog from '../base/alert-dialog';
+
+export default function PrivateThreadMenu({ threadId }: { threadId: string }) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const [openAlert, setOpenAlert] = React.useState(false);
+  const [alertText, setAlertText] = React.useState('');
+  const router = useRouter();
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setAnchorEl(null);
+    setOpenAlert(false);
+  };
+
+  const handleEditClick: React.MouseEventHandler = (event) => {
+    event.preventDefault();
+    router.push(
+      parseThreadLink({
+        action: 'edit',
+        threadId,
+      }),
+    );
+  };
+
+  const handleDelete: React.MouseEventHandler = async (event) => {
+    event.preventDefault();
+    await deleteThread(threadId);
+    setAlertText('삭제되었어요!');
+    setOpenAlert(true);
+  };
+
+  return (
+    <div>
+      <IconButton
+        type="button"
+        aria-label="private offer menu button"
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <MoreVertIcon className="text-xl md:text-2xl text-dark-200" />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleEditClick} sx={{ justifyContent: 'center' }}>
+          글 수정
+        </MenuItem>
+        <MenuItem sx={{ justifyContent: 'center' }}>
+          <PostDeleteDialog handleDelete={handleDelete} />
+        </MenuItem>
+      </Menu>
+      <AlertDialog
+        open={openAlert}
+        text={alertText}
+        handleClose={handleClose}
+      />
+    </div>
+  );
+}
