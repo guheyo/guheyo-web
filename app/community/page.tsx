52572e5c065c89c51154a6e0e6432704517508be
keypport <keypport@gmail.com>
2024-09-29 20:02:11 +0900

    feat: change CommunityHomeFeedLayout to BrandHomeFeedLayout in Community Pages

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import CommunityHomeFeedLayout from '@/components/community/community-home-feed-layout';
-import ThreadAndReviewFeed from '@/components/community/thread-and-review-feed';
+import BrandHomeFeedLayout from '@/components/brand/brand-home-feed.layout';
+import ThreadFeed from '@/components/thread/thread-feed';
 import { SortOrder } from '@/types/sort.types';
 import { Suspense } from 'react';
 
@@ -13,19 +13,20 @@ export default function Page() {
 
   return (
     <Suspense>
-      <CommunityHomeFeedLayout
-        showCategories
-        showTags
+      <BrandHomeFeedLayout
+        postPreviewType="listview"
+        showChannels
+        showCategories={false}
         showSelectors
         showMoreLink={false}
       >
-        <ThreadAndReviewFeed
+        <ThreadFeed
           type="listview"
           defaultWhere={where}
           defaultOrderBy={orderBy}
           showInput
         />
-      </CommunityHomeFeedLayout>
+      </BrandHomeFeedLayout>
     </Suspense>
   );
 }
