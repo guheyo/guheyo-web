56e89187cdc277fe6c396bd79e6738f02fb4e92a
keypport <keypport@gmail.com>
2024-10-01 01:22:12 +0900

    feat: add WRITABLE_CHANNEL_OPTIONS

--- lib/write/write.constants.ts
+++ lib/write/write.constants.ts
@@ -7,6 +7,33 @@ export const WRITABLE_CHANNELS = [
   'community',
 ];
 
+export const WRITABLE_CHANNEL_OPTIONS = [
+  {
+    value: 'auction',
+    label: '경매',
+  },
+  {
+    value: 'sell',
+    label: '판매',
+  },
+  {
+    value: 'buy',
+    label: '구매',
+  },
+  {
+    value: 'swap',
+    label: '교환',
+  },
+  {
+    value: 'gb',
+    label: '공동구매',
+  },
+  {
+    value: 'community',
+    label: '게시글',
+  },
+];
+
 export const NON_WRITABLE_CHANNELS = ['brand', 'member', 'review', 'report'];
 
 export const ALL_CHANNELS = [...WRITABLE_CHANNELS, ...NON_WRITABLE_CHANNELS];
