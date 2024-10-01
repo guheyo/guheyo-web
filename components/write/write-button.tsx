94603761ac7966c846dd97024a0c66ef62204d79
keypport <keypport@gmail.com>
2024-10-02 03:12:46 +0900

    feat: return WriteChannelSelector if not writable channels

--- components/write/write-button.tsx
+++ components/write/write-button.tsx
@@ -3,10 +3,7 @@
 import { usePathname } from 'next/navigation';
 import React from 'react';
 import { extractGroupAndChannel } from '@/lib/group/extract-group-and-channel';
-import {
-  ALL_CHANNELS,
-  NON_WRITABLE_CHANNELS,
-} from '@/lib/write/write.constants';
+import { WRITABLE_CHANNELS } from '@/lib/write/write.constants';
 import { ComponentSize } from '@/lib/component/component.types';
 import WriteChannelSelector from './write-channel-selector';
 import WriteLink from './write-link';
@@ -15,11 +12,10 @@ export default function WriteButton({ size }: { size: ComponentSize }) {
   const pathname = usePathname();
   const { groupSlug, channelSlug } = extractGroupAndChannel(pathname);
 
-  if (!channelSlug || !ALL_CHANNELS.includes(channelSlug)) return null;
+  if (!channelSlug || !WRITABLE_CHANNELS.includes(channelSlug))
+    return <WriteChannelSelector groupSlug={groupSlug} size={size} />;
 
-  return NON_WRITABLE_CHANNELS.includes(channelSlug) ? (
-    <WriteChannelSelector groupSlug={groupSlug} size={size} />
-  ) : (
+  return (
     <WriteLink groupSlug={groupSlug} channelSlug={channelSlug} size={size} />
   );
 }
