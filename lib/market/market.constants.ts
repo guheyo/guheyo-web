d231531578c66d15c53b289a72c70e379e0fcec1
keypport <keypport@gmail.com>
2024-10-02 03:38:22 +0900

    feat: add MARKET_CHANNEL_OPTIONS

--- lib/market/market.constants.ts
+++ lib/market/market.constants.ts
@@ -1,3 +1,10 @@
+import { BUSINESS_FUNCTION_OPTIONS } from '../offer/offer.constants';
+
 export const OFFER_CHANNELS = ['sell', 'buy', 'swap'];
 
 export const MARKET_CHANNELS = ['auction', ...OFFER_CHANNELS];
+
+export const MARKET_CHANNEL_OPTIONS = [
+  ...BUSINESS_FUNCTION_OPTIONS,
+  { value: 'gb', label: '공동구매' },
+];
