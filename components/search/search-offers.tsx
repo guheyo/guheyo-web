f8f4d2b12475d4ccebf399773df90ec730a7ae74
keypport <keypport@gmail.com>
2024-10-02 16:31:32 +0900

    feat: update SearchOffers

--- components/search/search-offers.tsx
+++ components/search/search-offers.tsx
@@ -1,18 +1,19 @@
 'use client';
 
-import { BusinessFunction } from '@/lib/offer/offer.types';
-import { useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { POST_SEARCH_OPTIONS } from '@/lib/post/post.constants';
+import { parseBusinessFunctionLabel } from '@/lib/offer/parse-business-function-label';
+import { BusinessFunction } from '@/lib/offer/offer.types';
 import ProductCategoriesNavbar from '../categories/product-categories-navbar';
 import OfferSelectors from '../selectors/offer-selectors';
 import OfferFeed from '../offers/offer-feed';
-import BusinessFunctionQueryUpdater from '../offers/business-function-query-updater';
 import SearchContainer from './search-container';
+import MarketChannelNavbar from '../market/market-channel-navbar';
 
 export default function SearchOffers() {
-  const searchParams = useSearchParams();
-  const businessFunction =
-    (searchParams.get('businessFunction') as BusinessFunction) || 'sell';
+  const pathname = usePathname();
+  const businessFunction = (pathname.split('/').at(-1) ||
+    'sell') as BusinessFunction;
 
   const where = {
     businessFunction,
@@ -24,12 +25,15 @@ export default function SearchOffers() {
 
   return (
     <SearchContainer
-      placeholder="어떤 제품을 찾고 있나요?"
+      placeholder={`어떤 ${parseBusinessFunctionLabel({
+        businessFunction,
+      })}글을 찾고 있나요?`}
       options={POST_SEARCH_OPTIONS}
+      channels={<MarketChannelNavbar />}
       categories={<ProductCategoriesNavbar types={['product', 'service']} />}
       selectors={
         <>
-          <BusinessFunctionQueryUpdater defaultBusinessFunction="sell" />
+          <div />
           <OfferSelectors />
         </>
       }
