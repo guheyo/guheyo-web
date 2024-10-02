2009bf482abb74449d99664834f18e39bcd2ab49
keypport <keypport@gmail.com>
2024-10-03 01:45:53 +0900

    feat: set categories if groupSlug exists

--- components/search/search-auctions.tsx
+++ components/search/search-auctions.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { POST_SEARCH_OPTIONS } from '@/lib/post/post.constants';
+import { useSearchParams } from 'next/navigation';
 import ProductCategoriesNavbar from '../categories/product-categories-navbar';
 import AuctionSelectors from '../auction/auction-selectors';
 import AuctionFeed from '../auction/auction-feed';
@@ -8,11 +9,16 @@ import SearchContainer from './search-container';
 import MarketChannelNavbar from '../market/market-channel-navbar';
 
 export default function SearchAuctions() {
+  const searchParams = useSearchParams();
+  const groupSlug = searchParams.get('group');
+
   return (
     <SearchContainer
       placeholder="어떤 경매를 찾고 있나요?"
       options={POST_SEARCH_OPTIONS}
-      categories={<ProductCategoriesNavbar types={['product', 'service']} />}
+      categories={
+        groupSlug && <ProductCategoriesNavbar types={['product', 'service']} />
+      }
       channels={<MarketChannelNavbar />}
       selectors={
         <>
