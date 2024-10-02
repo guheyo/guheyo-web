a82c3eabadf07af373aec8253ccc37423e14f5b1
keypport <keypport@gmail.com>
2024-10-02 22:59:45 +0900

    feat: add selectors

--- components/search/search-brands.tsx
+++ components/search/search-brands.tsx
@@ -2,6 +2,9 @@
 
 import SearchContainer from './search-container';
 import BrandFeed from '../brand/brand-feed';
+import FollowFilterClickButton from '../follow/follow-filter-click-button';
+import BrandSelectors from '../brand/brand-selectors';
+import BrandChannelNavbar from '../brand/brand-channel-navbar';
 
 export default function SearchBrands() {
   const where = {};
@@ -10,6 +13,13 @@ export default function SearchBrands() {
   return (
     <SearchContainer
       placeholder="어떤 브랜드를 찾고 있나요?"
+      channels={<BrandChannelNavbar />}
+      selectors={
+        <>
+          <FollowFilterClickButton />
+          <BrandSelectors />
+        </>
+      }
       Feed={BrandFeed}
       feedProps={{
         type: 'listview',
