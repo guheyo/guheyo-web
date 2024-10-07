3e67170cbed0a56aabee34c05da72b60396c8777
keypport <keypport@gmail.com>
2024-10-08 02:15:50 +0900

    feat: update findThreadPreviews cache via ThreadResponse

--- lib/apollo/cache/thread.ts
+++ lib/apollo/cache/thread.ts
@@ -1,14 +1,24 @@
-import { ThreadPreviewResponse } from '@/generated/graphql';
+import { ThreadResponse } from '@/generated/graphql';
 import { cache } from './cache';
 
-export function updateCacheWithNewThread(newThread: ThreadPreviewResponse) {
+export function updateCacheWithNewThread(newThread: ThreadResponse) {
   cache.modify({
     fields: {
       findThreadPreviews(existingThreadPreviews = {}) {
         return {
           ...existingThreadPreviews,
           edges: [
-            { node: newThread, cursor: newThread.id },
+            {
+              node: {
+                ...newThread,
+                __typename: 'ThreadPreviewResponse',
+                post: {
+                  ...newThread.post,
+                  __typename: 'PostPreviewWithAuthorResponse',
+                },
+              },
+              cursor: newThread.id,
+            },
             ...existingThreadPreviews.edges,
           ],
         };
