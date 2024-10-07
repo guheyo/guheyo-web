a2c429bde2e696ffb7fb089bbf8eadd4a8c03554
keypport <keypport@gmail.com>
2024-10-08 04:07:36 +0900

    feat: remove setTimeout with reload, use updateCacheWithNewOffer

--- components/offers/write-offer-form.tsx
+++ components/offers/write-offer-form.tsx
@@ -12,9 +12,10 @@ import { findCategory } from '@/lib/group/find-category';
 import { parseOfferTermAlertMessage } from '@/lib/offer/parse-offer-term-alert-message';
 import { isPostingLimitExceededError } from '@/lib/post/is-posting-limit-exceeded-error';
 import parseCreateOfferInput from '@/lib/offer/parse-create-offer-input';
-import { createOffer } from '@/lib/api/offer';
+import { createOffer, findOfferPreview } from '@/lib/api/offer';
 import { parseMarketLink } from '@/lib/offer/parse-market-link';
 import { BusinessFunction } from '@/lib/offer/offer.types';
+import { updateCacheWithNewOffer } from '@/lib/apollo/cache/offer';
 import { AuthContext } from '../auth/auth.provider';
 import AlertDialog from '../base/alert-dialog';
 import OfferForm from './offer-form';
@@ -39,15 +40,16 @@ export default function WriteOfferForm({ group }: { group: GroupResponse }) {
 
     try {
       await createOffer(input);
+
+      const { data } = await findOfferPreview(input.id);
+      if (data.findOfferPreview) updateCacheWithNewOffer(data.findOfferPreview);
+
       router.push(
         parseMarketLink({
           groupSlug: group.slug,
           businessFunction: input.businessFunction as BusinessFunction,
         }),
       );
-      setTimeout(() => {
-        window.location.reload();
-      }, 1000);
     } catch (e: any) {
       if (isPostingLimitExceededError(e.message)) {
         const category = findCategory(group.categories, {
