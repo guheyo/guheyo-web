3ef709703f5c6489b834847de950b1d8e5047b4e
keypport <keypport@gmail.com>
2024-10-10 03:00:21 +0900

    refactor: use ButtonDialog

--- components/posts/post-delete-dialog.tsx
+++ components/posts/post-delete-dialog.tsx
@@ -1,56 +1,20 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
+import ButtonDialog from '../base/button-dialog';
 
 export default function PostDeleteDialog({
   handleDelete,
 }: {
   handleDelete: React.MouseEventHandler;
 }) {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen: React.MouseEventHandler = (e) => {
-    e.preventDefault();
-    setOpen(true);
-  };
-
-  const handleClose: React.MouseEventHandler = (e) => {
-    e.preventDefault();
-    setOpen(false);
-  };
-
   return (
-    <>
-      <button type="button" onClick={handleClickOpen} className="w-full">
-        삭제
-      </button>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogContent>
-          <DialogContentText className="text-base md:text-lg text-dark-500 font-medium">
-            게시글을 삭제할까요?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <div className="flex justify-between w-full px-4 pb-3">
-            <Button
-              onClick={handleClose}
-              className="bg-gray-200 hover:bg-gray-300 text-dark-500 font-semibold"
-            >
-              취소
-            </Button>
-            <Button
-              onClick={handleDelete}
-              autoFocus
-              className="bg-gray-800 hover:bg-gray-900 text-gray-300 font-semibold"
-            >
-              삭제
-            </Button>
-          </div>
-        </DialogActions>
-      </Dialog>
-    </>
-  );
+    <ButtonDialog
+      buttonName="삭제"
+      title="게시글 삭제"
+      content="게시글을 완전히 삭제할까요?"
+      closeButtonName="취소"
+      confirmButtonName="삭제"
+      onClose={() => {}}
+      onConfirm={handleDelete}
+    />
+  )
 }
