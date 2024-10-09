63abae6a525358bcde5682e2cb749ef415661873
keypport <keypport@gmail.com>
2024-10-10 01:57:19 +0900

    feat: implement BgDialog

---
+++ components/base/bg-dialog.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import { DialogTitle } from '@mui/material';
+
+export default function BgDialog({
+  open,
+  title,
+  content,
+  closeButtonName,
+  confirmButtonName,
+  onClose,
+  onConfirm,
+}: {
+  open: boolean;
+  title: string;
+  content: string;
+  closeButtonName: string;
+  confirmButtonName: string;
+  onClose: React.MouseEventHandler;
+  onConfirm: React.MouseEventHandler;
+}) {
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle className="text-base font-medium text-dark-500">
+        {title}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText className="text-sm text-dark-500 font-medium">
+          {content}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <div className="flex justify-end gap-2 w-full p-0">
+          <Button
+            onClick={onClose}
+            className="hover:bg-blurple-200 rounded-full text-blurple-500 font-medium"
+          >
+            {closeButtonName}
+          </Button>
+          <Button
+            onClick={onConfirm}
+            autoFocus
+            className="hover:bg-blurple-200 rounded-full text-blurple-500 font-medium"
+          >
+            {confirmButtonName}
+          </Button>
+        </div>
+      </DialogActions>
+    </Dialog>
+  );
+}
