ceab47a620eee29e1504870c50c6f05c57eddd29
keypport <keypport@gmail.com>
2024-10-10 00:38:47 +0900

    feat: route to market channel if offer detail page

--- components/offers/private-offer-menu.tsx
+++ components/offers/private-offer-menu.tsx
@@ -2,24 +2,28 @@ import * as React from 'react';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { useRouter } from 'next/navigation';
-import { OFFER_OPEN, OFFER_CLOSED } from '@/lib/offer/offer.constants';
+import { usePathname, useRouter } from 'next/navigation';
+import { OFFER_OPEN, OFFER_CLOSED, OFFER } from '@/lib/offer/offer.constants';
 import { parseOfferStatusLabel } from '@/lib/offer/parse-offer-status-label';
 import { deleteOffer, findOfferPreview, updateOffer } from '@/lib/api/offer';
-import { OfferStatus } from '@/lib/offer/offer.types';
+import { BusinessFunction, OfferStatus } from '@/lib/offer/offer.types';
 import { parseOfferLink } from '@/lib/offer/parse-offer-link';
 import { IconButton } from '@mui/material';
 import { updateCacheWithDeletedOffer } from '@/lib/apollo/cache/offer';
+import { parseMarketLink } from '@/lib/offer/parse-market-link';
+import { parseUrlSegments } from '@/lib/group/parse-url-segments';
 import PostDeleteDialog from '../posts/post-delete-dialog';
 import AlertDialog from '../base/alert-dialog';
 
 export default function PrivateOfferMenu({
   offerId,
   offerStatus,
+  businessFunction,
   archivedAt,
 }: {
   offerId: string;
   offerStatus: OfferStatus;
+  businessFunction: BusinessFunction;
   archivedAt?: Date;
 }) {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -27,6 +31,8 @@ export default function PrivateOfferMenu({
   const [openAlert, setOpenAlert] = React.useState(false);
   const [alertText, setAlertText] = React.useState('');
   const router = useRouter();
+  const pathname = usePathname();
+  const { channelSlug, identifier } = parseUrlSegments(pathname);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -97,8 +103,15 @@ export default function PrivateOfferMenu({
   const handleDelete: React.MouseEventHandler = async (event) => {
     event.preventDefault();
     await deleteOffer(offerId);
-
     updateCacheWithDeletedOffer(offerId);
+
+    if (channelSlug === OFFER && identifier) {
+      router.push(
+        parseMarketLink({
+          businessFunction,
+        }),
+      );
+    }
 
     setAlertText('삭제되었어요!');
     setOpenAlert(true);
