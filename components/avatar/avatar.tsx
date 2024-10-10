acd040f61567e9ca87617ccf54740795274bc2f3
keypport <keypport@gmail.com>
2024-10-10 21:37:31 +0900

    feat: import convertAvatarSize in Avatar

--- components/avatar/avatar.tsx
+++ components/avatar/avatar.tsx
@@ -1,25 +1,6 @@
+import { convertAvatarSize } from '@/lib/avatar/convert-avatar-size';
 import { FontSize } from '@/lib/font/font.types';
 import { Avatar as MuiAvatar } from '@mui/material';
-
-const convertAvatarSize = (fontSize: FontSize): string => {
-  if (fontSize === 'text-3xs') return '16px';
-  if (fontSize === 'text-2xs') return '20px';
-  if (fontSize === 'text-xs') return '24px';
-  if (fontSize === 'text-sm') return '28px';
-  if (fontSize === 'text-base') return '32px';
-  if (fontSize === 'text-lg') return '36px';
-  if (fontSize === 'text-xl') return '40px';
-  if (fontSize === 'text-2xl') return '44px';
-  if (fontSize === 'text-3xl') return '48px';
-  if (fontSize === 'text-4xl') return '52px';
-  if (fontSize === 'text-5xl') return '56px';
-  if (fontSize === 'text-6xl') return '60px';
-  if (fontSize === 'text-7xl') return '64px';
-  if (fontSize === 'text-8xl') return '68px';
-  if (fontSize === 'text-9xl') return '72px';
-
-  return '32px';
-};
 
 export default function Avatar({
   name,
