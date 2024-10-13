6a1f3f2b497119dc07387c3e5422dd834e4bad54
keypport <113260136+keypport@users.noreply.github.com>
2024-10-14 05:09:34 +0900

    refactor: use BgDialog in ShareButton (#441)

--- components/share/share-button.tsx
+++ components/share/share-button.tsx
@@ -2,12 +2,8 @@
 
 import ReplyIcon from '@mui/icons-material/Reply';
 import React, { useState } from 'react';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
 import { IconButton, Tooltip } from '@mui/material';
+import BgDialog from '../base/bg-dialog';
 
 export default function ShareButton({
   displayLabel,
@@ -57,20 +53,13 @@ export default function ShareButton({
           </IconButton>
         )}
       </Tooltip>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle className="text-lg md:text-xl">공유하기</DialogTitle>
-        <DialogContent className="overflow-x-hidden px-6">
-          {message}
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={handleClose}
-            className="bg-blurple-500 hover:bg-blurple-600 text-light-200 font-semibold"
-          >
-            확인
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <BgDialog
+        open={open}
+        title="공유하기"
+        content={message}
+        closeButtonName="확인"
+        onClose={handleClose}
+      />
     </>
   );
 }
