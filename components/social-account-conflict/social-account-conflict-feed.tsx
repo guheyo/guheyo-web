1bb1c4c1913534f34738c28fb449101b47e0d087
keypport <keypport@gmail.com>
2024-10-16 00:53:28 +0900

    refactor: use SocialAccountConflictFeedHeader

--- components/social-account-conflict/social-account-conflict-feed.tsx
+++ components/social-account-conflict/social-account-conflict-feed.tsx
@@ -10,6 +10,7 @@ import { parseReportStatus } from '@/lib/report/parse-report-status';
 import { convertPeriodToDateString } from '@/lib/date/date.converter';
 import { useInfiniteSocialAccountConflicts } from '@/hooks/use-infinite-social-account-conflicts';
 import SocialAccountConflictPreview from './social-account-conflict-preview';
+import SocialAccountConflictFeedHeader from './social-account-conflict-feed-header';
 
 export default function SocialAccountConflictFeed({
   defaultWhere,
@@ -52,12 +53,7 @@ export default function SocialAccountConflictFeed({
 
   return (
     <div className="grid gap-2 grid-cols-1">
-      <div className="grid grid-cols-12 justify-between items-center w-full p-4 text-gray-200 bg-blurple-500 rounded-lg text-xs md:text-sm">
-        <div className="col-span-5">신규 유저</div>
-        <div className="col-span-5">기존 유저</div>
-        <div className="col-span-1">앱</div>
-        <div className="col-span-1">시간</div>
-      </div>
+      <SocialAccountConflictFeedHeader />
       {conflicts.map((conflict) => (
         <SocialAccountConflictPreview
           key={conflict.cursor}
