f6b3d48109d5dc6b939c4871be040b12bc42e123
keypport <keypport@gmail.com>
2024-10-16 03:40:55 +0900

    feat: add PROVIDER_OPTIONS in social.constants

---
+++ lib/social/social.constants.ts
@@ -0,0 +1,14 @@
+export const PROVIDER_OPTIONS = [
+  {
+    value: 'all',
+    label: '모든 플랫폼',
+  },
+  {
+    value: 'kakao',
+    label: '카카오',
+  },
+  {
+    value: 'naver',
+    label: '네이버',
+  },
+];
