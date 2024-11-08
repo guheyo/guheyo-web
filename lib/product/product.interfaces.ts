e5691e83ac0aeadca5dae00e35a6eecd3ec7a68a
keypport <keypport@gmail.com>
2024-11-09 03:27:33 +0900

    feat: add groupId, categoryId, images in ProductFormValues

--- lib/product/product.interfaces.ts
+++ lib/product/product.interfaces.ts
@@ -1,6 +1,11 @@
+import { UserImage } from '../image/image.interfaces';
+
 export type ProductFormValues = {
   id: string;
   name: string;
   description?: string;
+  groupId?: string;
+  categoryId?: string;
   brandId?: string;
+  images: UserImage[];
 };
