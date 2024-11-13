8f81ff44c4a3685fc6d493c17b3e69b5b176eb55
keypport <keypport@gmail.com>
2024-11-13 21:43:23 +0900

    feat: add tag constatns

---
+++ lib/text/tag.constants.ts
@@ -0,0 +1,10 @@
+export const TAG_PREVIEWS_BUTTON_ARIA_LABEL = 'Tag preview close button';
+
+export const DEFAULT_TAG_PREVIEWS_STYLE =
+  'flex flex-row gap-4 items-center justify-items-start';
+
+export const DEFAULT_TAG_PREVIEW_LAYOUT_STYLE =
+  'flex flex-col relative w-fit h-fit';
+
+export const DEFAULT_TAG_PREVIEW_STYLE =
+  'bg-blurple-500 text-gray-200 rounded-lg p-2';
