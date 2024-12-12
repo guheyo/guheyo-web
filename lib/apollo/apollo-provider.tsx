2efba0cf967cdc87b2d7e568446bf932b1b12983
keypport <keypport@gmail.com>
2024-12-12 21:46:10 +0900

    fix: change deprecated import path

--- lib/apollo/apollo-provider.tsx
+++ lib/apollo/apollo-provider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ApolloNextAppProvider } from '@apollo/experimental-nextjs-app-support/ssr';
+import { ApolloNextAppProvider } from '@apollo/experimental-nextjs-app-support';
 import { makeClient } from './client';
 
 export default function ApolloWrapper({ children }: React.PropsWithChildren) {
