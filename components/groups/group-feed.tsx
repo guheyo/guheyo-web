c7f6e856b10d577d0e4c8c0dab4b3fa36168e57b
keypport <keypport@gmail.com>
2025-03-02 07:12:29 +0900

    feat: set variables in useFindGroupPreviewsQuery variables

--- components/groups/group-feed.tsx
+++ components/groups/group-feed.tsx
@@ -1,20 +1,30 @@
 'use client';
 
-import { useFindGroupPreviewsQuery } from '@/generated/graphql';
+import { GroupStatus, useFindGroupPreviewsQuery } from '@/generated/graphql';
 import GroupPreview from './group-preview';
 
 export default function GroupFeed({ keyword }: { keyword?: string }) {
   const { loading, data } = useFindGroupPreviewsQuery({
+    variables: {
+      where: {
+        status: GroupStatus.Major,
+      },
+      orderBy: {
+        position: 'asc',
+      },
+      take: 10,
+      skip: 0,
+    },
     fetchPolicy: 'cache-first',
   });
   if (loading) return <div />;
   if (!data?.findGroupPreviews) return <div />;
-  const groups = data.findGroupPreviews;
+  const groups = data.findGroupPreviews.edges;
 
   return (
     <div className="grid grid-rows gap-4 md:gap-12">
       {groups.map((group) => (
-        <GroupPreview group={group} key={group.slug} />
+        <GroupPreview group={group.node} key={group.node.slug} />
       ))}
     </div>
   );
