847aef75fa761240ee73c3bd61ee3db849800da4
keypport <113260136+keypport@users.noreply.github.com>
2025-03-20 23:36:17 +0900

    feat: update report alert message (#488)

--- components/reports/reports-alert-dialog.tsx
+++ components/reports/reports-alert-dialog.tsx
@@ -33,7 +33,7 @@ export default function ReportsAlertDialog({ user }: { user: MyUserResponse }) {
       <BgDialog
         open={open}
         title="[신고 알림]"
-        content={`${user.username}님에게 신고가 접수되었어요\n\n소명 댓글을 달지 않을 경우\n이용 제한과 페널티가 부여되니\n사유에 대해 성실하게 소명해 주세요`}
+        content={`${user.username}님에게 신고가 접수되었어요\n\n적합한 소명 댓글을 달지 않을 경우\n이용 제한과 페널티가 부여되니\n사유에 대해 성실하게 소명해 주세요`}
         closeButtonName="확인"
         onClose={handleClose}
       />
