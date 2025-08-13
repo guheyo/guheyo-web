44daa33191ddea011c2c6094080c9625d1692837
keypport <113260136+keypport@users.noreply.github.com>
2025-08-14 01:32:25 +0900

    feat: implement Withdraw component, page (#501)
    * feat: handle redirect in about page
    
    * feat: add withdraw in graphl
    
    * feat: add withdraw in SETTING_PROFILE_OPTIONS
    
    * feat: implement Withdraw component
    
    * feat: add withdraw page

--- components/setting/setting.constants.ts
+++ components/setting/setting.constants.ts
@@ -3,4 +3,5 @@ export const SETTING_PROFILE_OPTIONS = [
   // { value: 'discord', label: '디스코드 프로필' },
   // { value: 'naver', label: '네이버 인증' },
   { value: 'kakao', label: '카카오 인증' },
+  { value: 'withdraw', label: '탈퇴하기' },
 ];
