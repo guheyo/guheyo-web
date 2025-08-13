d89f55a1cf86212df6066766853b2f2954747fce
keypport <113260136+keypport@users.noreply.github.com>
2025-08-14 08:29:34 +0900

    feat: implement UserReviewHome (#505)
    ## 해결하려는 문제가 무엇인가요?
    1. home page와 group home page - 거래 후기 요약 추가
    2. Brand community 비활성화 문제
    
    ## 어떻게 해결했나요?
    1. implement UserReviewHome
    2. remove BrandOverview in Home & Group Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import AuctionHome from '@/components/auction/auction-home';
-import BrandOverview from '@/components/brand/brand-overview';
 import CommunityHome from '@/components/community/community-home';
 import HomeFooter from '@/components/footers/home-footer';
 import GbHome from '@/components/gb/gb-home';
 import GroupFeed from '@/components/groups/group-feed';
 import OfferHome from '@/components/offers/offer-home';
+import UserReviewHome from '@/components/user-review/user-review-home';
 import { Suspense } from 'react';
 
 export const metadata = {
@@ -21,8 +21,8 @@ function Page() {
         <OfferHome businessFunction="buy" />
         <OfferHome businessFunction="swap" />
         <GbHome />
+        <UserReviewHome />
         <CommunityHome />
-        <BrandOverview />
         <div className="px-2 md:px-0 pt-4">
           <GroupFeed />
         </div>
