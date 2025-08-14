b9685cfc3e784eab8c4da77ad7fd021548b98973
keypport <keypport@gmail.com>
2025-08-14 18:42:01 +0900

    feat: change community to thread in THREAD_CHANNELS

--- lib/thread/thread.constants.ts
+++ lib/thread/thread.constants.ts
@@ -4,4 +4,4 @@ export const PRIVATE_THREAD_OPTIONS = [{ value: 'post', label: '작성글' }];
 
 export const PUBLIC_THREAD_OPTIONS = [{ value: 'post', label: '작성글' }];
 
-export const THREAD_CHANNELS = ['gb', 'community'];
+export const THREAD_CHANNELS = ['gb', 'thread'];
